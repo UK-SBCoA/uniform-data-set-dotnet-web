1659b62b1a9707f0378fdd38b6c5469e245ba15f
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-06-09 15:57:23 -0400

    Adds Display Names

--- src/UDS.Net.Forms/Models/UDS3/B7.cs
+++ src/UDS.Net.Forms/Models/UDS3/B7.cs
@@ -8,34 +8,34 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class B7 : FormModel
     {
-        [Display(Name = "")]
+        [Display(Name = "Writing checks, paying bills, or balancing a checkbook")]
         public int? BILLS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Assembling tax records, business affairs, or other papers")]
         public int? TAXES { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Shopping alone for clothes, household necessities, or groceries")]
         public int? SHOPPING { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "In the past four weeks, did the subject have any difficulty or need help with: Playing a game of skill such as bridge or chess, working on a hobby")]
         public int? GAMES { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "In the past four weeks, did the subject have any difficulty or need help with: Heating water, making a cup of coffee, turning off the stove")]
         public int? STOVE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Preparing a balanced meal")]
         public int? MEALPREP { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Keeping track of current events")]
         public int? EVENTS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Paying attention to and understanding a TV program, book, or magazine")]
         public int? PAYATTN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Remembering appointments, family occasions, holidays, medications")]
         public int? REMDATES { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Traveling out of the neighborhood, driving, or arranging to take public transportation")]
         public int? TRAVEL { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
