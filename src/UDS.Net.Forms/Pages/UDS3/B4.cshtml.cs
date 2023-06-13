b669921969745967c1052a8a707afd71af78e8a1
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-06-13 15:33:27 -0400

    Add radio button options as list items

--- src/UDS.Net.Forms/Pages/UDS3/B4.cshtml.cs
+++ src/UDS.Net.Forms/Pages/UDS3/B4.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using UDS.Net.Forms.Models.PageModels;
 using UDS.Net.Forms.Models.UDS3;
+using UDS.Net.Forms.TagHelpers;
 using UDS.Net.Services;
 
 namespace UDS.Net.Forms.Pages.UDS3
@@ -14,6 +15,24 @@ namespace UDS.Net.Forms.Pages.UDS3
     {
         [BindProperty]
         public B4 B4 { get; set; } = default!;
+
+        public List<RadioListItem> ImpairmentListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("None", "0"),
+            new RadioListItem("Questionable", "0.5"),
+            new RadioListItem("Mild", "1"),
+            new RadioListItem("Moderate", "2"),
+            new RadioListItem("Severe", "3"),
+        };
+
+        public List<RadioListItem> ImpairmentSimpleListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("None", "0"),
+            new RadioListItem("Mild", "1"),
+            new RadioListItem("Moderate", "2"),
+            new RadioListItem("Severe", "3"),
+        };
+
         public B4Model(IVisitService visitService) : base(visitService, "B4")
         {
         }
