9a92cf48d62421bbc7c4be1f0eb2cbe844eb63e8
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-06-13 13:18:36 -0400

    adds display names

--- src/UDS.Net.Forms/Models/UDS3/T1.cs
+++ src/UDS.Net.Forms/Models/UDS3/T1.cs
@@ -9,35 +9,35 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class T1 : FormModel
     {
-        [Display(Name = "")]
+        [Display(Name = "Subject is too cognitively impaired for an in-person UDS visit.")]
         public int? TELCOG { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Subject is too physically impaired (medical illness or injury) to attend an in-person UDS visit.")]
         public int? TELILL { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Subject is homebound or in a nursing home and cannot travel.")]
         public int? TELHOME { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Subject or co-participant refused an in-person UDS visit.")]
         public int? TELREFU { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "COVID pandemic precludes traditional in-person UDS visit.")]
         public int? TELCOV { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other")]
         public int? TELOTHR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other (SPECIFY)")]
         [MaxLength(60)]
         public string? TELOTHRX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "What modality of communication was used to collect this remote UDS packet?")]
         public int? TELMOD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Is the subject likely to resume in-person UDS follow-up evaluation?")]
         public int? TELINPER { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Has a Milestones Form documenting the change to telephone follow-up been completed?\n(If no, complete a Milestones Form now.)")]
         public int? TELMILE { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
