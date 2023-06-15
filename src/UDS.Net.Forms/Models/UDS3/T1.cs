4e3316f37f98519c6e88afc6090ca78a77544874
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-06-15 15:24:55 -0400

    Changes subject to participant

--- src/UDS.Net.Forms/Models/UDS3/T1.cs
+++ src/UDS.Net.Forms/Models/UDS3/T1.cs
@@ -9,16 +9,16 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class T1 : FormModel
     {
-        [Display(Name = "Subject is too cognitively impaired for an in-person UDS visit.")]
+        [Display(Name = "Participant is too cognitively impaired for an in-person UDS visit.")]
         public int? TELCOG { get; set; }
 
-        [Display(Name = "Subject is too physically impaired (medical illness or injury) to attend an in-person UDS visit.")]
+        [Display(Name = "Participant is too physically impaired (medical illness or injury) to attend an in-person UDS visit.")]
         public int? TELILL { get; set; }
 
-        [Display(Name = "Subject is homebound or in a nursing home and cannot travel.")]
+        [Display(Name = "Participant is homebound or in a nursing home and cannot travel.")]
         public int? TELHOME { get; set; }
 
-        [Display(Name = "Subject or co-participant refused an in-person UDS visit.")]
+        [Display(Name = "Participant or co-participant refused an in-person UDS visit.")]
         public int? TELREFU { get; set; }
 
         [Display(Name = "COVID pandemic precludes traditional in-person UDS visit.")]
@@ -34,7 +34,7 @@ namespace UDS.Net.Forms.Models.UDS3
         [Display(Name = "What modality of communication was used to collect this remote UDS packet?")]
         public int? TELMOD { get; set; }
 
-        [Display(Name = "Is the subject likely to resume in-person UDS follow-up evaluation?")]
+        [Display(Name = "Is the participant likely to resume in-person UDS follow-up evaluation?")]
         public int? TELINPER { get; set; }
 
         [Display(Name = "Has a Milestones Form documenting the change to telephone follow-up been completed? (If no, complete a Milestones Form now.)")]
