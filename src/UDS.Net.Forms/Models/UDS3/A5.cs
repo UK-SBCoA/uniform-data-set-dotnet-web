faaeecd5e154233932e3d95b6c5e188f70c7b947
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-06-26 15:26:22 -0400

    Adds display name, regex, range

--- src/UDS.Net.Forms/Models/UDS3/A5.cs
+++ src/UDS.Net.Forms/Models/UDS3/A5.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using UDS.Net.Forms.DataAnnotations;
 
 namespace UDS.Net.Forms.Models.UDS3
 {
@@ -8,208 +9,230 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class A5 : FormModel
     {
-        [Display(Name = "")]
+        [Display(Name = "Has subject smoked within the last 30 days?")]
         public int? TOBAC30 { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Has participant smoked more than 100 cigarettes in his/her life?")]
         public int? TOBAC100 { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Total years smoked")]
+        [RegularExpression("^(\\d|[1-7]\\d|8[0-7])|99)$", ErrorMessage = "(0-87, 99 = Unknown)")]
         public int? SMOKYRS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Average number of packs smoked per day")]
         public int? PACKSPER { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If the subject quit smoking, specify age at which he/she last smoked (i.e., quit)")]
+        [RegularExpression("^([89]|[1-9]\\d|10\\d|110)|888|999)$", ErrorMessage = "(8-110, 888 = N/A, 999 = Unknown)")]
         public int? QUITSMOK { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "In the past three months,has the subject consumed any alcohol?")]
         public int? ALCOCCAS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "During the past three months, how often did the subject have at least one drink of any alcoholic beverage such as wine, beer, malt liquor, or spirits?")]
         public int? ALCFREQ { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Heart attack/cardiac arrest")]
         public int? CVHATT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "More than one heart attack?")]
         public int? HATTMULT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Year of most recent heart attack")]
+        //custom validation
         public int? HATTYEAR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Atrial fibrillation")]
         public int? CVAFIB { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Angioplasty/endarterectomy/stent")]
         public int? CVANGIO { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cardiac bypass procedure")]
         public int? CVBYPASS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Pacemaker and/or defibrillator")]
         public int? CVPACDEF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Congestive heart failure")]
         public int? CVCHF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Angina")]
         public int? CVANGINA { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Heart valve replacement or repair")]
         public int? CVHVALVE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other cardiovascular disease")]
         public int? CVOTHR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other cardiovascular disease (specify)")]
+        [MaxLength(60)]
+        [RequiredIf(nameof(CVOTHR), "1", ErrorMessage = "Specify cardiovascular disease")]
+        [RequiredIf(nameof(CVOTHR), "2", ErrorMessage = "Specify cardiovascular disease")]
         public string? CVOTHRX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Stroke – by history, not exam (imaging is not required)")]
         public int? CBSTROKE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "More than one stroke?")]
         public int? STROKMUL { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Year of most recent stroke")]
+        //custom validation
         public int? STROKYR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Transient ischemic attack (TIA)")]
         public int? CBTIA { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "More than one TIA")]
         public int? TIAMULT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Year of most recent TIA")]
+        //custom valiation
         public int? TIAYEAR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Parkinson’s disease (PD)")]
         public int? PD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Year of PD diagnosis")]
+        //custom validation 
         public int? PDYR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other parkinsonian disorder (e.g, PSP, CBD")]
         public int? PDOTHR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Year of parkinsonian disorder diagnosis")]
+        //custom validation
         public int? PDOTHRYR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Seizures")]
         public int? SEIZURES { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Traumatic brain injury (TBI)")]
         public int? TBI { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "TBI with brief loss of consciousness (<5 minutes)")]
         public int? TBIBRIEF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "TBI with extended loss of consciousness (≥5 minutes)")]
         public int? TBIEXTEN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "TBI without loss of consciousness (as might result from military detonations or sports injuries)?")]
         public int? TBIWOLOS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Year of most recent TBI")]
+        //custom validation
         public int? TBIYEAR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Diabetes")]
         public int? DIABETES { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Recent/active or Remote/inactive, which type?")]
         public int? DIABTYPE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Hypertension")]
         public int? HYPERTEN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Hypercholesterolemia")]
         public int? HYPERCHO { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "B12 deficiency")]
         public int? B12DEF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Thyroid disease")]
         public int? THYROID { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Arthritis")]
         public int? ARTHRIT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Type of arthritis")]
         public int? ARTHTYPE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other (specify)")]
+        [RequiredIf(nameof(ARTHTYPE), "1", ErrorMessage = "Specify sleep disorder")]
+        [MaxLength(60)]
         public string? ARTHTYPX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Upper extremity")]
         public int? ARTHUPEX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Lower extremity")]
         public int? ARTHLOEX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Spine")]
         public int? ARTHSPIN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Unknown")]
         public int? ARTHUNK { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Incontinence - urinary")]
         public int? INCONTU { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Incontinence — bowel")]
         public int? INCONTF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Sleep apnea")]
         public int? APNEA { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "REM sleep behavior disorder (RBD)")]
         public int? RBD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Hyposomnia/insomnia")]
         public int? INSOMN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other sleep disorder")]
         public int? OTHSLEEP { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other sleep disorder (specify)")]
+        [MaxLength(60)]
+        [RequiredIf(nameof(OTHSLEEP), "1", ErrorMessage = "Specify sleep disorder")]
+        [RequiredIf(nameof(OTHSLEEP), "2", ErrorMessage = "Specify sleep disorder")]
         public string? OTHSLEEX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Alcohol abuse: Clinically significant impairment occuring over a 12-month period manifested in one of the following areas: work, driving, legal, or social")]
         public int? ALCOHOL { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other abused substances: Clinically significant impairment occuring over a 12-month period manifested in one of the following areas: work, driving, legal, or social")]
         public int? ABUSOTHR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If recent/active or remote/inactive, specify abused substance")]
+        [MaxLength(60)]
+        [RequiredIf(nameof(ABUSOTHR), "1", ErrorMessage = "Specify abused substance")]
+        [RequiredIf(nameof(ABUSOTHR), "2", ErrorMessage = "Specify abused substance")]
         public string? ABUSX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Post-traumatic stress disorder (PTSD)")]
         public int? PTSD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Bipolar disorder")]
         public int? BIPOLAR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Schizophrenia")]
         public int? SCHIZ { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Active depression in the last two years")]
         public int? DEP2YRS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Depression episodes more than two years ago")]
         public int? DEPOTHR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Anxiety")]
         public int? ANXIETY { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Obsessive-compulsive disorder (OCD)")]
         public int? OCD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Developmental neuropsychiatric disorders (e.g., autism spectrum disorder [ASD], attention-deficit hyperactivity disorder [ADHD], dyslexia)")]
         public int? NPSYDEV { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other psychiatric disorders")]
         public int? PSYCDIS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If recent/active or remote/inactive, specify disorder")]
+        [MaxLength(60)]
+        [RequiredIf(nameof(PSYCDIS), "1", ErrorMessage = "Specify disorder")]
+        [RequiredIf(nameof(PSYCDIS), "2", ErrorMessage = "Specify disorder")]
         public string? PSYCDISX { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
