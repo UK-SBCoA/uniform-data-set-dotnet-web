e306f1e56c86ed127625fa57a18c22dcf183fb63
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2023-06-27 11:04:20 -0400

    Displays labels

--- src/UDS.Net.Forms/Models/UDS3/B4.cs
+++ src/UDS.Net.Forms/Models/UDS3/B4.cs
@@ -8,34 +8,34 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class B4 : FormModel
     {
-        [Display(Name = "")]
+        [Display(Name = "MEMORY")]
         public int? MEMORY { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "ORIENTATION")]
         public int? ORIENT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "JUDGMENT AND PROBLEM SOLVING")]
         public int? JUDGMENT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "COMMUNITY AFFAIRS")]
         public int? COMMUN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "HOME AND HOBBIES")]
         public int? HOMEHOBB { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "PERSONAL CARE")]
         public int? PERSCARE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "CDR SUM OF BOXES")]
         public int? CDRSUM { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "GLOBAL CDR")]
         public int? CDRGLOB { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Behavior, comportment,and personality")]
         public int? COMPORT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Language")]
         public int? CDRLANG { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
