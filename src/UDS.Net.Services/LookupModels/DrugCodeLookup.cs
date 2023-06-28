7f2263cafac05d5788175e08c925381352ee30c6
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-06-28 15:18:52 -0400

    Add A4 drug code lookup

---
+++ src/UDS.Net.Services/LookupModels/DrugCodeLookup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UDS.Net.Dto;
+
+namespace UDS.Net.Services.LookupModels
+{
+    public class DrugCodeLookup
+    {
+        public string SearchTerm { get; set; }
+        public object LookupParameters { get; set; }
+
+        public List<DrugCode> DrugCodes { get; set; }
+
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public int TotalResultsCount { get; set; }
+
+        public ErrorDto Error { get; set; }
+    }
+}
+
