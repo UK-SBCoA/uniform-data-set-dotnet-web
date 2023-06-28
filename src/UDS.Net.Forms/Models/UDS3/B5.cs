3c4d71c2480df51d6b4162920df390a52a08345d
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2023-06-28 16:12:05 -0400

    Adds display labels

--- src/UDS.Net.Forms/Models/UDS3/B5.cs
+++ src/UDS.Net.Forms/Models/UDS3/B5.cs
@@ -29,52 +29,52 @@ namespace UDS.Net.Forms.Models.UDS3
 
         public int? HALLSEV { get; set; }
 
-        [Display(Name = "Agitation/aggression")]
+        [Display(Name = "Agitation/aggression - Is the patient resistive to help from others at times, or hard to handle?")]
         public int? AGIT { get; set; }
 
         public int? AGITSEV { get; set; }
 
-        [Display(Name = "Depression/dysphoria")]
+        [Display(Name = "Depression/dysphoria - Does the patient seem sad or say that he/she is despressed?")]
         public int? DEPD { get; set; }
 
         public int? DEPDSEV { get; set; }
 
-        [Display(Name = "Anxiety")]
+        [Display(Name = "Anxiety - Does the patient become upset when separated from you? Does he/she have any otehr signs of nervousness such as shortness of breath, sighing, being unable to relax or feeling excessively tense?")]
         public int? ANX { get; set; }
 
         public int? ANXSEV { get; set; }
 
-        [Display(Name = "Elation/euphoria")]
+        [Display(Name = "Elation/euphoria - Does the patient appear to feel too good or act excessively happy?")]
         public int? ELAT { get; set; }
 
         public int? ELATSEV { get; set; }
 
-        [Display(Name = "Apathy/indifference")]
+        [Display(Name = "Apathy/indifference - Does the patient seem less interested in his/her usual activities or in the activities and plans of others?")]
         public int? APA { get; set; }
 
         public int? APASEV { get; set; }
 
-        [Display(Name = "Disinhibition")]
+        [Display(Name = "Disinhibition - Does the patient seem to act impulsively, for example, talking to strangers as if he/she knows them, or saying things that may hurt people's feelings?")]
         public int? DISN { get; set; }
 
         public int? DISNSEV { get; set; }
 
-        [Display(Name = "Irritability/Iability")]
+        [Display(Name = "Irritability/Lability - Is the paitent impatient and cranky? Does he/she have difficulty coping with delays or waiting for planned activities?")]
         public int? IRR { get; set; }
 
         public int? IRRSEV { get; set; }
 
-        [Display(Name = "Motor disturbance")]
+        [Display(Name = "Motor disturbance - Does the patient engage in repetitive activities such as pacing around the house, handling buttons, wrapping string, or doing other things repeatedly?")]
         public int? MOT { get; set; }
 
         public int? MOTSEV { get; set; }
 
-        [Display(Name = "Nighttime behaviors")]
+        [Display(Name = "Nighttime behaviors - Does the patient awaken you during the night, rise too early in the morning, or take excessive naps during the day?")]
         public int? NITE { get; set; }
 
         public int? NITESEV { get; set; }
 
-        [Display(Name = "Appetite/eating")]
+        [Display(Name = "Appetite/eating - Has the patient lost or gained weight, or had a change in the type of food he/she likes?")]
         public int? APP { get; set; }
 
         public int? APPSEV { get; set; }
