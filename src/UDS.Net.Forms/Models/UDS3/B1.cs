ee9b3e6218058dfa130fd974046ac08086e97387
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-06-29 21:07:09 -0400

    Fix regex for height and diastolic reading, fixes small typos

--- src/UDS.Net.Forms/Models/UDS3/B1.cs
+++ src/UDS.Net.Forms/Models/UDS3/B1.cs
@@ -10,22 +10,22 @@ namespace UDS.Net.Forms.Models.UDS3
     public class B1 : FormModel
     {
         [Display(Name = "Participant height (inches)")]
-        [RegularExpression("^(3[6-9]\\.\\d| [4-7]\\d\\.\\d|8[0-7]\\.\\d|87\\.[0-9]|88\\.8)$", ErrorMessage = "(36.0-87.9, 88.8 = not assessed)")]
+        [RegularExpression("^(3[6-9].\\d|[4-7][0-9].\\d|8[0-7].\\d|88.8)$", ErrorMessage = "(36.0-87.9, 88.8 = not assessed)")]
         public double? HEIGHT { get; set; }
 
         [Display(Name = "Participant weight (lbs)")]
         [RegularExpression("^(5\\d|[6-9]\\d|[1-3]\\d{2}|400|888)$", ErrorMessage = "(50-400, 888 = Not assessed)")]
         public int? WEIGHT { get; set; }
 
-        [Display(Name = "Participantblood pressure at initial reading (sitting)")]
+        [Display(Name = "Systolic (sitting)")]
         [RegularExpression("^(7\\d|[89]\\d|1\\d{2}|2[0-2]\\d|230|777|888)$", ErrorMessage = "(70-230, 777 = BP Addendum submitted, 888 = Not assessed)")]
         public int? BPSYS { get; set; }
 
-        [Display(Name = "Participant blood pressure (sitting), diastolic")]
-        [RegularExpression("^(3\\d|[4-9]\\d|1[0-3]\\d|140|888)$", ErrorMessage = "(30-140, 888 = Not assessed)")]
+        [Display(Name = "Diastolic (sitting)")]
+        [RegularExpression("^(3\\d|[4-9]\\d|1[0-3]\\d|140|777|888)$", ErrorMessage = "(30-140, 777 = BP Addendum submitted, 888 = Not assessed)")]
         public int? BPDIAS { get; set; }
 
-        [Display(Name = "Participantresting heart rate (pulse)")]
+        [Display(Name = "Participant resting heart rate (pulse)")]
         [RegularExpression("^(3[3-9]|[4-9]\\d|1[0-5]\\d|160|888)$", ErrorMessage = "(33-160, 888 = Not assessed)")]
         public int? HRATE { get; set; }
 
