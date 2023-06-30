7db75c06ce468d264398c18dd4cdd7876497ef49
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-06-30 11:56:22 -0400

    adds display names

--- src/UDS.Net.Forms/Models/UDS3/D1.cs
+++ src/UDS.Net.Forms/Models/UDS3/D1.cs
@@ -9,274 +9,274 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class D1 : FormModel
     {
-        [Display(Name = "")]
+        [Display(Name = "Diagnosis method — responses in this form are based on diagnosis by")]
         public int? DXMETHOD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Does the subject have normal cognition (global CDR=0 and/or neuropsychological testing within normal range) and normal behavior (i.e., the subject does not exhibit behavior sufficient to diagnose MCI or dementia due to FTLD or LBD)?")]
         public int? NORMCOG { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Does the subject meet the criteria for dementia?")]
         public int? DEMENTED { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Amnestic multidomain dementia syndrome")]
         public int? AMNDEM { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Posterior cortical atrophy syndrome (or primary visual presentation)")]
         public int? PCA { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Primary progressive aphasia (PPA) syndrome")]
         public int? PPASYN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Please indicate PPA type")]
         public int? PPASYNT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Behavioral variant FTD (bvFTD) syndrome")]
         public int? FTDSYN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Lewy body dementia syndrome")]
         public int? LBDSYN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Non-amnestic multidomain dementia, not PCA, PPA, bvFTD, or DLB syndrome")]
         public int? NAMNDEM { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Amnestic MCI, single domain (aMCI SD)")]
         public int? MCIAMEM { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Amnestic MCI, multiple domains (aMCI MD)")]
         public int? MCIAPLUS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Language")]
         public int? MCIAPLAN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Attention")]
         public int? MCIAPATT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Executive")]
         public int? MCIAPEX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Visuospatial")]
         public int? MCIAPVIS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Non-amnestic MCI, single domain (naMCI SD)")]
         public int? MCINON1 { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Language")]
         public int? MCIN1LAN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Attention")]
         public int? MCIN1ATT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Executive")]
         public int? MCIN1EX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Visuospatial")]
         public int? MCIN1VIS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Non-amnestic MCI, multiple domains (naMCI MD)")]
         public int? MCINON2 { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Language")]
         public int? MCIN2LAN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Attention")]
         public int? MCIN2ATT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Executive")]
         public int? MCIN2EX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Visuospatial")]
         public int? MCIN2VIS { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "IMPNOMCI")]
         public int? IMPNOMCI { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "AMYLPET")]
         public int? AMYLPET { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Abnormally low amyloid in CSF")]
         public int? AMYLCSF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "FDG-PET pattern of AD")]
         public int? FDGAD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Hippocampal atrophy")]
         public int? HIPPATR { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Tau PET evidence for AD")]
         public int? TAUPETAD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Abnormally elevated CSF tau or ptau")]
         public int? CSFTAU { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "FDG-PET evidence for frontal or anterior temporal hypometabolism for FTLD")]
         public int? FDGFTLD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Tau PET evidence for FTLD")]
         public int? TPETFTLD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Structural MR evidence for frontal or anterior temporal atrophy for FTLD")]
         public int? MRFTLD { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Dopamine transporter scan (DATscan) evidence for Lewy body disease")]
         public int? DATSCAN { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other (specify)")]
         public int? OTHBIOM { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other (specify)")]
         [MaxLength(60)]
         public string? OTHBIOMX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Large vessel infarct(s)")]
         public int? IMAGLINF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Lacunar infarct(s)")]
         public int? IMAGLAC { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Macrohemorrhage(s)")]
         public int? IMAGMACH { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Microhemorrhage(s)")]
         public int? IMAGMICH { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Moderate white-matter hyperintensity (CHS score 5–6)")]
         public int? IMAGMWMH { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Extensive white-matter hyperintensity (CHS score 7–8+)")]
         public int? IMAGEWMH { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Does the subject have a dominantly inherited AD mutation (PSEN1, PSEN2, APP)")]
         public int? ADMUT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Does the subject have a hereditary FTLD mutation (e.g., GRN, VCP, TARBP, FUS, C9orf72, CHMP2B, MAPT)?")]
         public int? FTLDMUT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Does the subject have a hereditary mutation other than an AD or FTLD mutation?")]
         public int? OTHMUT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other (specify)")]
         [MaxLength(60)]
         public string? OTHMUTX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Alzheimer's disease")]
         public int? ALZDIS { get; set; }
 
         [Display(Name = "")]
         public int? ALZDISIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Lewy body disease")]
         public int? LBDIS { get; set; }
 
         [Display(Name = "")]
         public int? LBDIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Parkinson's disease")]
         public int? PARK { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Multiple system atrophy")]
         public int? MSA { get; set; }
 
         [Display(Name = "")]
         public int? MSAIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Progressive supranuclear palsy (PSP)")]
         public int? PSP { get; set; }
 
         [Display(Name = "")]
         public int? PSPIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Corticobasal degeneration (CBD)")]
         public int? CORT { get; set; }
 
         [Display(Name = "")]
         public int? CORTIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "FTLD with motor neuron disease")]
         public int? FTLDMO { get; set; }
 
         [Display(Name = "")]
         public int? FTLDMOIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "FTLD NOS")]
         public int? FTLDNOS { get; set; }
 
         [Display(Name = "")]
         public int? FTLDNOIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If FTLD (Questions 14a – 14d) is Present, specify FTLD subtype")]
         public int? FTLDSUBT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other (specify)")]
         [MaxLength(60)]
         public string? FTLDSUBX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Vascular brain injury (based on clinical or imaging evidence)")]
         public int? CVD { get; set; }
 
         [Display(Name = "")]
         public int? CVDIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Previous symptomatic stroke?")]
         public int? PREVSTK { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Temporal relationship between stroke and cognitive decline?")]
         public int? STROKDEC { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Confirmation of stroke by neuroimaging?")]
         public int? STKIMAG { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Is there imaging evidence of cystic infarction in cognitive network(s)?")]
         public int? INFNETW { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Is there imaging evidence of cystic infarction, imaging evidence of extensive white matter hyperintensity (CHS grade 7–8+), and impairment in executive function?")]
         public int? INFWMH { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Essential tremor")]
         public int? ESSTREM { get; set; }
 
         [Display(Name = "")]
         public int? ESSTREIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Down syndrome")]
         public int? DOWNS { get; set; }
 
         [Display(Name = "")]
         public int? DOWNSIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Huntington's disease")]
         public int? HUNT { get; set; }
 
         [Display(Name = "")]
         public int? HUNTIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Prion disease (CJD, other)")]
         public int? PRION { get; set; }
 
         [Display(Name = "")]
         public int? PRIONIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Traumatic brain injury")]
         public int? BRNINJ { get; set; }
 
         [Display(Name = "")]
         public int? BRNINJIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, does the subject have symptoms consistent with chronic traumatic encephalopathy?")]
         public int? BRNINCTE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Normal-pressure hydrocephalus")]
         public int? HYCEPH { get; set; }
 
         [Display(Name = "")]
         public int? HYCEPHIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Epilepsy")]
         public int? EPILEP { get; set; }
 
         [Display(Name = "")]
         public int? EPILEPIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "CNS neoplasm")]
         public int? NEOP { get; set; }
 
         [Display(Name = "")]
@@ -285,125 +285,125 @@ namespace UDS.Net.Forms.Models.UDS3
         [Display(Name = "")]
         public int? NEOPSTAT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Human immunodeficiency virus (HIV)")]
         public int? HIV { get; set; }
 
         [Display(Name = "")]
         public int? HIVIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment due to other neurologic, genetic, or infectious conditions not listed above")]
         public int? OTHCOG { get; set; }
 
         [Display(Name = "")]
         public int? OTHCOGIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, specify")]
         [MaxLength(60)]
         public string? OTHCOGX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Active depression")]
         public int? DEP { get; set; }
 
         [Display(Name = "")]
         public int? DEPIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, select one")]
         public int? DEPTREAT { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Bipolar disorder")]
         public int? BIPOLDX { get; set; }
 
         [Display(Name = "")]
         public int? BIPOLDIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Schizophrenia or other psychosis")]
         public int? SCHIZOP { get; set; }
 
         [Display(Name = "")]
         public int? SCHIZOIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Anxiety disorder")]
         public int? ANXIET { get; set; }
 
         [Display(Name = "")]
         public int? ANXIETIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Delirium")]
         public int? DELIR { get; set; }
 
         [Display(Name = "")]
         public int? DELIRIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Post-traumatic stress disorder (PTSD)")]
         public int? PTSDDX { get; set; }
 
         [Display(Name = "")]
         public int? PTSDDXIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Other psychiatric disease")]
         public int? OTHPSY { get; set; }
 
         [Display(Name = "")]
         public int? OTHPSYIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, specify")]
         [MaxLength(60)]
         public string? OTHPSYX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment due to alcohol abuse")]
         public int? ALCDEM { get; set; }
 
         [Display(Name = "")]
         public int? ALCDEMIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Current alcohol abuse")]
         public int? ALCABUSE { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment due to other substance abuse")]
         public int? IMPSUB { get; set; }
 
         [Display(Name = "")]
         public int? IMPSUBIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment due to systemic disease/medical illness (as indicated on Form D2)")]
         public int? DYSILL { get; set; }
 
         [Display(Name = "")]
         public int? DYSILLIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment due to medications")]
         public int? MEDS { get; set; }
 
         [Display(Name = "")]
         public int? MEDSIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment NOS")]
         public int? COGOTH { get; set; }
 
         [Display(Name = "")]
         public int? COGOTHIF { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, specify")]
         [MaxLength(60)]
         public string? COGOTHX { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment NOS")]
         public int? COGOTH2 { get; set; }
 
         [Display(Name = "")]
         public int? COGOTH2F { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, specify")]
         [MaxLength(60)]
         public string? COGOTH2X { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Cognitive impairment NOS")]
         public int? COGOTH3 { get; set; }
 
         [Display(Name = "")]
         public int? COGOTH3F { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "If Present, specify")]
         [MaxLength(60)]
         public string? COGOTH3X { get; set; }
 
