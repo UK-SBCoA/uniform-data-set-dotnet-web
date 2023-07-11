c810f55b7abd50e728f2870560a5a4e1fd1c2916
mlan225 <17088502+mlan225@users.noreply.github.com>
2023-07-11 09:28:36 -0400

    Add program.cs code from dashboard

--- src/UDS.Net.Web.MVC/Program.cs
+++ src/UDS.Net.Web.MVC/Program.cs
@@ -8,20 +8,62 @@ using UDS.Net.Web.MVC.Services;
 using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Microsoft.Extensions.FileProviders;
 using System.Diagnostics;
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web.UI;
+using Microsoft.Identity.Client;
+using Microsoft.Extensions.Caching.Cosmos;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+ConfigurationManager configuration = builder.Configuration;
 // Replace identity service with your preferred provider rather than using this one. Roles required:
 // Administrator
 // SuperUser
 // Examiner
-var connectionString = configuration.GetConnectionString("AuthServiceConnection");
+//var connectionString = configuration.GetConnectionString("AuthServiceConnection");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
+    {
+        cacheOptions.DatabaseName = configuration["Cosmos:CacheDatabase"];
+        cacheOptions.ContainerName = configuration["Cosmos:CacheContainer"];
+        cacheOptions.ClientBuilder = new Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder(configuration.GetConnectionString("CosmosCache"));
+        cacheOptions.CreateIfNotExists = true;
+    });
+}
+
+builder.Services
+       .AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureAd")
+       .EnableTokenAcquisitionToCallDownstreamApi()
+       .AddMicrosoftGraph(configuration.GetSection("DownstreamApis:UDSNetApi"))
+       .AddDistributedTokenCaches();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(3600);
+    options.Cookie.IsEssential = true;
+});
+
+// MicrosoftIdentityWebChallengeUserException https://aka.ms/ms-id-web/ca_incremental-consent
+builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme,
+        options =>
+        {
+            options.AccessDeniedPath = new PathString("/Account/AccessDenied");
+            options.ReturnUrlParameter = "returnUrl";
+            if (!builder.Environment.IsProduction())
+                options.Events = new RejectSessionCookieWhenAccountNotInCacheEvents();
+        });
+
+builder.Services.AddHttpContextAccessor();
+
 
 ////*************************************************************************************************
 // Replace API and implemented services with your own if you don't want to use the included API here
@@ -38,7 +80,15 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddRazorPages(); // Enables UDS.Net.Forms razor class library. More here: https://learn.microsoft.com/en-us/aspnet/core/razor-pages/
 
-var mvcBuilder = builder.Services.AddControllersWithViews();
+//var mvcBuilder = builder.Services.AddControllersWithViews();
+var mvcBuilder = builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+})
+.AddMicrosoftIdentityUI();
 
 ////*************************************************************************************************
 // Only used during development
@@ -69,31 +119,31 @@ if (builder.Environment.IsDevelopment() && Debugger.IsAttached)
 ////*************************************************************************************************
 
 // Uncomment to remove enforced authentication
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-});
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+//        .RequireAuthenticatedUser()
+//        .Build();
+//});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-    using (var scope = app.Services.CreateScope())
-    {
-        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        db.Database.Migrate();
-    }
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseMigrationsEndPoint();
+//    using (var scope = app.Services.CreateScope())
+//    {
+//        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//        db.Database.Migrate();
+//    }
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // A must for using content from referenced UDS.Net.Forms RCL
@@ -116,3 +166,30 @@ app.UseEndpoints(endpoints =>
 
 app.Run();
 
+// Workaround for in memory token caching on dev and non-production environments
+// https://github.com/AzureAD/microsoft-identity-web/issues/13#issuecomment-878528492
+internal class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticationEvents
+{
+    public async override Task ValidatePrincipal(CookieValidatePrincipalContext context)
+    {
+        try
+        {
+            var tokenAcquisition = context.HttpContext.RequestServices.GetRequiredService<ITokenAcquisition>();
+            var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var token = await tokenAcquisition.GetAccessTokenForUserAsync(
+                scopes: config.GetValue<string>("DownstreamApis:GraphApi:Scopes")?.Split(' '),
+                user: context.Principal);
+        }
+        catch (MicrosoftIdentityWebChallengeUserException ex)
+            when (AccountDoesNotExitInTokenCache(ex))
+        {
+            context.RejectPrincipal();
+        }
+    }
+
+    private bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
+    {
+        return ex.InnerException is MsalUiRequiredException
+                                  && (ex.InnerException as MsalUiRequiredException).ErrorCode == MsalError.UserNullError;
+    }
+}
