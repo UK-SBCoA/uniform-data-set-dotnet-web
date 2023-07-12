936c9a632917094eca0ddbe25e4f8282ded3e6a6
mlan225 <17088502+mlan225@users.noreply.github.com>
2023-07-12 15:13:25 -0400

    Removed unused code from auth adjustment
    Commented out code when changing to AD auth, removing commented out code from default identity login

--- src/UDS.Net.Web.MVC/Program.cs
+++ src/UDS.Net.Web.MVC/Program.cs
@@ -19,8 +19,6 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
-
-//------ marvin auth ------
 // Get the scopes from the configuration (appsettings.json)
 var initialScopes = configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
 
@@ -49,57 +47,6 @@ builder.Services.AddControllersWithViews(options =>
 builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
 
-//------ end marvin auth ------
-
-
-
-//// Replace identity service with your preferred provider rather than using this one. Roles required:
-//// Administrator
-//// SuperUser
-//// Examiner
-////var connectionString = configuration.GetConnectionString("AuthServiceConnection");
-
-////builder.Services.AddDbContext<ApplicationDbContext>(options =>
-////    options.UseSqlServer(connectionString));
-
-////builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-////    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-//if (builder.Environment.IsProduction())
-//{
-//    builder.Services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
-//    {
-//        cacheOptions.DatabaseName = configuration["Cosmos:CacheDatabase"];
-//        cacheOptions.ContainerName = configuration["Cosmos:CacheContainer"];
-//        cacheOptions.ClientBuilder = new Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder(configuration.GetConnectionString("CosmosCache"));
-//        cacheOptions.CreateIfNotExists = true;
-//    });
-//}
-
-//builder.Services
-//       .AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureAd")
-//       .EnableTokenAcquisitionToCallDownstreamApi()
-//       .AddMicrosoftGraph(configuration.GetSection("DownstreamApis:UDSNetApi"))
-//       .AddDistributedTokenCaches();
-
-//builder.Services.AddSession(options =>
-//{
-//    options.IdleTimeout = TimeSpan.FromSeconds(3600);
-//    options.Cookie.IsEssential = true;
-//});
-
-// MicrosoftIdentityWebChallengeUserException https://aka.ms/ms-id-web/ca_incremental-consent
-//builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme,
-//        options =>
-//        {
-//            options.AccessDeniedPath = new PathString("/Account/AccessDenied");
-//            options.ReturnUrlParameter = "returnUrl";
-//            if (!builder.Environment.IsProduction())
-//                options.Events = new RejectSessionCookieWhenAccountNotInCacheEvents();
-//        });
-
-//builder.Services.AddHttpContextAccessor();
-
 
 ////*************************************************************************************************
 // Replace API and implemented services with your own if you don't want to use the included API here
@@ -114,72 +61,7 @@ builder.Services.AddSingleton<ILookupService, LookupService>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-//builder.Services.AddRazorPages(); // Enables UDS.Net.Forms razor class library. More here: https://learn.microsoft.com/en-us/aspnet/core/razor-pages/
-
-//var mvcBuilder = builder.Services.AddControllersWithViews();
-//var mvcBuilder = builder.Services.AddControllersWithViews(options =>
-//{
-//    var policy = new AuthorizationPolicyBuilder()
-//        .RequireAuthenticatedUser()
-//        .Build();
-//    options.Filters.Add(new AuthorizeFilter(policy));
-//})
-//.AddMicrosoftIdentityUI();
-
-////*************************************************************************************************
-// Only used during development
-// Runtime compilation of razor assets. Read more here: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/view-compilation?view=aspnetcore-7.0&tabs=visual-studio#enable-runtime-compilation-conditionally
-
-//if (builder.Environment.IsDevelopment() && Debugger.IsAttached)
-//{
-//    mvcBuilder.AddRazorRuntimeCompilation(); // this can't be configured in startup assembly for RCL runtime compilation, it must be here. The fix is available soon in .NET 7 https://github.com/dotnet/aspnetcore/issues/38465
-
-//    // Is also required for runtime compilation of Razor Class Library UDS.Net.Forms
-//    builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
-//    {
-//        var libraryPath = "";
-
-//        if (System.Environment.OSVersion.Platform != PlatformID.Win32NT)
-//        {
-//            libraryPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "UDS.Net.Forms"));
-//        }
-
-//        if (!String.IsNullOrWhiteSpace(libraryPath))
-//        {
-//            options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
-//        }
-//    });
-
-//}
-
-////*************************************************************************************************
-
-// Uncomment to remove enforced authentication
-//builder.Services.AddAuthorization(options =>
-//{
-//    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-//        .RequireAuthenticatedUser()
-//        .Build();
-//});
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//    using (var scope = app.Services.CreateScope())
-//    {
-//        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//        db.Database.Migrate();
-//    }
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // A must for using content from referenced UDS.Net.Forms RCL
@@ -198,34 +80,4 @@ app.UseEndpoints(endpoints =>
         pattern: "{controller=Home}/{action=Index}/{id?}");
 });
 
-
-
 app.Run();
-
-// Workaround for in memory token caching on dev and non-production environments
-// https://github.com/AzureAD/microsoft-identity-web/issues/13#issuecomment-878528492
-//internal class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticationEvents
-//{
-//    public async override Task ValidatePrincipal(CookieValidatePrincipalContext context)
-//    {
-//        try
-//        {
-//            var tokenAcquisition = context.HttpContext.RequestServices.GetRequiredService<ITokenAcquisition>();
-//            var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
-//            var token = await tokenAcquisition.GetAccessTokenForUserAsync(
-//                scopes: config.GetValue<string>("DownstreamApis:GraphApi:Scopes")?.Split(' '),
-//                user: context.Principal);
-//        }
-//        catch (MicrosoftIdentityWebChallengeUserException ex)
-//            when (AccountDoesNotExitInTokenCache(ex))
-//        {
-//            context.RejectPrincipal();
-//        }
-//    }
-
-//    private bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
-//    {
-//        return ex.InnerException is MsalUiRequiredException
-//                                  && (ex.InnerException as MsalUiRequiredException).ErrorCode == MsalError.UserNullError;
-//    }
-//}
