3eb8aac148cf246d35132645e9e900519ebd71af
mlan225 <17088502+mlan225@users.noreply.github.com>
2023-07-12 14:47:33 -0400

    Apply azure AD authentication by following the docs
    Removed the default identity login and replaced it with azure AD auth from the microsoft documentation

--- src/UDS.Net.Web.MVC/Program.cs
+++ src/UDS.Net.Web.MVC/Program.cs
@@ -14,55 +14,91 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web.UI;
 using Microsoft.Identity.Client;
 using Microsoft.Extensions.Caching.Cosmos;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
-// Replace identity service with your preferred provider rather than using this one. Roles required:
-// Administrator
-// SuperUser
-// Examiner
-//var connectionString = configuration.GetConnectionString("AuthServiceConnection");
 
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
 
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<ApplicationDbContext>();
+//------ marvin auth ------
+// Get the scopes from the configuration (appsettings.json)
+var initialScopes = configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
 
-if (builder.Environment.IsProduction())
+
+// Add sign-in with Microsoft
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(configuration.GetSection("AzureAd"))
+
+        // Add the possibility of acquiring a token to call a protected web API
+        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+
+            // Enables controllers and pages to get GraphServiceClient by dependency injection
+            // And use an in memory token cache
+            .AddMicrosoftGraph(configuration.GetSection("DownstreamApi"))
+            .AddInMemoryTokenCaches();
+
+builder.Services.AddControllersWithViews(options =>
 {
-    builder.Services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
-    {
-        cacheOptions.DatabaseName = configuration["Cosmos:CacheDatabase"];
-        cacheOptions.ContainerName = configuration["Cosmos:CacheContainer"];
-        cacheOptions.ClientBuilder = new Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder(configuration.GetConnectionString("CosmosCache"));
-        cacheOptions.CreateIfNotExists = true;
-    });
-}
-
-builder.Services
-       .AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureAd")
-       .EnableTokenAcquisitionToCallDownstreamApi()
-       .AddMicrosoftGraph(configuration.GetSection("DownstreamApis:UDSNetApi"))
-       .AddDistributedTokenCaches();
-
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromSeconds(3600);
-    options.Cookie.IsEssential = true;
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
 });
 
-// MicrosoftIdentityWebChallengeUserException https://aka.ms/ms-id-web/ca_incremental-consent
-builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme,
-        options =>
-        {
-            options.AccessDeniedPath = new PathString("/Account/AccessDenied");
-            options.ReturnUrlParameter = "returnUrl";
-            if (!builder.Environment.IsProduction())
-                options.Events = new RejectSessionCookieWhenAccountNotInCacheEvents();
-        });
+// Enables a UI and controller for sign in and sign out.
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
 
-builder.Services.AddHttpContextAccessor();
+//------ end marvin auth ------
+
+
+
+//// Replace identity service with your preferred provider rather than using this one. Roles required:
+//// Administrator
+//// SuperUser
+//// Examiner
+////var connectionString = configuration.GetConnectionString("AuthServiceConnection");
+
+////builder.Services.AddDbContext<ApplicationDbContext>(options =>
+////    options.UseSqlServer(connectionString));
+
+////builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+////    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+//if (builder.Environment.IsProduction())
+//{
+//    builder.Services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
+//    {
+//        cacheOptions.DatabaseName = configuration["Cosmos:CacheDatabase"];
+//        cacheOptions.ContainerName = configuration["Cosmos:CacheContainer"];
+//        cacheOptions.ClientBuilder = new Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder(configuration.GetConnectionString("CosmosCache"));
+//        cacheOptions.CreateIfNotExists = true;
+//    });
+//}
+
+//builder.Services
+//       .AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureAd")
+//       .EnableTokenAcquisitionToCallDownstreamApi()
+//       .AddMicrosoftGraph(configuration.GetSection("DownstreamApis:UDSNetApi"))
+//       .AddDistributedTokenCaches();
+
+//builder.Services.AddSession(options =>
+//{
+//    options.IdleTimeout = TimeSpan.FromSeconds(3600);
+//    options.Cookie.IsEssential = true;
+//});
+
+// MicrosoftIdentityWebChallengeUserException https://aka.ms/ms-id-web/ca_incremental-consent
+//builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme,
+//        options =>
+//        {
+//            options.AccessDeniedPath = new PathString("/Account/AccessDenied");
+//            options.ReturnUrlParameter = "returnUrl";
+//            if (!builder.Environment.IsProduction())
+//                options.Events = new RejectSessionCookieWhenAccountNotInCacheEvents();
+//        });
+
+//builder.Services.AddHttpContextAccessor();
 
 
 ////*************************************************************************************************
@@ -78,43 +114,43 @@ builder.Services.AddSingleton<ILookupService, LookupService>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddRazorPages(); // Enables UDS.Net.Forms razor class library. More here: https://learn.microsoft.com/en-us/aspnet/core/razor-pages/
+//builder.Services.AddRazorPages(); // Enables UDS.Net.Forms razor class library. More here: https://learn.microsoft.com/en-us/aspnet/core/razor-pages/
 
 //var mvcBuilder = builder.Services.AddControllersWithViews();
-var mvcBuilder = builder.Services.AddControllersWithViews(options =>
-{
-    var policy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-    options.Filters.Add(new AuthorizeFilter(policy));
-})
-.AddMicrosoftIdentityUI();
+//var mvcBuilder = builder.Services.AddControllersWithViews(options =>
+//{
+//    var policy = new AuthorizationPolicyBuilder()
+//        .RequireAuthenticatedUser()
+//        .Build();
+//    options.Filters.Add(new AuthorizeFilter(policy));
+//})
+//.AddMicrosoftIdentityUI();
 
 ////*************************************************************************************************
 // Only used during development
 // Runtime compilation of razor assets. Read more here: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/view-compilation?view=aspnetcore-7.0&tabs=visual-studio#enable-runtime-compilation-conditionally
 
-if (builder.Environment.IsDevelopment() && Debugger.IsAttached)
-{
-    mvcBuilder.AddRazorRuntimeCompilation(); // this can't be configured in startup assembly for RCL runtime compilation, it must be here. The fix is available soon in .NET 7 https://github.com/dotnet/aspnetcore/issues/38465
+//if (builder.Environment.IsDevelopment() && Debugger.IsAttached)
+//{
+//    mvcBuilder.AddRazorRuntimeCompilation(); // this can't be configured in startup assembly for RCL runtime compilation, it must be here. The fix is available soon in .NET 7 https://github.com/dotnet/aspnetcore/issues/38465
 
-    // Is also required for runtime compilation of Razor Class Library UDS.Net.Forms
-    builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
-    {
-        var libraryPath = "";
+//    // Is also required for runtime compilation of Razor Class Library UDS.Net.Forms
+//    builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
+//    {
+//        var libraryPath = "";
 
-        if (System.Environment.OSVersion.Platform != PlatformID.Win32NT)
-        {
-            libraryPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "UDS.Net.Forms"));
-        }
+//        if (System.Environment.OSVersion.Platform != PlatformID.Win32NT)
+//        {
+//            libraryPath = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "UDS.Net.Forms"));
+//        }
 
-        if (!String.IsNullOrWhiteSpace(libraryPath))
-        {
-            options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
-        }
-    });
+//        if (!String.IsNullOrWhiteSpace(libraryPath))
+//        {
+//            options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
+//        }
+//    });
 
-}
+//}
 
 ////*************************************************************************************************
 
@@ -168,28 +204,28 @@ app.Run();
 
 // Workaround for in memory token caching on dev and non-production environments
 // https://github.com/AzureAD/microsoft-identity-web/issues/13#issuecomment-878528492
-internal class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticationEvents
-{
-    public async override Task ValidatePrincipal(CookieValidatePrincipalContext context)
-    {
-        try
-        {
-            var tokenAcquisition = context.HttpContext.RequestServices.GetRequiredService<ITokenAcquisition>();
-            var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
-            var token = await tokenAcquisition.GetAccessTokenForUserAsync(
-                scopes: config.GetValue<string>("DownstreamApis:GraphApi:Scopes")?.Split(' '),
-                user: context.Principal);
-        }
-        catch (MicrosoftIdentityWebChallengeUserException ex)
-            when (AccountDoesNotExitInTokenCache(ex))
-        {
-            context.RejectPrincipal();
-        }
-    }
+//internal class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticationEvents
+//{
+//    public async override Task ValidatePrincipal(CookieValidatePrincipalContext context)
+//    {
+//        try
+//        {
+//            var tokenAcquisition = context.HttpContext.RequestServices.GetRequiredService<ITokenAcquisition>();
+//            var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+//            var token = await tokenAcquisition.GetAccessTokenForUserAsync(
+//                scopes: config.GetValue<string>("DownstreamApis:GraphApi:Scopes")?.Split(' '),
+//                user: context.Principal);
+//        }
+//        catch (MicrosoftIdentityWebChallengeUserException ex)
+//            when (AccountDoesNotExitInTokenCache(ex))
+//        {
+//            context.RejectPrincipal();
+//        }
+//    }
 
-    private bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
-    {
-        return ex.InnerException is MsalUiRequiredException
-                                  && (ex.InnerException as MsalUiRequiredException).ErrorCode == MsalError.UserNullError;
-    }
-}
+//    private bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
+//    {
+//        return ex.InnerException is MsalUiRequiredException
+//                                  && (ex.InnerException as MsalUiRequiredException).ErrorCode == MsalError.UserNullError;
+//    }
+//}
