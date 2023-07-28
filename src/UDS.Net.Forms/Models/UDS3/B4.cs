97caec95997cbd728d1fad565d5b98852ca8d14b
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-07-28 14:40:45 -0400

    Add completion validation for B4

--- src/UDS.Net.Forms/Models/UDS3/B4.cs
+++ src/UDS.Net.Forms/Models/UDS3/B4.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using UDS.Net.Forms.DataAnnotations;
 
 namespace UDS.Net.Forms.Models.UDS3
 {
@@ -9,34 +10,44 @@ namespace UDS.Net.Forms.Models.UDS3
     public class B4 : FormModel
     {
         [Display(Name = "Memory")]
+        [RequiredOnComplete]
         public double? MEMORY { get; set; }
 
         [Display(Name = "Orientation")]
+        [RequiredOnComplete]
         public double? ORIENT { get; set; }
 
         [Display(Name = "Judgment and problem solving")]
+        [RequiredOnComplete]
         public double? JUDGMENT { get; set; }
 
         [Display(Name = "Community affairs")]
+        [RequiredOnComplete]
         public double? COMMUN { get; set; }
 
         [Display(Name = "Homes and hobbies")]
+        [RequiredOnComplete]
         public double? HOMEHOBB { get; set; }
 
         [Display(Name = "Personal care")]
+        [RequiredOnComplete]
         public double? PERSCARE { get; set; }
 
         [Display(Name = "CDR sum of boxes")]
         [RegularExpression("^(0(.5)?|[1-9](.5)?|1[0-5](.5)?|1[6-8])$", ErrorMessage = "0,18 (except scores of 16.5 and 17.5 not possible)")]
+        [RequiredOnComplete]
         public double? CDRSUM { get; set; }
 
         [Display(Name = "Global CDR")]
+        [RequiredOnComplete]
         public double? CDRGLOB { get; set; }
 
         [Display(Name = "Behavior, comportment, and personality")]
+        [RequiredOnComplete]
         public double? COMPORT { get; set; }
 
         [Display(Name = "Language")]
+        [RequiredOnComplete]
         public double? CDRLANG { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
