d3ae3d4031847a394e446382d4d4884b65befbbd
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-07-28 15:46:16 -0400

    Add B8 completion validation

--- src/UDS.Net.Forms/Pages/UDS3/B8.cshtml.cs
+++ src/UDS.Net.Forms/Pages/UDS3/B8.cshtml.cs
@@ -38,6 +38,167 @@ namespace UDS.Net.Forms.Pages.UDS3
             new RadioListItem("Not assessed", "8")
         };
 
+        public Dictionary<string, UIBehavior> NORMEXAMUIBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("B8.PARKSIGN"),
+                    new UIDisableAttribute("B8.CVDSIGNS"),
+                    new UIDisableAttribute("B8.POSTCORT"),
+                    new UIDisableAttribute("B8.PSPCBS"),
+                    new UIDisableAttribute("B8.ALSFIND"),
+                    new UIDisableAttribute("B8.GAITNPH"),
+                    new UIDisableAttribute("B8.OTHNEUR")
+                }
+            } },
+            { "1", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("B8.PARKSIGN"),
+                    new UIEnableAttribute("B8.CVDSIGNS"),
+                    new UIEnableAttribute("B8.POSTCORT"),
+                    new UIEnableAttribute("B8.PSPCBS"),
+                    new UIEnableAttribute("B8.ALSFIND"),
+                    new UIEnableAttribute("B8.GAITNPH"),
+                    new UIEnableAttribute("B8.OTHNEUR")
+                }
+            } },
+            { "2", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("B8.PARKSIGN"),
+                    new UIDisableAttribute("B8.CVDSIGNS"),
+                    new UIDisableAttribute("B8.POSTCORT"),
+                    new UIDisableAttribute("B8.PSPCBS"),
+                    new UIDisableAttribute("B8.ALSFIND"),
+                    new UIDisableAttribute("B8.GAITNPH"),
+                    new UIEnableAttribute("B8.OTHNEUR"),
+                    new UIPropertyAttributes("B8.OTHNEUR", new Dictionary<string, string>
+                    {
+                        { "val", "1" }
+                    })
+                }
+            } }
+        };
+
+        public Dictionary<string, UIBehavior> PARKSIGNUIBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("B8.RESTTRL"),
+                    new UIDisableAttribute("B8.RESTTRR"),
+                    new UIDisableAttribute("B8.RESTTRL"),
+                    new UIDisableAttribute("B8.SLOWINGL"),
+                    new UIDisableAttribute("B8.SLOWINGR"),
+                    new UIDisableAttribute("B8.RIGIDL"),
+                    new UIDisableAttribute("B8.RIGIDR"),
+                    new UIDisableAttribute("B8.BRADY"),
+                    new UIDisableAttribute("B8.PARKGAIT"),
+                    new UIDisableAttribute("B8.POSTINST")
+                }
+            } },
+            { "1", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("B8.RESTTRL"),
+                    new UIEnableAttribute("B8.RESTTRR"),
+                    new UIEnableAttribute("B8.RESTTRL"),
+                    new UIEnableAttribute("B8.SLOWINGL"),
+                    new UIEnableAttribute("B8.SLOWINGR"),
+                    new UIEnableAttribute("B8.RIGIDL"),
+                    new UIEnableAttribute("B8.RIGIDR"),
+                    new UIEnableAttribute("B8.BRADY"),
+                    new UIEnableAttribute("B8.PARKGAIT"),
+                    new UIEnableAttribute("B8.POSTINST")
+                }
+            } }
+        };
+
+        public Dictionary<string, UIBehavior> CVDSIGNSUIBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("B8.CORTDEF"),
+                    new UIDisableAttribute("B8.SIVDFIND"),
+                    new UIDisableAttribute("B8.CVDMOTL"),
+                    new UIDisableAttribute("B8.CVDMOTR"),
+                    new UIDisableAttribute("B8.CORTVISL"),
+                    new UIDisableAttribute("B8.CORTVISR"),
+                    new UIDisableAttribute("B8.SOMATL"),
+                    new UIDisableAttribute("B8.SOMATR")
+                }
+            } },
+            { "1", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("B8.CORTDEF"),
+                    new UIEnableAttribute("B8.SIVDFIND"),
+                    new UIEnableAttribute("B8.CVDMOTL"),
+                    new UIEnableAttribute("B8.CVDMOTR"),
+                    new UIEnableAttribute("B8.CORTVISL"),
+                    new UIEnableAttribute("B8.CORTVISR"),
+                    new UIEnableAttribute("B8.SOMATL"),
+                    new UIEnableAttribute("B8.SOMATR")
+                }
+            } }
+        };
+        public Dictionary<string, UIBehavior> PSPCBSUIBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("B8.EYEPSP"),
+                    new UIDisableAttribute("B8.DYSPSP"),
+                    new UIDisableAttribute("B8.AXIALPSP"),
+                    new UIDisableAttribute("B8.GAITPSP"),
+                    new UIDisableAttribute("B8.APRAXSP"),
+                    new UIDisableAttribute("B8.APRAXL"),
+                    new UIDisableAttribute("B8.APRAXR"),
+                    new UIDisableAttribute("B8.CORTSENL"),
+                    new UIDisableAttribute("B8.CORTSENR"),
+                    new UIDisableAttribute("B8.ATAXL"),
+                    new UIDisableAttribute("B8.ATAXR"),
+                    new UIDisableAttribute("B8.ALIENLML"),
+                    new UIDisableAttribute("B8.ALIENLMR"),
+                    new UIDisableAttribute("B8.DYSTONL"),
+                    new UIDisableAttribute("B8.DYSTONR"),
+                    new UIDisableAttribute("B8.MYOCLLT"),
+                    new UIDisableAttribute("B8.MYOCLRT")
+                }
+            } },
+            { "1", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("B8.EYEPSP"),
+                    new UIEnableAttribute("B8.DYSPSP"),
+                    new UIEnableAttribute("B8.AXIALPSP"),
+                    new UIEnableAttribute("B8.GAITPSP"),
+                    new UIEnableAttribute("B8.APRAXSP"),
+                    new UIEnableAttribute("B8.APRAXL"),
+                    new UIEnableAttribute("B8.APRAXR"),
+                    new UIEnableAttribute("B8.CORTSENL"),
+                    new UIEnableAttribute("B8.CORTSENR"),
+                    new UIEnableAttribute("B8.ATAXL"),
+                    new UIEnableAttribute("B8.ATAXR"),
+                    new UIEnableAttribute("B8.ALIENLML"),
+                    new UIEnableAttribute("B8.ALIENLMR"),
+                    new UIEnableAttribute("B8.DYSTONL"),
+                    new UIEnableAttribute("B8.DYSTONR"),
+                    new UIEnableAttribute("B8.MYOCLLT"),
+                    new UIEnableAttribute("B8.MYOCLRT")
+                }
+            } }
+        };
+
+        public Dictionary<string, UIBehavior> OTHNEURUIBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0", new UIBehavior { PropertyAttribute = new UIDisableAttribute("B8.OTHNEURX")} },
+            { "1", new UIBehavior { PropertyAttribute = new UIEnableAttribute("B8.OTHNEURX")} }
+        };
+
         public B8Model(IVisitService visitService) : base(visitService, "B8")
         {
         }
