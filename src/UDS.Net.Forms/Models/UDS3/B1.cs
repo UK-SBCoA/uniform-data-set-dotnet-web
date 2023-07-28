6ba380e14b85f157eeecc2de2e8b652925f90cdf
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-07-28 14:34:13 -0400

    Add completion validation and ui behaviors for B1

--- src/UDS.Net.Forms/Models/UDS3/B1.cs
+++ src/UDS.Net.Forms/Models/UDS3/B1.cs
@@ -11,28 +11,35 @@ namespace UDS.Net.Forms.Models.UDS3
     {
         [Display(Name = "Participant height (inches)")]
         [RegularExpression("^(3[6-9].\\d|[4-7][0-9].\\d|8[0-7].\\d|88.8)$", ErrorMessage = "(36.0-87.9, 88.8 = not assessed)")]
+        [RequiredOnComplete]
         public double? HEIGHT { get; set; }
 
         [Display(Name = "Participant weight (lbs)")]
         [RegularExpression("^(5\\d|[6-9]\\d|[1-3]\\d{2}|400|888)$", ErrorMessage = "(50-400, 888 = Not assessed)")]
+        [RequiredOnComplete]
         public int? WEIGHT { get; set; }
 
         [Display(Name = "Systolic (sitting)")]
         [RegularExpression("^(7\\d|[89]\\d|1\\d{2}|2[0-2]\\d|230|777|888)$", ErrorMessage = "(70-230, 777 = BP Addendum submitted, 888 = Not assessed)")]
+        [RequiredOnComplete]
         public int? BPSYS { get; set; }
 
         [Display(Name = "Diastolic (sitting)")]
         [RegularExpression("^(3\\d|[4-9]\\d|1[0-3]\\d|140|777|888)$", ErrorMessage = "(30-140, 777 = BP Addendum submitted, 888 = Not assessed)")]
+        [RequiredOnComplete]
         public int? BPDIAS { get; set; }
 
         [Display(Name = "Participant resting heart rate (pulse)")]
         [RegularExpression("^(3[3-9]|[4-9]\\d|1[0-5]\\d|160|888)$", ErrorMessage = "(33-160, 888 = Not assessed)")]
+        [RequiredOnComplete]
         public int? HRATE { get; set; }
 
         [Display(Name = "Without corrective lenses, is the participant’s vision functionally normal?")]
+        [RequiredOnComplete]
         public int? VISION { get; set; }
 
         [Display(Name = "Does the participant usually wear corrective lenses?")]
+        [RequiredOnComplete]
         public int? VISCORR { get; set; }
 
         [Display(Name = "If yes, is the participant’s vision functionally normal with corrective lenses?")]
@@ -40,9 +47,11 @@ namespace UDS.Net.Forms.Models.UDS3
         public int? VISWCORR { get; set; }
 
         [Display(Name = "Without a hearing aid(s), is the participant’s hearing functionally normal?")]
+        [RequiredOnComplete]
         public int? HEARING { get; set; }
 
         [Display(Name = "Does the participant usually wear a hearing aid(s)?")]
+        [RequiredOnComplete]
         public int? HEARAID { get; set; }
 
         [Display(Name = "If yes, is the participant’s hearing functionally normal with a hearing aid(s)?")]
