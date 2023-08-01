6cfff6de5d1154b566fffe7cf8fc25be5c6f8885
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-08-01 13:53:03 -0400

    Add range validation along with client-side functionality

---
+++ src/UDS.Net.Forms/DataAnnotations/RequiredIfRangeAttribute.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using UDS.Net.Forms.Models;
+using UDS.Net.Services.Enums;
+
+namespace UDS.Net.Forms.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class RequiredIfRangeAttribute : ValidationAttribute, IClientModelValidator
+    {
+        private string _watchedField = "";
+        private int _watchedFieldLowValue = int.MinValue;
+        private int _watchedFieldHighValue = int.MaxValue;
+
+        public RequiredIfRangeAttribute(string watchedField, int lowValue, int highValue) : base()
+        {
+            _watchedField = watchedField;
+            if (lowValue > highValue)
+                throw new Exception("Range validation error");
+
+            _watchedFieldLowValue = lowValue;
+            _watchedFieldHighValue = highValue;
+
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            // does the form have a status and is it attempting to be finalized?
+            if (validationContext.ObjectType.IsSubclassOf(typeof(FormModel)))
+            {
+                var form = (FormModel)validationContext.ObjectInstance;
+
+                // only validate if the form is attempting to be completed
+                if (form.Status == FormStatus.Complete)
+                {
+                    // get the watched property and compare
+                    var type = validationContext.ObjectType;
+
+                    if (type != null)
+                    {
+                        var watchedProperty = type.GetProperty(_watchedField);
+
+                        if (watchedProperty != null)
+                        {
+                            var currentValue = watchedProperty.GetValue(validationContext.ObjectInstance, null);
+                            if (currentValue != null && Int32.TryParse(currentValue.ToString(), out int integerValue))
+                            {
+                                if (integerValue >= _watchedFieldLowValue && integerValue <= _watchedFieldHighValue)
+                                {
+                                    // if the watched field's value is within the range then this field requires a value
+                                    if (value == null)
+                                        return new ValidationResult(this.ErrorMessage);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            MergeAttribute(context.Attributes, "data-val", "true");
+            MergeAttribute(context.Attributes, "data-val-requiredifrange", this.ErrorMessage);
+            string watched = _watchedField;
+            var containerName = context.ModelMetadata.ContainerType.Name;
+            if (!String.IsNullOrEmpty(containerName))
+            {
+                watched = containerName + "." + _watchedField;
+            }
+            MergeAttribute(context.Attributes, "data-val-requiredifrange-watchedfield", watched);
+            MergeAttribute(context.Attributes, "data-val-requiredifrange-lowvalue", _watchedFieldLowValue.ToString());
+            MergeAttribute(context.Attributes, "data-val-requiredifrange-highvalue", _watchedFieldHighValue.ToString());
+        }
+
+        private static bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+
+            attributes.Add(key, value);
+            return true;
+        }
+    }
+}
+
--- src/UDS.Net.Forms/wwwroot/js/jquery.unobtrusive.custom_validation.js
+++ src/UDS.Net.Forms/wwwroot/js/jquery.unobtrusive.custom_validation.js
@@ -240,8 +240,6 @@ $.validator.unobtrusive.adapters.add('birthmonth', ['maximum', 'minimum', 'allow
     let maximum = parseInt(options.params.maximum);
     let allowUnknown = options.params.allowunknown; // TODO parse bool
 
-    console.log(allowUnknown);
-
     options.rules.birthmonth = [element, { allowUnknown, maximum, minimum }];
 
     options.messages.birthmonth = options.message;
@@ -282,7 +280,6 @@ $.validator.unobtrusive.adapters.add('birthyear', ['allowunknown', 'maximum', 'm
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* Diagnosis */
 $.validator.addMethod('diagnosis', function (value, element, params) {
-  console.log(element);
   let codes = [40, 41, 42, 43, 44, 45, 50, 70, 80, 100, 110, 120, 130, 131, 132, 133, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 310, 320, 400, 410, 420, 421, 430, 431, 432, 433, 434, 435, 436, 439, 440, 450, 490, 999];
   if (codes.includes(value)) {
     return true;
@@ -320,7 +317,9 @@ $.validator.addMethod('requiredif', function (value, element, params) {
     let selected = watched.val();
     let watchedFieldIsRequiredValue = parameters.watchedfieldvalue;
     if (selected == watchedFieldIsRequiredValue) {
-      return false;
+      if (value === '') {
+        return false;
+      }
     }
   }
 
@@ -349,3 +348,61 @@ $.validator.unobtrusive.adapters.add('requiredif', ['watchedfield', 'watchedfiel
     options.messages.requiredif = options.message;
   });
 
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* RequiredIfRange */
+$.validator.addMethod('requiredifrange', function (value, element, params) {
+  let parameters = params[1];
+  let watchedFieldName = parameters.watchedfield;
+
+  let watched = $('input[name="' + watchedFieldName + '"]');
+  if (watched.length) {
+    let watchedValue = watched.val();
+    if (watchedValue.length) {
+      let watchedInt = parseInt(watchedValue);
+      let lowInt = parseInt(parameters.lowvalue);
+      let highInt = parseInt(parameters.highvalue);
+
+      if (watchedInt >= lowInt && watchedInt <= highInt) {
+        // if within range field is required
+        if (value === '') {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+});
+
+$.validator.unobtrusive.adapters.add('requiredifrange', ['watchedfield', 'lowvalue', 'highvalue'],
+  function (options) {
+    let watchedFieldName = options.params.watchedfield;
+    let watched = $('input[name="' + watchedFieldName + '"]');
+    if (watched.length) {
+      watched.on('change', function () {
+        // clear the validation if the watched field is changed to outside the range
+        let watchedValue = watched.val();
+        if (watchedValue.length) {
+          let watchedInt = parseInt(watchedValue);
+          let lowInt = parseInt(options.params.lowvalue);
+          let highInt = parseInt(options.params.highvalue);
+          if (watchedInt <= lowInt || watchedInt >= highInt) {
+            // outside the range, reset everything
+
+            let element = $(options.element);
+            if (element.length) {
+              // reset css
+              element.removeClass('input-validation-error');
+              element.addClass('block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm placeholder:text-gray-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none');
+              // reset error messages
+              let validator = $('#UDSForm').validate();
+              validator.form();
+            }
+          }
+        }
+      });
+    }
+    options.rules.requiredifrange = [options.element, options.params]; // rules are required for the onChange event to trigger validation
+    options.messages.requiredifrange = options.message;
+  });
