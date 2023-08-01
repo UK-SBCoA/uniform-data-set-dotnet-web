9b5daf4ffbe2e16bb66519bb20faf24f3bc77888
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-08-01 13:53:13 -0400

    In-progress validation

--- src/UDS.Net.Forms/Models/UDS3/C2.cs
+++ src/UDS.Net.Forms/Models/UDS3/C2.cs
@@ -11,11 +11,12 @@ namespace UDS.Net.Forms.Models.UDS3
     public class C2 : FormModel
     {
         [Display(Name = "Was any part of MoCA administered?")]
+        [RequiredOnComplete]
         public int? MOCACOMP { get; set; }
 
-        [Display(Name = "No (If No, enter reason code, 95–98)")]
+        [Display(Name = "If No, enter reason code, 95–98")]
         [Range(95, 98)]
-        [RequiredIf(nameof(MOCACOMP), "1", ErrorMessage = "Provide reason code")]
+        [RequiredIf(nameof(MOCACOMP), "0", ErrorMessage = "Provide reason code")]
         public int? MOCAREAS { get; set; }
 
         [Display(Name = "MoCA was administered")]
@@ -36,102 +37,104 @@ namespace UDS.Net.Forms.Models.UDS3
         [Display(Name = "Subject was unable to complete one or more sections due to hearing impairment")]
         public int? MOCAHEAR { get; set; }
 
-        [Display(Name = "TOTAL RAW SCORE - UNCORRECTED (Not corrected for education or visual/hearing impairment)")]
+        [Display(Name = "TOTAL RAW SCORE - UNCORRECTED (Not corrected for education or visual/hearing impairment)", Description = "enter 88 if any of the following MoCA items were not administered: 1g-1l, 1n-1t, 1w-1bb")]
         [RegularExpression("^(\\d|[0-2]\\d|30|88)$", ErrorMessage = "(0-30 or 88 = not administered)")]
         public int? MOCATOTS { get; set; }
 
-        [Display(Name = "Visuospatial/executive — Trails")]
+        [Display(Name = "Visuospatial/executive — Trails", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCATRAI { get; set; }
 
-        [Display(Name = " Visuospatial/executive — Cube")]
+        [Display(Name = " Visuospatial/executive — Cube", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCACUBE { get; set; }
 
-        [Display(Name = "Visuospatial/executive — Clock contour")]
+        [Display(Name = "Visuospatial/executive — Clock contour", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCACLOC { get; set; }
 
-        [Display(Name = "Visuospatial/executive — Clock numbers")]
+        [Display(Name = "Visuospatial/executive — Clock numbers", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCACLON { get; set; }
 
-        [Display(Name = "Visuospatial/executive — Clock hands")]
+        [Display(Name = "Visuospatial/executive — Clock hands", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCACLOH { get; set; }
 
-        [Display(Name = "Language — Naming")]
+        [Display(Name = "Language — Naming", Description = "(0-3, 95-98)")]
         [RegularExpression("^([0-3]|9[5-8])$", ErrorMessage = "(0-3, 95-98)")]
         public int? MOCANAMI { get; set; }
 
-        [Display(Name = "Memory — Registration (two trials)")]
+        [Display(Name = "Memory — Registration (two trials)", Description = "(0-10, 95-98)")]
         [RegularExpression("^(\\d|10|9[5-8])$", ErrorMessage = "(0-10, 95-98)")]
         public int? MOCAREGI { get; set; }
 
-        [Display(Name = "Attention — Digits")]
+        [Display(Name = "Attention — Digits", Description = "(0-2, 95-98)")]
         [RegularExpression("^([0-2]|9[5-8])$", ErrorMessage = "(0-2, 95-98)")]
         public int? MOCADIGI { get; set; }
 
-        [Display(Name = "Attention — Letter A")]
+        [Display(Name = "Attention — Letter A", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCALETT { get; set; }
 
-        [Display(Name = "Attention — Serial 7s")]
+        [Display(Name = "Attention — Serial 7s", Description = "(0-3, 95-98)")]
         [RegularExpression("^([0-3]|9[5-8])$", ErrorMessage = "(0-3, 95-98)")]
         public int? MOCASER7 { get; set; }
 
-        [Display(Name = "Language — Repetition")]
+        [Display(Name = "Language — Repetition", Description = "(0-2, 95-98)")]
         [RegularExpression("^([0-2]|9[5-8])$", ErrorMessage = "(0-2, 95-98)")]
         public int? MOCAREPE { get; set; }
 
-        [Display(Name = "Language — Fluency")]
+        [Display(Name = "Language — Fluency", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAFLUE { get; set; }
 
-        [Display(Name = "Abstraction")]
+        [Display(Name = "Abstraction", Description = "(0-2, 95-98)")]
         [RegularExpression("^([0-2]|9[5-8])$", ErrorMessage = "(0-2, 95-98)")]
         public int? MOCAABST { get; set; }
 
-        [Display(Name = "Delayed recall — No cue")]
+        [Display(Name = "Delayed recall — No cue", Description = "(0-5, 95-98)")]
         [RegularExpression("^([0-5]|9[5-8])$", ErrorMessage = "(0-5, 95-98)")]
         public int? MOCARECN { get; set; }
 
-        [Display(Name = "Delayed recall — Category cue")]
+        [Display(Name = "Delayed recall — Category cue", Description = "(0-5, 88 = not applicable)")]
         [RegularExpression("^([0-5]|88)$", ErrorMessage = "(0-5, 88 = not applicable)")]
         public int? MOCARECC { get; set; }
 
-        [Display(Name = "Delayed recall — Recognition")]
+        [Display(Name = "Delayed recall — Recognition", Description = "(0-5, 88 = not applicable)")]
         [RegularExpression("^([0-5]|88)$", ErrorMessage = "(0-5, 88 = not applicable)")]
         public int? MOCARECR { get; set; }
 
-        [Display(Name = "Orientation — Date")]
+        [Display(Name = "Orientation — Date", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAORDT { get; set; }
 
-        [Display(Name = "Orientation — Month")]
+        [Display(Name = "Orientation — Month", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAORMO { get; set; }
 
-        [Display(Name = "Orientation — Year")]
+        [Display(Name = "Orientation — Year", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAORYR { get; set; }
 
-        [Display(Name = "Orientation — Day")]
+        [Display(Name = "Orientation — Day", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAORDY { get; set; }
 
-        [Display(Name = "Orientation — Place")]
+        [Display(Name = "Orientation — Place", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAORPL { get; set; }
 
-        [Display(Name = "Orientation — City")]
+        [Display(Name = "Orientation — City", Description = "(0-1, 95-98)")]
         [RegularExpression("^([0-1]|9[5-8])$", ErrorMessage = "(0-1, 95-98)")]
         public int? MOCAORCT { get; set; }
 
         [Display(Name = "The tests following the MoCA were administered")]
+        [RequiredOnComplete]
         public int? NPSYCLOC { get; set; }
 
         [Display(Name = "Language of test administration")]
+        [RequiredOnComplete]
         public int? NPSYLAN { get; set; }
 
         [Display(Name = "Other (specify)")]
@@ -140,149 +143,171 @@ namespace UDS.Net.Forms.Models.UDS3
         [SpecialCharacter]
         public string? NPSYLANX { get; set; }
 
-        [Display(Name = "Total story units recalled, verbatim scoring")]
+        [Display(Name = "Total story units recalled, verbatim scoring", Description = "(0-44, 95-98)")]
         [RegularExpression("^([0-9]|[1-3][0-9]|4[0-4]|9[5-8])$", ErrorMessage = "(0-44, 95-98)")]
+        [RequiredOnComplete]
         public int? CRAFTVRS { get; set; }
 
-        [Display(Name = "Total story units recalled, paraphrase scoring")]
+        [Display(Name = "Total story units recalled, paraphrase scoring", Description = "(0-25)")]
         [Range(0, 25, ErrorMessage = "(0-25)")]
         public int? CRAFTURS { get; set; }
 
-        [Display(Name = "Total Score for copy of Benson figure")]
+        [Display(Name = "Total Score for copy of Benson figure", Description = "(0-17, 95-98)")]
         [RegularExpression("^(\\d|1[0-7]|9[5-8])$", ErrorMessage = "(0-17, 95-98)")]
+        [RequiredOnComplete]
         public int? UDSBENTC { get; set; }
 
-        [Display(Name = "Number of correct trials")]
+        [Display(Name = "Number of correct trials", Description = "(0-14, 95-98)")]
         [RegularExpression("^(\\d|1[0-4]|9[5-8])$", ErrorMessage = "(0-14, 95-98)")]
+        [RequiredOnComplete]
         public int? DIGFORCT { get; set; }
 
-        [Display(Name = "Longest span forward")]
+        [Display(Name = "Longest span forward", Description = "(0, 3-9)")]
         [RegularExpression("^(0|[3-9])$", ErrorMessage = "(0, 3-9)")]
         public int? DIGFORSL { get; set; }
 
-        [Display(Name = "Number of correct trials")]
+        [Display(Name = "Number of correct trials", Description = "(0-14, 95-98)")]
         [RegularExpression("^(\\d|1[0-4]|9[5-8])$", ErrorMessage = "(0-14, 95-98)")]
+        [RequiredOnComplete]
         public int? DIGBACCT { get; set; }
 
-        [Display(Name = "Longest span backward")]
+        [Display(Name = "Longest span backward", Description = "(0, 2-8)")]
         [RegularExpression("^(0|[2-8])$", ErrorMessage = "(0, 2-8)")]
         public int? DIGBACLS { get; set; }
 
-        [Display(Name = "Animals: Total number of animals named in 60 seconds")]
+        [Display(Name = "Animals: Total number of animals named in 60 seconds", Description = "(0-77, 95-98)")]
         [RegularExpression("^(\\d|[1-6]\\d|7[0-7]|9[5-8])$", ErrorMessage = "(0-77, 95-98)")]
+        [RequiredOnComplete]
         public int? ANIMALS { get; set; }
 
-        [Display(Name = "Vegetables: Total number of vegtables named in 60 seconds")]
+        [Display(Name = "Vegetables: Total number of vegtables named in 60 seconds", Description = "(0-77, 95-98)")]
         [RegularExpression("^(\\d|[1-6]\\d|7[0-7]|9[5-8])$", ErrorMessage = "(0-77, 95-98)")]
+        [RequiredOnComplete]
         public int? VEG { get; set; }
 
-        [Display(Name = "Part A: Total number of seconds to complete")]
+        [Display(Name = "Part A: Total number of seconds to complete", Description = "(0-150, 995-998)")]
         [RegularExpression("^(\\d|[1-9]\\d|1[0-4]\\d|150|99[5-8])$", ErrorMessage = "(0-150, 995-998)")]
+        [RequiredOnComplete]
         public int? TRAILA { get; set; }
 
-        [Display(Name = "Number of commission errors")]
+        [Display(Name = "Number of commission errors", Description = "(0-40)")]
         [Range(0, 40, ErrorMessage = "(0-40)")]
         public int? TRAILARR { get; set; }
 
-        [Display(Name = "Number of correct lines")]
+        [Display(Name = "Number of correct lines", Description = "(0-24)")]
         [Range(0, 24, ErrorMessage = "(0-24)")]
         public int? TRAILALI { get; set; }
 
-        [Display(Name = "Part B: Total number of seconds to complete")]
+        [Display(Name = "Part B: Total number of seconds to complete", Description = "(0-300, 995-998)")]
         [RegularExpression("^(\\d|[1-9]\\d|[12]\\d{2}|300|99[5-8])$", ErrorMessage = "(0-300, 995-998)")]
         public int? TRAILB { get; set; }
 
-        [Display(Name = "Number of commission errors")]
+        [Display(Name = "Number of commission errors", Description = "(0-40)")]
         [Range(0, 40, ErrorMessage = "(0-40)")]
         public int? TRAILBRR { get; set; }
 
-        [Display(Name = "Number of correct lines")]
+        [Display(Name = "Number of correct lines", Description = "(0-24)")]
         [Range(0, 24, ErrorMessage = "(0-24)")]
         public int? TRAILBLI { get; set; }
 
-        [Display(Name = "Total story units recalled, verbatim scoring")]
+        [Display(Name = "Total story units recalled, verbatim scoring", Description = "(0-44, 95-98)")]
         [RegularExpression("^(\\d|[1-3]\\d|4[0-4]|9[5-8])$", ErrorMessage = "(0-44, 95-98)")]
+        [RequiredOnComplete]
         public int? CRAFTDVR { get; set; }
 
-        [Display(Name = "Total story units recalled, paraphrase scoring")]
+        [Display(Name = "Total story units recalled, paraphrase scoring", Description = "(0-24)")]
         [Range(0, 25, ErrorMessage = "(0-24)")]
         public int? CRAFTDRE { get; set; }
 
-        [Display(Name = "Delay time (minutes)")]
+        [Display(Name = "Delay time (minutes)", Description = "(0-85 minutes, 99 = unknown)")]
         [RegularExpression("^(\\d|[1-7]\\d|8[0-5]|99)$", ErrorMessage = "(0-85 minutes, 99 = unknown)")]
         public int? CRAFTDTI { get; set; }
 
         [Display(Name = "Cue (\"boy\") needed")]
         public int? CRAFTCUE { get; set; }
 
-        [Display(Name = "Total score for drawing of Benson figure following 10- to 15-minuted delay")]
+        [Display(Name = "Total score for drawing of Benson figure following 10- to 15-minuted delay", Description = "(0-17, 95-98)")]
         [RegularExpression("^(\\d|1[0-7]|9[5-8])$", ErrorMessage = "(0-17, 95-98)")]
+        [RequiredOnComplete]
         public int? UDSBENTD { get; set; }
 
         [Display(Name = "Recognized original stimulus among four options?")]
         public int? UDSBENRS { get; set; }
 
-        [Display(Name = "Total score")]
+        [Display(Name = "Total score", Description = "(0-32, 95-98)")]
         [RegularExpression("^(\\d|[12]\\d|3[0-2]|9[5-8])$", ErrorMessage = "(0-32, 95-98)")]
+        [RequiredOnComplete]
         public int? MINTTOTS { get; set; }
 
-        [Display(Name = "Total correct without semantic cue")]
+        [Display(Name = "Total correct without semantic cue", Description = "(0-32)")]
         [Range(0, 32, ErrorMessage = "(0-32)")]
         public int? MINTTOTW { get; set; }
 
-        [Display(Name = "Semantic cues: Number given")]
+        [Display(Name = "Semantic cues: Number given", Description = "(0-32)")]
         [Range(0, 32, ErrorMessage = "(0-32)")]
         public int? MINTSCNG { get; set; }
 
-        [Display(Name = "Semantic cues: Number correct with cue")]
+        [Display(Name = "Semantic cues: Number correct with cue", Description = "(0-32, 88 = not applicable)")]
         [RegularExpression("^(\\d|[12]\\d|3[0-2]|88)$", ErrorMessage = "(0-32, 88)")]
         public int? MINTSCNC { get; set; }
 
-        [Display(Name = "Phonemic cues: Number given")]
+        [Display(Name = "Phonemic cues: Number given", Description = "(0-32)")]
         [Range(0, 32, ErrorMessage = "(0-32)")]
         public int? MINTPCNG { get; set; }
 
-        [Display(Name = "Phonemic cues: Number correct with cue")]
+        [Display(Name = "Phonemic cues: Number correct with cue", Description = "(0-32, 88 = not applicable)")]
         [RegularExpression("^(\\d|[12]\\d|3[0-2]|88)$", ErrorMessage = "(0-32, 88)")]
         public int? MINTPCNC { get; set; }
 
-        [Display(Name = "Number of correct F-words generated in 1 minute")]
+        [Display(Name = "Number of correct F-words generated in 1 minute", Description = "(0-40, 95-98)")]
         [RegularExpression("^(\\d|[1-3]\\d|40|9[5-8])$", ErrorMessage = "(0-40, 95-98)")]
+        [RequiredOnComplete]
         public int? UDSVERFC { get; set; }
 
-        [Display(Name = "Number of correct F-words repeated in 1 minute")]
+        [Display(Name = "Number of correct F-words repeated in 1 minute", Description = "(0-15)")]
         [Range(0, 15, ErrorMessage = "(0-15)")]
         public int? UDSVERFN { get; set; }
 
-        [Display(Name = "Number of non-F-words and rule violation errors in 1 minute")]
+        [Display(Name = "Number of non-F-words and rule violation errors in 1 minute", Description = "(0-15)")]
         [Range(0, 15, ErrorMessage = "(0-15)")]
         public int? UDSVERNF { get; set; }
 
-        [Display(Name = "Number of correct L-words generated in 1 minute")]
+        #region If not completed, skip to question 13a
+
+        [Display(Name = "Number of correct L-words generated in 1 minute", Description = "(0-40, 95-98)")]
         [RegularExpression("^(\\d|[1-3]\\d|40|9[5-8])$", ErrorMessage = "(0-40, 95-98)")]
+        [RequiredOnComplete]
         public int? UDSVERLC { get; set; }
 
-        [Display(Name = "Number of correct L-words repeated in 1 minute")]
+        [Display(Name = "Number of L-words repeated in 1 minute", Description = "(0-15)")]
         [Range(0, 15, ErrorMessage = "(0-15)")]
+        [RequiredIfRange(nameof(UDSVERLC), 0, 40, ErrorMessage = "Provide count of L-words repeated.")]
         public int? UDSVERLR { get; set; }
 
-        [Display(Name = "Number of non-L-words and rule violation errors in 1 minute")]
+        [Display(Name = "Number of non-L-words and rule violation errors in 1 minute", Description = "(0-15)")]
         [Range(0, 15, ErrorMessage = "(0-15)")]
+        [RequiredIfRange(nameof(UDSVERLC), 0, 40, ErrorMessage = "Provide count of non-L-words and rule violation errors.")]
         public int? UDSVERLN { get; set; }
 
-        [Display(Name = "TOTAL number of correct F-words and L-words")]
+        [Display(Name = "TOTAL number of correct F-words and L-words", Description = "(0-80)")]
         [Range(0, 80, ErrorMessage = "(0-80)")]
+        [RequiredOnComplete]
         public int? UDSVERTN { get; set; }
 
-        [Display(Name = "TOTAL number of F-word and L-words repetition errors")]
+        [Display(Name = "TOTAL number of F-word and L-words repetition errors", Description = "(0-30)")]
         [Range(0, 30, ErrorMessage = "(0-30)")]
+        [RequiredOnComplete]
         public int? UDSVERTE { get; set; }
 
-        [Display(Name = "TOTAL number of non-F/L words and rule violation errors")]
+        [Display(Name = "TOTAL number of non-F/L words and rule violation errors", Description = "(0-30)")]
         [Range(0, 30, ErrorMessage = "(0-30)")]
+        [RequiredOnComplete]
         public int? UDSVERTI { get; set; }
 
+        #endregion
+
         [Display(Name = "Per the clinician (e.g., neuropsychologist, behavioral neurologist, or other suitably qualified clinician), based on the UDS neuropsychological examination, the participants cognitive status is deemed")]
+        [RequiredOnComplete]
         public int? COGSTAT { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
--- src/UDS.Net.Forms/Pages/UDS3/C2.cshtml.cs
+++ src/UDS.Net.Forms/Pages/UDS3/C2.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using UDS.Net.Forms.Extensions;
 using UDS.Net.Forms.Models.PageModels;
 using UDS.Net.Forms.Models.UDS3;
@@ -17,6 +18,14 @@ namespace UDS.Net.Forms.Pages.UDS3
     {
         [BindProperty]
         public C2 C2 { get; set; } = default!;
+
+        //public List<SelectListItem> ReasonCodeListItems { get; set; } = new List<SelectListItem>
+        //{
+        //    new SelectListItem("Physical problem", "95"),
+        //    new SelectListItem("Cognitive/behavior problem", "96"),
+        //    new SelectListItem("Other problem", "97"),
+        //    new SelectListItem("Verbal refusal", "98")
+        //};
 
         public List<RadioListItem> SimpleNoYesListItems { get; set; } = new List<RadioListItem>
         {
@@ -51,6 +60,148 @@ namespace UDS.Net.Forms.Pages.UDS3
             new RadioListItem("One or two test scores are abnormal", "3"),
             new RadioListItem("Three or more scores are abnormal or lower than expected", "4"),
             new RadioListItem("Clinician unable to render opinion", "0")
+        };
+
+        //public Dictionary<string, UIBehavior> MOCACOMPBehavior = new Dictionary<string, UIBehavior>
+        //{
+        //    { "0", new UIBehavior { PropertyAttribute = new UIDisableAttribute("C2.MOCAREAS")} }, // "skip to question 2a"
+        //    { "1", new UIBehavior { PropertyAttribute = new UIEnableAttribute("C2.MOCAREAS")} } // "continue to question 1b"
+        //};
+
+        public Dictionary<string, UIBehavior> MOCACOMPBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("C2.MOCAREAS"),
+                    new UIDisableAttribute("C2.MOCALOC"),
+                    new UIDisableAttribute("C2.MOCALAN"),
+                    new UIDisableAttribute("C2.MOCAVIS"),
+                    new UIDisableAttribute("C2.MOCAHEAR"),
+                    new UIDisableAttribute("C2.MOCATOTS"),
+                    new UIDisableAttribute("C2.MOCATRAI"),
+                    new UIDisableAttribute("C2.MOCACUBE"),
+                    new UIDisableAttribute("C2.MOCACLOC"),
+                    new UIDisableAttribute("C2.MOCACLON"),
+                    new UIDisableAttribute("C2.MOCACLOH"),
+                    new UIDisableAttribute("C2.MOCANAMI"),
+                    new UIDisableAttribute("C2.MOCAREGI"),
+                    new UIDisableAttribute("C2.MOCADIGI"),
+                    new UIDisableAttribute("C2.MOCALETT"),
+                    new UIDisableAttribute("C2.MOCASER7"),
+                    new UIDisableAttribute("C2.MOCAREPE"),
+                    new UIDisableAttribute("C2.MOCAFLUE"),
+                    new UIDisableAttribute("C2.MOCAABST"),
+                    new UIDisableAttribute("C2.MOCARECN"),
+                    new UIDisableAttribute("C2.MOCARECC"),
+                    new UIDisableAttribute("C2.MOCARECR"),
+                    new UIDisableAttribute("C2.MOCAORDT"),
+                    new UIDisableAttribute("C2.MOCAORMO"),
+                    new UIDisableAttribute("C2.MOCAORYR"),
+                    new UIDisableAttribute("C2.MOCAORDY"),
+                    new UIDisableAttribute("C2.MOCAORPL"),
+                    new UIDisableAttribute("C2.MOCAORCT")
+                },
+                InstructionalMessage = "skip to question 2a"
+            } },
+            { "1", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("C2.MOCAREAS"),
+                    new UIEnableAttribute("C2.MOCALOC"),
+                    new UIEnableAttribute("C2.MOCALAN"),
+                    new UIEnableAttribute("C2.MOCAVIS"),
+                    new UIEnableAttribute("C2.MOCAHEAR"),
+                    new UIEnableAttribute("C2.MOCATOTS"),
+                    new UIEnableAttribute("C2.MOCATRAI"),
+                    new UIEnableAttribute("C2.MOCACUBE"),
+                    new UIEnableAttribute("C2.MOCACLOC"),
+                    new UIEnableAttribute("C2.MOCACLON"),
+                    new UIEnableAttribute("C2.MOCACLOH"),
+                    new UIEnableAttribute("C2.MOCANAMI"),
+                    new UIEnableAttribute("C2.MOCAREGI"),
+                    new UIEnableAttribute("C2.MOCADIGI"),
+                    new UIEnableAttribute("C2.MOCALETT"),
+                    new UIEnableAttribute("C2.MOCASER7"),
+                    new UIEnableAttribute("C2.MOCAREPE"),
+                    new UIEnableAttribute("C2.MOCAFLUE"),
+                    new UIEnableAttribute("C2.MOCAABST"),
+                    new UIEnableAttribute("C2.MOCARECN"),
+                    new UIEnableAttribute("C2.MOCARECC"),
+                    new UIEnableAttribute("C2.MOCARECR"),
+                    new UIEnableAttribute("C2.MOCAORDT"),
+                    new UIEnableAttribute("C2.MOCAORMO"),
+                    new UIEnableAttribute("C2.MOCAORYR"),
+                    new UIEnableAttribute("C2.MOCAORDY"),
+                    new UIEnableAttribute("C2.MOCAORPL"),
+                    new UIEnableAttribute("C2.MOCAORCT")
+                },
+                InstructionalMessage = "continue to question 1b"
+            } }
+        };
+
+        public Dictionary<string, UIBehavior> MOCALANBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("C2.MOCALANX")} },
+            { "2", new UIBehavior { PropertyAttribute = new UIDisableAttribute("C2.MOCALANX")} },
+            { "3", new UIBehavior { PropertyAttribute = new UIEnableAttribute("C2.MOCALANX")} }
+        };
+
+        public Dictionary<string, UIBehavior> NPSYLANBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("C2.NPSYLANX")} },
+            { "2", new UIBehavior { PropertyAttribute = new UIDisableAttribute("C2.NPSYLANX")} },
+            { "3", new UIBehavior { PropertyAttribute = new UIEnableAttribute("C2.NPSYLANX")} }
+        };
+
+        public Dictionary<string, UIBehavior> CRAFTVRSBehavior = new Dictionary<string, UIBehavior>
+        {
+            { "0-44", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("C2.CRAFTURS"),
+                },
+                InstructionalMessage = "if test not completed, enter reason code, 95-98, and skip to question 4a"
+            } },
+            { "95-98", new UIBehavior {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIDisableAttribute("C2.CRAFTURS"),
+                }
+            } }
+        };
+
+        //public Dictionary<string, UIBehavior> UDSVERLCBehavior = new Dictionary<string, UIBehavior>
+        //{
+        //    { "0-40", new UIBehavior {
+        //        PropertyAttributes = new List<UIPropertyAttributes>
+        //        {
+        //            new UIEnableAttribute("C2.UDSVERLR"),
+        //            new UIEnableAttribute("C2.UDSVERLN"),
+        //            new UIEnableAttribute("C2.UDSVERTN"),
+        //            new UIEnableAttribute("C2.UDSVERTE"),
+        //            new UIEnableAttribute("C2.UDSVERTI")
+        //        },
+        //        InstructionalMessage = "if test not completed, enter reason code, 95-98, and skip to question 4a"
+        //    } }
+        //};
+
+        public UIRangeToggle UDSVERLCBehavior = new UIRangeToggle
+        {
+            Low = 0,
+            High = 40,
+            UIBehavior = new UIBehavior
+            {
+                PropertyAttributes = new List<UIPropertyAttributes>
+                {
+                    new UIEnableAttribute("C2.UDSVERLR"),
+                    new UIEnableAttribute("C2.UDSVERLN"),
+                    new UIEnableAttribute("C2.UDSVERTN"),
+                    new UIEnableAttribute("C2.UDSVERTE"),
+                    new UIEnableAttribute("C2.UDSVERTI")
+                },
+                InstructionalMessage = "if test not completed, enter reason code, 95-98, and skip to question 4a"
+            }
         };
 
         public C2Model(IVisitService visitService) : base(visitService, "C2")
