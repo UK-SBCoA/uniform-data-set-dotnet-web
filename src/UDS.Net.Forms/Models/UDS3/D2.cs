1a77c64c51eaf17af9e07d9bf3e92599b38516cd
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-10-11 17:08:17 -0400

    Adds Required validation (progress)

--- src/UDS.Net.Forms/Models/UDS3/D2.cs
+++ src/UDS.Net.Forms/Models/UDS3/D2.cs
@@ -11,6 +11,7 @@ namespace UDS.Net.Forms.Models.UDS3
     public class D2 : FormModel
     {
         [Display(Name = "Cancer (excluding non-melanoma skin cancer), primary or metastatic")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? CANCER { get; set; }
 
         [Display(Name = "If yes, specify primary site")]
@@ -21,33 +22,43 @@ namespace UDS.Net.Forms.Models.UDS3
         public string? CANCSITE { get; set; }
 
         [Display(Name = "Diabetes")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? DIABET { get; set; }
 
         [Display(Name = "Myocardial infarct")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? MYOINF { get; set; }
 
         [Display(Name = "Congestive heart failure")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? CONGHRT { get; set; }
 
         [Display(Name = "Atrial fibrillation")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? AFIBRILL { get; set; }
 
         [Display(Name = "Hypertension")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? HYPERT { get; set; }
 
         [Display(Name = "Angina")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? ANGINA { get; set; }
 
         [Display(Name = "Hypercholesterolemia")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? HYPCHOL { get; set; }
 
         [Display(Name = "B12 deficiency")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? VB12DEF { get; set; }
 
         [Display(Name = "Thyroid disease")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? THYDIS { get; set; }
 
         [Display(Name = "Arthritis")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? ARTH { get; set; }
 
         [Display(Name = "If yes, what type?")]
@@ -73,21 +84,27 @@ namespace UDS.Net.Forms.Models.UDS3
         public bool ARTUNKN { get; set; }
 
         [Display(Name = "Incontinence — urinary")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? URINEINC { get; set; }
 
         [Display(Name = "Incontinence — bowel")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? BOWLINC { get; set; }
 
         [Display(Name = "Sleep apnea")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? SLEEPAP { get; set; }
 
         [Display(Name = "REM sleep behavior disorder (RBD)")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? REMDIS { get; set; }
 
         [Display(Name = "Hyposomnia/insomnia")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? HYPOSOM { get; set; }
 
         [Display(Name = "Other sleep disorder")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? SLEEPOTH { get; set; }
 
         [Display(Name = "Other sleep disorder (specify)")]
@@ -97,18 +114,23 @@ namespace UDS.Net.Forms.Models.UDS3
         public string? SLEEPOTX { get; set; }
 
         [Display(Name = "Carotid procedure: angioplasty, endarterectomy, or stent")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? ANGIOCP { get; set; }
 
         [Display(Name = "Percutaneous coronary intervention: angioplasty and/or stent")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? ANGIOPCI { get; set; }
 
         [Display(Name = "Procedure: pacemaker and/or defibrillator")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? PACEMAKE { get; set; }
 
         [Display(Name = "Procedure: heart valve replacement or repair")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? HVALVE { get; set; }
 
         [Display(Name = "Antibody-mediated encephalopathy")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? ANTIENC { get; set; }
 
         [Display(Name = "Specify antibody")]
@@ -118,6 +140,7 @@ namespace UDS.Net.Forms.Models.UDS3
         public string? ANTIENCX { get; set; }
 
         [Display(Name = "Other medical conditions or procedures not listed above")]
+        [RequiredOnComplete(ErrorMessage = "Response required")]
         public int? OTHCOND { get; set; }
 
         [Display(Name = "If yes, specify")]
