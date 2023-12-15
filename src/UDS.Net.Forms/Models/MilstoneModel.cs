4ee83c720365f986afd66fd3795793923e611fde
mlan225 <17088502+mlan225@users.noreply.github.com>
2023-12-15 17:27:29 -0500

    Build out the create view with client side validation

--- src/UDS.Net.Forms/Models/MilstoneModel.cs
+++ src/UDS.Net.Forms/Models/MilstoneModel.cs
@@ -9,32 +9,48 @@ namespace UDS.Net.Forms.Models
         public int FormId { get; set; }
         public int ParticipationId { get; set; }
         public string Status { get; set; }
+        [Display(Name = "Month")]
         public int? CHANGEMO { get; set; }
+        [Display(Name = "Day")]
         public int? CHANGEDY { get; set; }
+        [Display(Name = "Year")]
         public int? CHANGEYR { get; set; }
+        [Display(Name = "UDS data collection status changed; subject's new status is (CHECK ONE):")]
         public int? PROTOCOL { get; set; }
+        [Display(Name = "Autopsy consent on file?")]
         public int? ACONSENT { get; set; }
+        [Display(Name = "Subject is too cognitively impaired")]
         public bool RECOGIM { get; set; }
-        public int? REPHYILL { get; set; }
-        public int? REREFUSE { get; set; }
-        public int? RENAVAIL { get; set; }
-        public int? RENURSE { get; set; }
+        [Display(Name = "Subject is too ill or\nphysically impaired")]
+        public bool? REPHYILL { get; set; }
+        [Display(Name = "Subject refuses neuropsychological testing or clinical exam")]
+        public bool? REREFUSE { get; set; }
+        [Display(Name = "Subject or coparticipant unreachable, not available, or moved away")]
+        public bool? RENAVAIL { get; set; }
+        [Display(Name = "Subject has permanently entered nursing home")]
+        public bool? RENURSE { get; set; }
         public int? NURSEMO { get; set; }
         public int? NURSEDY { get; set; }
         public int? NURSEYR { get; set; }
-        public int? REJOIN { get; set; }
-        public int? FTLDDISC { get; set; }
+        [Display(Name = "Subject is rejoining ADC")]
+        public bool? REJOIN { get; set; }
+        [Display(Name = "Subject will no longer receive FTLD Module follow-up, but annual in-person UDS visits will continue")]
+        public bool? FTLDDISC { get; set; }
         public int? FTLDREAS { get; set; }
         public string FTLDREAX { get; set; }
-        public int? DECEASED { get; set; }
-        public int? DISCONT { get; set; }
+        [Display (Name = "Subject has DIED (COMPLETE DEATH SECTION, BELOW)")]
+        public bool? DECEASED { get; set; }
+        [Display (Name = "Subject has been DROPPED from ADC. (COMPLETE DROPPED SECTION, BELOW)")]
+        public bool? DISCONT { get; set; }
         public int? DEATHMO { get; set; }
         public int? DEATHDY { get; set; }
         public int? DEATHYR { get; set; }
+        [Display(Name = "ADC autopsy")]
         public int? AUTOPSY { get; set; }
         public int? DISCMO { get; set; }
         public int? DISCDAY { get; set; }
         public int? DISCYR { get; set; }
+        [Display(Name = "Main reason for being dropped from ADC (CHECK ONE):")]
         public int? DROPREAS { get; set; }
         public DateTime CreatedAt { get; set; }
         public string CreatedBy { get; set; }
