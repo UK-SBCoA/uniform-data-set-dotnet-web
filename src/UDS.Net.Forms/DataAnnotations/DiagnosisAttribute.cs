f96e3fe40b34b87d29006b025daea98734a146c0
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-02-05 16:49:31 -0500

    Modifies to add allowunknown property

--- src/UDS.Net.Forms/DataAnnotations/DiagnosisAttribute.cs
+++ src/UDS.Net.Forms/DataAnnotations/DiagnosisAttribute.cs
@@ -1,67 +1,62 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.Reflection.Emit;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using UDS.Net.Forms.Models;
 using UDS.Net.Forms.Models.UDS3;
-using UDS.Net.Services.DomainModels;
 using UDS.Net.Services.Enums;
 
-namespace UDS.Net.Forms.DataAnnotations
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class DiagnosisAttribute : ValidationAttribute, IClientModelValidator
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class DiagnosisAttribute : ValidationAttribute, IClientModelValidator
+    private static string ERRORMESSAGE = "Diagnosis code invalid. Please see reference.";
+    private static int[] CODES = new int[] { 40, 41, 42, 43, 44, 45, 50, 70, 80, 100, 110, 120, 130, 131, 132, 133, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 310, 320, 400, 410, 420, 421, 430, 431, 432, 433, 434, 435, 436, 439, 440, 450, 490, 999 };
+
+    public bool AllowUnknown { get; set; } = false;
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        private static string ERRORMESSAGE = "Diagnosis code invalid. Please see reference.";
-        private static int[] CODES = new int[] { 40, 41, 42, 43, 44, 45, 50, 70, 80, 100, 110, 120, 130, 131, 132, 133, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 310, 320, 400, 410, 420, 421, 430, 431, 432, 433, 434, 435, 436, 439, 440, 450, 490, 999 };
-
-        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        if (validationContext.ObjectType.IsSubclassOf(typeof(FormModel)))
         {
-            if (validationContext.ObjectType.IsSubclassOf(typeof(FormModel)))
+            var form = (FormModel)validationContext.ObjectInstance;
+
+            // Only validate if the form is attempting to be completed
+            if (form.Status == FormStatus.Complete)
             {
-                var form = (FormModel)validationContext.ObjectInstance;
-                var a3 = (A3)validationContext.ObjectInstance;
-
-                if (form.Status == FormStatus.Complete)
+                if (value is int)
                 {
-                    bool validNeurStatus = (a3.MOMNEUR >= 1 && a3.MOMNEUR <= 5) || (a3.DADNEUR >= 1 && a3.DADNEUR <= 5);
-
-                    if (validNeurStatus)
-                    {
-                        if (value is int code && CODES.Contains(code))
-                        {
-                            return ValidationResult.Success;
-                        }
-                        return new ValidationResult(ERRORMESSAGE);
-                    }
-                    else if (value == null && (a3.MOMNEUR == 8 || a3.MOMNEUR == 9 || a3.DADNEUR == 8 || a3.DADNEUR == 9))
+                    int code = (int)value;
+                    if (CODES.Contains(code))
                     {
                         return ValidationResult.Success;
                     }
-                    return new ValidationResult(ERRORMESSAGE);
                 }
+
+                if (AllowUnknown && value == null)
+                {
+                    return ValidationResult.Success;
+                }
+
+                return new ValidationResult(ERRORMESSAGE);
             }
-            return ValidationResult.Success;
         }
+        return ValidationResult.Success;
+    }
+    public void AddValidation(ClientModelValidationContext context)
+    {
+        MergeAttribute(context.Attributes, "data-val", "true");
+        MergeAttribute(context.Attributes, "data-val-diagnosis", ERRORMESSAGE);
+    }
 
-        public void AddValidation(ClientModelValidationContext context)
+    /// <summary>
+    /// See https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-7.0#iclientmodelvalidator-for-client-side-validation
+    /// </summary>
+    private static bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+    {
+        if (attributes.ContainsKey(key))
         {
-            MergeAttribute(context.Attributes, "data-val", "true");
-            MergeAttribute(context.Attributes, "data-val-diagnosis", ERRORMESSAGE);
+            return false;
         }
 
-        /// <summary>
-        /// See https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-7.0#iclientmodelvalidator-for-client-side-validation
-        /// </summary>
-        private static bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
-        {
-            if (attributes.ContainsKey(key))
-            {
-                return false;
-            }
-
-            attributes.Add(key, value);
-            return true;
-        }
+        attributes.Add(key, value);
+        return true;
     }
 }
