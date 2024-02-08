7a266bfb20e524c1ba288fdd4b771df93dfebd6e
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-02-08 11:13:32 -0500

    Add display name to A2 properties

--- src/UDS.Net.Forms/Models/UDS3/A2.cs
+++ src/UDS.Net.Forms/Models/UDS3/A2.cs
@@ -14,18 +14,30 @@ namespace UDS.Net.Forms.Models.UDS3
     /// </summary>
     public class A2 : FormModel
     {
+        [Display(Name = "Is this a new co-participant - i.e., one who was not a co-participant at any past UDS visit?")]
         public int? NEWINF { get; set; }
+        [Display(Name = "What is the co-participant’s relationship to the participant?")]
         public int? INRELTO { get; set; }
+        [Display(Name = "How long has the co-participant known the participant?")]
         public int? INKNOWN { get; set; }
+        [Display(Name = "Does the co-participant live with the participant?")]
         public int? INLIVWTH { get; set; }
+        [Display(Name = "What is the primary mode of contact with the participant?")]
         public int? INCNTMOD { get; set; }
+        [Display(Name = "Primary mode of contact with the participant - Other (specify)")]
         [MaxLength(60)]
         public string? INCNTMDX { get; set; }
+        [Display(Name = "What is the approximate frequency of contact?")]
         public int? INCNTFRQ { get; set; }
+        [Display(Name = "What is the average amount of time spent in contact with the participant during each encounter?")]
         public int? INCNTTIM { get; set; }
+        [Display(Name = "Is there a question about the co-participant’s reliability?")]
         public int? INRELY { get; set; }
+        [Display(Name = "Do you feel like the participant's memory is becoming worse?")]
         public int? INMEMWORS { get; set; }
+        [Display(Name = "About how often does the participant have trouble remembering things?")]
         public int? INMEMTROUB { get; set; }
+        [Display(Name = "Compared to 10 years ago, would you say that the participant's memory is much worse, a little worse, the same, a little better, or much better?")]
         public int? INMEMTEN { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
