018404972f391d85affb936fcba562738d8bc95f
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-02-13 16:21:57 -0500

    Adds validation

--- src/UDS.Net.Forms/Models/UDS4/A2.cs
+++ src/UDS.Net.Forms/Models/UDS4/A2.cs
@@ -17,27 +17,39 @@ namespace UDS.Net.Forms.Models.UDS4
         [Display(Name = "Is this a new co-participant - i.e., one who was not a co-participant at any past UDS visit?")]
         public int? NEWINF { get; set; }
         [Display(Name = "What is the co-participant’s relationship to the participant?")]
+        [RequiredOnComplete]
         public int? INRELTO { get; set; }
         [Display(Name = "How long has the co-participant known the participant?")]
+        [RequiredOnComplete]
+        [RegularExpression("^(0|[1-9]\\d?|120|999)$", ErrorMessage = "Valid range is 0-120 or 999")]
         public int? INKNOWN { get; set; }
         [Display(Name = "Does the co-participant live with the participant?")]
+        [RequiredOnComplete]
         public int? INLIVWTH { get; set; }
         [Display(Name = "What is the primary mode of contact with the participant?")]
+        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "Please provide a primary mode of contact")]
         public int? INCNTMOD { get; set; }
         [Display(Name = "Primary mode of contact with the participant - Other (specify)")]
         [MaxLength(60)]
+        [RequiredIf(nameof(INCNTMOD), "6", ErrorMessage = "If primary mode of contact is 'Other', please provide a response")]
         public string? INCNTMDX { get; set; }
         [Display(Name = "What is the approximate frequency of contact?")]
+        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "What is the frequency of contact?")]
         public int? INCNTFRQ { get; set; }
         [Display(Name = "What is the average amount of time spent in contact with the participant during each encounter?")]
+        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "What is the amount of time spent in contact with the participant during each encounter")]
         public int? INCNTTIM { get; set; }
         [Display(Name = "Is there a question about the co-participant’s reliability?")]
+        [RequiredOnComplete]
         public int? INRELY { get; set; }
         [Display(Name = "Do you feel like the participant's memory is becoming worse?")]
+        [RequiredOnComplete]
         public int? INMEMWORS { get; set; }
         [Display(Name = "About how often does the participant have trouble remembering things?")]
+        [RequiredOnComplete]
         public int? INMEMTROUB { get; set; }
         [Display(Name = "Compared to 10 years ago, would you say that the participant's memory is much worse, a little worse, the same, a little better, or much better?")]
+        [RequiredOnComplete]
         public int? INMEMTEN { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
--- src/UDS.Net.Forms/Pages/UDS4/A2.cshtml.cs
+++ src/UDS.Net.Forms/Pages/UDS4/A2.cshtml.cs
@@ -19,7 +19,7 @@ namespace UDS.Net.Forms.Pages.UDS4
         [BindProperty]
         public A2 A2 { get; set; } = default!;
 
-        #region UDS4
+
 
         public List<RadioListItem> INRELTO { get; } = new List<RadioListItem>
         {
@@ -35,6 +35,28 @@ namespace UDS.Net.Forms.Pages.UDS4
         {
             new RadioListItem("No", "0"),
             new RadioListItem("Yes (SKIP TO QUESTION 5)", "1")
+        };
+
+        public Dictionary<string, UIBehavior> INLIVWTHUIBehavior = new Dictionary<string, UIBehavior>
+        {
+
+            { "0", new UIBehavior { PropertyAttributes = new List<UIPropertyAttributes>
+            {
+            new UIEnableAttribute("A2.INCNTMOD"),
+            new UIEnableAttribute("A2.INCNTFRQ"),
+            new UIEnableAttribute("A2.INCNTMDX"),
+            new UIEnableAttribute("A2.INCNTTIM")}
+            } },
+
+            {"1", new UIBehavior {PropertyAttributes = new List<UIPropertyAttributes>
+            {
+            new UIDisableAttribute("A2.INCNTMOD"),
+            new UIDisableAttribute("A2.INCNTFRQ"),
+            new UIDisableAttribute("A2.INCNTMDX"),
+            new UIDisableAttribute("A2.INCNTTIM")
+            }
+            } }
+
         };
 
         public List<RadioListItem> INCNTMOD { get; } = new List<RadioListItem>
@@ -101,228 +123,6 @@ namespace UDS.Net.Forms.Pages.UDS4
         };
 
 
-        #endregion
-
-        public List<RadioListItem> SexListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("Male", "1"),
-            new RadioListItem("Female", "2")
-        };
-
-        public List<RadioListItem> NewCoParticipantListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("No (If No, SKIP TO QUESTION 9)", "0"),
-            new RadioListItem("Yes", "1")
-        };
-
-        public Dictionary<string, UIBehavior> NewCoParticipantUIBehavior = new Dictionary<string, UIBehavior>
-        {
-            { "0", new UIBehavior{
-            PropertyAttributes = new List<UIPropertyAttributes>
-            {
-                new UIDisableAttribute("A2.INHISP"),
-                new UIDisableAttribute("A2.INHISPOR"),
-                new UIDisableAttribute("A2.INRACE"),
-                new UIDisableAttribute("A2.INRASEC"),
-                new UIDisableAttribute("A2.INRATER"),
-                new UIDisableAttribute("A2.INEDUC")
-            },
-            InstructionalMessage = "skip to question 9"
-
-            } },
-            { "1", new UIBehavior{
-                PropertyAttributes = new List<UIPropertyAttributes>
-            {
-                new UIEnableAttribute("A2.INHISP"),
-                new UIEnableAttribute("A2.INHISPOR"),
-                new UIEnableAttribute("A2.INRACE"),
-                new UIEnableAttribute("A2.INRASEC"),
-                new UIEnableAttribute("A2.INRATER"),
-                new UIEnableAttribute("A2.INEDUC")
-            },
-            } }
-        };
-
-        public List<RadioListItem> HispanicLatinoListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("No  (If No, SKIP TO QUESTION 4)", "1"),
-            new RadioListItem("Yes", "2"),
-            new RadioListItem("Unknown (If Unknown, SKIP TO QUESTION 4)", "9")
-        };
-
-        public List<RadioListItem> HispanicLatinoFVPListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("No  (If No, SKIP TO QUESTION 5)", "1"),
-            new RadioListItem("Yes", "2"),
-            new RadioListItem("Unknown (If Unknown, SKIP TO QUESTION 5)", "9")
-        };
-        public Dictionary<string, UIBehavior> HispanicLatinoUIBehavior = new Dictionary<string, UIBehavior>
-        {
-            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOR") } },
-            { "2", new UIBehavior { PropertyAttribute = new UIEnableAttribute("A2.INHISPOR") } },
-            { "9", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOR") } }
-        };
-
-        public List<RadioListItem> OriginsListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("Mexican, Chicano, or Mexican-American", "1"),
-            new RadioListItem("Puerto Rican", "2"),
-            new RadioListItem("Cuban", "3"),
-            new RadioListItem("Dominican", "4"),
-            new RadioListItem("Central American", "5"),
-            new RadioListItem("South American", "6"),
-            new RadioListItem("Other (specify)", "50"),
-            new RadioListItem("Unknown","99")
-        };
-
-        public Dictionary<string, UIBehavior> OriginsUIBehavior = new Dictionary<string, UIBehavior>
-        {
-            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } },
-            { "2", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } },
-            { "3", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } },
-            { "4", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } },
-            { "5", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } },
-            { "6", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } },
-            { "50", new UIBehavior { PropertyAttribute = new UIEnableAttribute("A2.INHISPOX") } },
-            { "99", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INHISPOX") } }
-
-        };
-        public List<RadioListItem> RacialGroupsListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("White", "1"),
-            new RadioListItem("Black or African American", "2"),
-            new RadioListItem("American Indian or Alaska Native", "3"),
-            new RadioListItem("Native Hawaiian or other Pacific Islander", "4"),
-            new RadioListItem("Asian", "5"),
-            new RadioListItem("Other (specify)", "50"),
-            new RadioListItem("Unknown", "99")
-        };
-
-
-        public Dictionary<string, UIBehavior> RacialGroupsUIBehavior = new Dictionary<string, UIBehavior>
-        {
-            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX") } },
-            { "2", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX") } },
-            { "3", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX") } },
-            { "4", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX" ) } },
-            { "5", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX") } },
-            { "50", new UIBehavior { PropertyAttribute = new UIEnableAttribute("A2.INRACEX")  } },
-            { "99", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX") } },
-
-        };
-
-        public List<RadioListItem> AdditionalRacialGroupsListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("White", "1"),
-            new RadioListItem("Black or African American", "2"),
-            new RadioListItem("American Indian or Alaska Native", "3"),
-            new RadioListItem("Native Hawaiian or other Pacific Islander", "4"),
-            new RadioListItem("Asian", "5"),
-            new RadioListItem("Other (specify)", "50"),
-            new RadioListItem("Unknown", "99")
-        };
-
-        public Dictionary<string, UIBehavior> AdditionalRacialGroupsUIBehavior = new Dictionary<string, UIBehavior>
-        {
-            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRASECX") } },
-            { "2", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRASECX") } },
-            { "3", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRACEX") } },
-            { "4", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRASECX" ) } },
-            { "5", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRASECX") } },
-            { "50", new UIBehavior { PropertyAttribute = new UIEnableAttribute("A2.INRASECX")  } },
-            { "99", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRASECX") } },
-
-        };
-
-        public List<RadioListItem> BeyondAdditionalRacialGroupsListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("White", "1"),
-            new RadioListItem("Black or African American", "2"),
-            new RadioListItem("American Indian or Alaska Native", "3"),
-            new RadioListItem("Native Hawaiian or other Pacific Islander", "4"),
-            new RadioListItem("Asian", "5"),
-            new RadioListItem("Other (specify)", "50"),
-            new RadioListItem("Unknown", "99")
-        };
-
-        public Dictionary<string, UIBehavior> BeyondAdditionalRacialGroupsUIBehavior = new Dictionary<string, UIBehavior>
-        {
-            { "1", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRATERX") } },
-            { "2", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRATERX") } },
-            { "3", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRATERX") } },
-            { "4", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRATERX" ) } },
-            { "5", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRATERX") } },
-            { "50", new UIBehavior { PropertyAttribute = new UIEnableAttribute("A2.INRATERX")  } },
-            { "99", new UIBehavior { PropertyAttribute = new UIDisableAttribute("A2.INRATERX") } },
-
-        };
-
-
-        public List<RadioListItem> RelationshipTypesListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("Spouse, partner, or companion (include ex-spouse, ex-partner, fiancé, boyfriend, girlfriend)", "1"),
-            new RadioListItem("Child (by blood or through marriage or adoption)", "2"),
-            new RadioListItem("Sibling (by blood or through marriage or adoption)", "3"),
-            new RadioListItem("Other relative (by blood or through marriage or adoption)", "4"),
-            new RadioListItem("Friend, neighbor, or someone known through family, friends, work, or community (e.g., church)", "5"),
-            new RadioListItem("Paid caregiver, health care provider, or clinician", "6"),
-        };
-
-        public List<RadioListItem> LivingSituationListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("No", "0"),
-            new RadioListItem("Yes (If Yes, SKIP TO QUESTION 10)", "1")
-        };
-
-        public List<RadioListItem> LivingSituationFVPListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("No", "0"),
-            new RadioListItem("Yes (If Yes, SKIP TO QUESTION 11)", "1")
-        };
-
-        public Dictionary<string, UIBehavior> LivingSituationUIBehavior = new Dictionary<string, UIBehavior>
-        {
-
-            { "0", new UIBehavior { PropertyAttributes = new List<UIPropertyAttributes>
-            {
-            new UIEnableAttribute("A2.INVISITS"),
-            new UIEnableAttribute("A2.INCALLS")}
-            } },
-
-            {"1", new UIBehavior {PropertyAttributes = new List<UIPropertyAttributes>
-            {
-            new UIDisableAttribute("A2.INVISITS"),
-            new UIDisableAttribute("A2.INCALLS")
-            }
-            } }
-
-        };
-
-        public List<RadioListItem> VisitContactFrequencyListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("Daily", "1"),
-            new RadioListItem("At least 3 times per week", "2"),
-            new RadioListItem("Weekly", "3"),
-            new RadioListItem("At least three times per month", "4"),
-            new RadioListItem("Monthly", "5"),
-            new RadioListItem("Less than once a month", "6"),
-        };
-
-        public List<RadioListItem> TelephoneContactFrequencyListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("Daily", "1"),
-            new RadioListItem("At least 3 times per week", "2"),
-            new RadioListItem("Weekly", "3"),
-            new RadioListItem("At least three times per month", "4"),
-            new RadioListItem("Monthly", "5"),
-            new RadioListItem("Less than once a month", "6"),
-        };
-
-        public List<RadioListItem> ReliabilityListItems { get; } = new List<RadioListItem>
-        {
-            new RadioListItem("No", "0"),
-            new RadioListItem("Yes", "1")
-        };
 
         public A2Model(IVisitService visitService) : base(visitService, "A2")
         {
