24d2ddcd24ac71abf104f83fbc3a23097246a12d
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-02-14 15:00:05 -0500

    Update error messages

--- src/UDS.Net.Forms/Models/UDS4/A2.cs
+++ src/UDS.Net.Forms/Models/UDS4/A2.cs
@@ -17,39 +17,39 @@ namespace UDS.Net.Forms.Models.UDS4
         [Display(Name = "Is this a new co-participant - i.e., one who was not a co-participant at any past UDS visit?")]
         public int? NEWINF { get; set; }
         [Display(Name = "What is the co-participant’s relationship to the participant?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete(ErrorMessage = "Response to co-participant's relationship to participant required")]
         public int? INRELTO { get; set; }
         [Display(Name = "How long has the co-participant known the participant?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete(ErrorMessage = "Response to how long co-participant has know the participant reuiqred")]
         [RegularExpression("^(0|[1-9]\\d?|1[01]\\d|120|999)$", ErrorMessage = "Valid range is 0-120 or 999")]
         public int? INKNOWN { get; set; }
         [Display(Name = "Does the co-participant live with the participant?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete(ErrorMessage = "Response to co-participant living with participant required")]
         public int? INLIVWTH { get; set; }
         [Display(Name = "What is the primary mode of contact with the participant?")]
-        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "Please provide a primary mode of contact")]
+        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "Response to primary mode of contact required")]
         public int? INCNTMOD { get; set; }
         [Display(Name = "Primary mode of contact with the participant - Other (specify)")]
         [MaxLength(60)]
-        [RequiredIf(nameof(INCNTMOD), "6", ErrorMessage = "If primary mode of contact is 'Other', please provide a response")]
+        [RequiredIf(nameof(INCNTMOD), "6", ErrorMessage = "If primary mode of contact is 'Other', response is required")]
         public string? INCNTMDX { get; set; }
         [Display(Name = "What is the approximate frequency of contact?")]
-        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "What is the frequency of contact?")]
+        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "Response to frequency of contact required")]
         public int? INCNTFRQ { get; set; }
         [Display(Name = "What is the average amount of time spent in contact with the participant during each encounter?")]
-        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "What is the amount of time spent in contact with the participant during each encounter")]
+        [RequiredIf(nameof(INLIVWTH), "0", ErrorMessage = "Response to average amount of time spent in contact requried")]
         public int? INCNTTIM { get; set; }
         [Display(Name = "Is there a question about the co-participant’s reliability?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete(ErrorMessage = "Response to co-participant's reliability required")]
         public int? INRELY { get; set; }
         [Display(Name = "Do you feel like the participant's memory is becoming worse?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete(ErrorMessage = "Response to participants memory becoming worse required")]
         public int? INMEMWORS { get; set; }
         [Display(Name = "About how often does the participant have trouble remembering things?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete(ErrorMessage = "Response to participants memory required")]
         public int? INMEMTROUB { get; set; }
         [Display(Name = "Compared to 10 years ago, would you say that the participant's memory is much worse, a little worse, the same, a little better, or much better?")]
-        [RequiredOnComplete]
+        [RequiredOnComplete (ErrorMessage = "Response to participants memory since 10 years ago required")]
         public int? INMEMTEN { get; set; }
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
