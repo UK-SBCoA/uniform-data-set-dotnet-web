bae0eb3bcd70f6ca7b0f76f312e2cc2d100443b5
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-02-29 14:02:14 -0500

    Form validation progress

--- src/UDS.Net.Forms/Models/UDS4/A3.cs
+++ src/UDS.Net.Forms/Models/UDS4/A3.cs
@@ -17,77 +17,6 @@ namespace UDS.Net.Forms.Models.UDS4
         [Display(Name = "Since the last visit, is new information available concerning genetic mutations addressed by Questions 2a through 4b, below?\n“Affected” = having dementia or one of the non-normal diagnoses listed in Appendix 1")]
         public int? NWINFMUT { get; set; } // follow-up visits
 
-        [Display(Name = "In this family, is there evidence for an AD mutation? If Yes, select predominant mutation.")]
-        [RegularExpression("^([0-3]|8|9)$")]
-        public int? FADMUT { get; set; }
-
-        [Display(Name = "If Yes, Other (specify)")]
-        [MaxLength(60)]
-        [RequiredIf(nameof(FADMUT), "8", ErrorMessage = "Other predominant AD mutation must be specified.")]
-        [ProhibitedCharacters]
-        public string? FADMUTX { get; set; }
-
-        [Display(Name = "Source of evidence for AD mutation")]
-        [RegularExpression("^([1-3]|8|9)$")]
-        [RequiredIf(nameof(FADMUT), "1", ErrorMessage = "Define source of evidence for APP mutation.")]
-        [RequiredIf(nameof(FADMUT), "2", ErrorMessage = "Define source of evidence for PS-1 mutation.")]
-        [RequiredIf(nameof(FADMUT), "3", ErrorMessage = "Define source of evidence for PS-2 mutation.")]
-        [RequiredIf(nameof(FADMUT), "8", ErrorMessage = "Define source of evidence for other mutation.")]
-        public int? FADMUSO { get; set; }
-
-        [Display(Name = "If other, (specify)")]
-        [MaxLength(60)]
-        [RequiredIf(nameof(FADMUSO), "8", ErrorMessage = "Other source of evidence for AD mutation must be specified.")]
-        [ProhibitedCharacters]
-        public string? FADMUSOX { get; set; }
-
-        [Display(Name = "In this family, is there evidence for an FTLD mutation? If Yes, select predominant mutation")]
-        [RegularExpression("^([0-4]|8|9)$")]
-        public int? FFTDMUT { get; set; }
-
-        [Display(Name = "If Yes, Other (specify)")]
-        [MaxLength(60)]
-        [RequiredIf(nameof(FFTDMUT), "8", ErrorMessage = "Other predominant FTLD mutation must be specified.")]
-        [ProhibitedCharacters]
-        public string? FFTDMUTX { get; set; }
-
-        [Display(Name = "Source of evidence for FTLD mutation")]
-        [RegularExpression("^([1-3]|8|9)$")]
-        [RequiredIf(nameof(FFTDMUT), "8", ErrorMessage = "Other predominant FTLD mutation must be specified.")]
-        public int? FFTDMUSO { get; set; }
-
-        [Display(Name = "If other, (specify)")]
-        [MaxLength(60)]
-        [RequiredIf(nameof(FFTDMUSO), "8", ErrorMessage = "Other source of evidence for FTLD mutation must be specified.")]
-        [ProhibitedCharacters]
-        public string? FFTDMUSX { get; set; }
-
-        [Display(Name = "In this family, is there evidence for a mutation other than an AD or FTLD mutation?")]
-        [RegularExpression("^(0|1|9)$")]
-        public int? FOTHMUT { get; set; }
-
-        [Display(Name = "If Yes, specify")]
-        [MaxLength(60)]
-        [RequiredIf(nameof(FFTDMUSO), "1", ErrorMessage = "Specify other evidence for a mutation other than an AD or FTLD mutation.")]
-        [ProhibitedCharacters]
-        public string? FOTHMUTX { get; set; }
-
-        [Display(Name = "Source of evidence for other mutation")]
-        [RegularExpression("^([1-3]|8|9)$")]
-        public int? FOTHMUSO { get; set; }
-
-        [Display(Name = "If other, specify")]
-        [MaxLength(60)]
-        [RequiredIf(nameof(FOTHMUSO), "8", ErrorMessage = "Other source of evidence required.")]
-        [ProhibitedCharacters]
-        public string? FOTHMUSX { get; set; }
-
-        /// <summary>
-        /// Only required on follow-up visits
-        /// </summary>
-        [Display(Name = "Since the last UDS visit, is new information available concerning the status of the participant's biological mother or father?")]
-        public int? NWINFPAR { get; set; }
-
         [Display(Name = "Mother — birth year")]
         [BirthYear(AllowUnknown = true)]
         public int? MOMYOB { get; set; }
@@ -97,20 +26,19 @@ namespace UDS.Net.Forms.Models.UDS4
         [Required(ErrorMessage = "Please provide an age at death or indicate otherwise")]
         public int? MOMDAGE { get; set; }
 
-        //[Display(Name = "Mother — neurological problem")]
-        //[RegularExpression("^([1-5]|8|9)$", ErrorMessage = "Mother neurological problem/psychiatric condition invalid. Please see reference.")]
-        //[Required(ErrorMessage = "Please provide a value for Primary neurological problem/psychiatric condition")]
-        //public int? MOMNEUR { get; set; }
 
         [Display(Name = "Mother — Primary dx")]
+        [RegularExpression("^([0-9]|1[0-2]|99)$", ErrorMessage = "Codes must be 00-12 or 99")]
+        [RequiredOnComplete]
         public int? MOMETPR { get; set; }
 
         [Display(Name = "Mother — primary diagnosis")]
+        [RegularExpression("^([0-9]|1[0-2]|88|99)$", ErrorMessage ="Codes must be 00-12 88 or 99")]
         [Diagnosis(AllowUnknown = true)]
         public int? MOMETSEC { get; set; }
 
         [Display(Name = "Mother — method of evaluation")]
-        [Range(1, 7)]
+        [Range(1, 4, ErrorMessage ="Must be in the range of 1-4")]
         public int? MOMMEVAL { get; set; }
 
         [Display(Name = "Mother — age of onset")]
