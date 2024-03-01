ffdc4e7adbe1907f72033f52ebac3ed35855d152
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-01 15:15:50 -0500

    adds jquery for enable/disable behavior based on ANYMEDS

--- src/UDS.Net.Web.MVC/wwwroot/js/A4.js
+++ src/UDS.Net.Web.MVC/wwwroot/js/A4.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).ready(function () {
+    $('input[type=radio][name="A4.ANYMEDS"]').change(function () {
+        if (this.value == '1') {
+            $('input[type=checkbox]').prop('disabled', false);
+        }
+        else if (this.value == '0') {
+            $('input[type=checkbox]').prop('disabled', true).prop('checked', false);
+        }
+    });
+});
