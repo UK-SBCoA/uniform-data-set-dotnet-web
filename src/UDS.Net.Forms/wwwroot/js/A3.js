b7a564692e167797dfb25d34d0780aae5d7f5184
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-03-06 15:41:43 -0500

    Update javascript

--- src/UDS.Net.Forms/wwwroot/js/A3.js
+++ src/UDS.Net.Forms/wwwroot/js/A3.js
@@ -22,6 +22,9 @@
       const enabled = i < rowCount;
       const rowInputs = $(`#${this.tableId}_${i}__YOB, #${this.tableId}_${i}__AGD, #${this.tableId}_${i}__ETPR, #${this.tableId}_${i}__ETSEC,#${this.tableId}_${i}__MEVAL,#${this.tableId}_${i}__AGO`);
       rowInputs.prop('disabled', !enabled);
+      if (enabled) {
+        this.updatePrimaryDxControls(i)
+      }
       if (!enabled) {
         rowInputs.val('');
       }
