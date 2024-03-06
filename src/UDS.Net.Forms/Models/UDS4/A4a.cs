5b3d3ca9653945867bcc113dbbd9a141ad630c70
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-06 16:59:32 -0500

    Adds form model

---
+++ src/UDS.Net.Forms/Models/UDS4/A4a.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+using UDS.Net.Forms.DataAnnotations;
+using UDS.Net.Forms.TagHelpers;
+using UDS.Net.Services.Enums;
+
+namespace UDS.Net.Forms.Models.UDS4
+{
+    public class A4a : FormModel
+    {
+        public int? ADVEVENT { get; set; }
+
+        public bool? ARIAE { get; set; }
+
+        public bool? ARIAH { get; set; }
+
+        public bool? ADVERSEOTH { get; set; }
+
+        [MaxLength(60)]
+        [ProhibitedCharacters]
+        public string? ADVERSEOTX { get; set; }
+
+        public int? TRTBIOMARK { get; set; }
+
+        public List<A4aTreatment> Treatment1 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment2 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment3 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment4 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment5 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment6 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment7 { get; set; } = new List<A4aTreatment>();
+
+        public List<A4aTreatment> Treatment8 { get; set; } = new List<A4aTreatment>();
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+
+            foreach (var result in base.Validate(validationContext))
+            {
+                yield return result;
+            }
+
+            yield break;
+        }
+    }
+}
+
