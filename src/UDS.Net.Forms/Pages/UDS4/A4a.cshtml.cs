9cc438d0265fc7ae870c1e01687142bd30c26090
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-12 11:38:21 -0400

    adds A4a code behind

---
+++ src/UDS.Net.Forms/Pages/UDS4/A4a.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UDS.Net.Forms.Extensions;
+using UDS.Net.Forms.Models;
+using UDS.Net.Forms.Models.PageModels;
+using UDS.Net.Forms.Models.UDS4;
+using UDS.Net.Forms.TagHelpers;
+using UDS.Net.Services;
+
+namespace UDS.Net.Forms.Pages.UDS4
+{
+	public class A4aModel : FormPageModel
+    {
+        public A4aModel(IVisitService visitService) : base(visitService, "A4a")
+        {
+        }
+
+    //    public async Task<IActionResult> OnGetAsync(int? id)
+    //    {
+    //        await base.OnGetAsync(id);
+
+    //        if (BaseForm != null)
+    //        {
+    //            A4a = (A4a)BaseForm;
+    //        }
+
+    //        return Page();
+    //    }
+
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> OnPostAsync(int id)
+    //    {
+    //        BaseForm = A4a; // reassign bounded and derived form to base form for base method
+
+    //        Visit.Forms.Add(A4a); // visit needs updated form as well
+
+    //        return await base.OnPostAsync(id); // checks for validation, etc.
+    //    }
+    }
+}
