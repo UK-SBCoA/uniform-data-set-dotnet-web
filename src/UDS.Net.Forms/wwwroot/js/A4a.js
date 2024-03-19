850cd177452d8ce38d32d2414f9af3d8d27b2e6e
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-19 16:59:09 -0400

    Adds JS to enable/disable table

---
+++ src/UDS.Net.Forms/wwwroot/js/A4a.js
@@ -0,0 +1,10 @@
+﻿
+$('input[type=radio][name="A4a.TRBTIOMARK"]').change(function () {
+
+    if ($(this).val() === '1' || $(this).val() === '9') {
+        $('.treatmentTable').prop('disabled', false);
+    } else {
+        $('.treatmentTable').prop('disabled', true);
+    }
+});
+
