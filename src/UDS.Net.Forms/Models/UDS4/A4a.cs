d999399be98dd24eb083af129c4ff7d58aa775a5
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-20 16:59:20 -0400

    Adds custom validation and validation attribute

--- src/UDS.Net.Forms/Models/UDS4/A4a.cs
+++ src/UDS.Net.Forms/Models/UDS4/A4a.cs
@@ -11,11 +11,13 @@ namespace UDS.Net.Forms.Models.UDS4
 {
     public class A4a : FormModel
     {
-
+        [RequiredOnComplete]
         [Display(Name = "Has the participant ever been prescribed or been enrolled in a clinical trial of a treatment expected to modify ADRD biomarkers?")]
         public int? TRTBIOMARK { get; set; }
 
-        [Display(Name = "Has the participant ever experienced amyloid related imaging abnormalities–edema\n(ARIA-E), amyloid related imaging abnormalities–hemorrhage (ARIA-H), or other major adverse events associated with treatments expected to modify ADRD biomarkers?")]
+        [RequiredIf(nameof(TRTBIOMARK), "1", ErrorMessage = "Please specify adverse events associated with treatments expected to modify ADRD biomarkers.")]
+        [RequiredIf(nameof(TRTBIOMARK), "9", ErrorMessage = "Please specify adverse events associated with treatments expected to modify ADRD biomarkers.")]
+        [Display(Name = "Has the participant ever experienced amyloid related imaging abnormalities–edema (ARIA-E), amyloid related imaging abnormalities–hemorrhage (ARIA-H), or other major adverse events associated with treatments expected to modify ADRD biomarkers?")]
         public int? ADVEVENT { get; set; }
 
         [Display(Name = "Amyloid related imaging abnormalities–edema (ARIA-E) 3a2. 1 Amyloid related")]
@@ -29,7 +31,23 @@ namespace UDS.Net.Forms.Models.UDS4
 
         [MaxLength(60)]
         [ProhibitedCharacters]
+        [RequiredIf(nameof(ADVERSEOTH), "1", ErrorMessage = "Specify other issues.")]
         public string? ADVERSEOTX { get; set; }
+
+        [RequiredIf(nameof(ADVEVENT), "1", ErrorMessage = "Please indicate major adverse event(s) associated with treatments expected to modify ADRD biomarkers.")]
+        [RequiredIf(nameof(ADVEVENT), "9", ErrorMessage = "Please indicate major adverse event(s) associated with treatments expected to modify ADRD biomarkers.")]
+        [NotMapped]
+        public bool? AdverseEventsIndicated
+        {
+            get
+            {
+                if (ARIAE.HasValue || ARIAH.HasValue || ADVERSEOTH.HasValue)
+                {
+                    return true;
+                }
+                else return null;
+            }
+        }
 
         public List<A4aTreatment> Treatment { get; set; } = new List<A4aTreatment>();
 
