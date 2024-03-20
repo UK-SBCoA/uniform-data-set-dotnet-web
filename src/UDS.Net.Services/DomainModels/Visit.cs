e23d0c08f1be21f080a5bfa17a23831105372ca6
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-03-20 13:13:50 -0400

    Add B3 FormContract

--- src/UDS.Net.Services/DomainModels/Visit.cs
+++ src/UDS.Net.Services/DomainModels/Visit.cs
@@ -63,6 +63,7 @@ namespace UDS.Net.Services.DomainModels
                             new FormContract("A2", true),
                             new FormContract("A3", false),
                             new FormContract("A4", true),
+                            new FormContract("B3", true),
                             new FormContract("B4", true),
                             new FormContract("B5", false),
                             new FormContract("B6", false),
