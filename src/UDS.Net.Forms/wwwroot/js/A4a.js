d821fd5ce6f7ca933fb7f4c432b8a566b8a3ffc5
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-22 16:53:27 -0400

    Modifies JS to handle radio buttons

--- src/UDS.Net.Forms/wwwroot/js/A4a.js
+++ src/UDS.Net.Forms/wwwroot/js/A4a.js
@@ -1,18 +1,18 @@
 ﻿$(document).ready(function () {
-    $('input[type=radio][name="CARETRIAL"]').prop('disabled', true);
-    $('input[type=radio][name="TRIALGRP"]').prop('disabled', true);
+    $('input[type=radio][name*="CARETRIAL"]').prop('disabled', true);
+    $('input[type=radio][name*="TRIALGRP"]').prop('disabled', true);
 
     function toggleTreatmentTable(value) {
         var enable = (value == '1' || value == '9');
         $('.treatmentTable').prop('disabled', !enable);
-        $('input[type=radio][name="CARETRIAL"]').prop('disabled', !enable);
-        $('input[type=radio][name="TRIALGRP"]').prop('disabled', !enable);
+        $('input[type=radio][name*="CARETRIAL"]').prop('disabled', !enable);
+        $('input[type=radio][name*="TRIALGRP"]').prop('disabled', !enable);
     }
 
-    $('input[type=radio][name="A4a.TRTBIOMARK"]').change(function () {
+    $('input[type=radio][name*="A4a.TRTBIOMARK"]').change(function () {
         toggleTreatmentTable($(this).val());
     });
 
-    var trtbiomarkValue = $('input[type=radio][name="A4a.TRTBIOMARK"]:checked').val();
+    var trtbiomarkValue = $('input[type=radio][name*="A4a.TRTBIOMARK"]:checked').val();
     toggleTreatmentTable(trtbiomarkValue);
 });
