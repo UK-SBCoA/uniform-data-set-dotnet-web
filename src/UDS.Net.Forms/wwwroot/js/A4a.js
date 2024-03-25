06c31703b7ccacdfb790c4cf1ebf3213fb1cdc16
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-03-25 16:58:43 -0400

    updates TARGETOTX enable/disable JS

--- src/UDS.Net.Forms/wwwroot/js/A4a.js
+++ src/UDS.Net.Forms/wwwroot/js/A4a.js
@@ -1,6 +1,4 @@
 ﻿$(document).ready(function () {
-    $('input[type=radio][name*="CARETRIAL"]').prop('disabled', true);
-    $('input[type=radio][name*="TRIALGRP"]').prop('disabled', true);
 
     function toggleTreatmentTable(value) {
         var enable = (value == '1' || value == '9');
@@ -15,4 +13,21 @@
 
     var trtbiomarkValue = $('input[type=radio][name*="A4a.TRTBIOMARK"]:checked').val();
     toggleTreatmentTable(trtbiomarkValue);
+
+    function toggleTargetotxInput(checkbox) {
+        var isChecked = $(checkbox).is(':checked');
+        var row = $(checkbox).closest('tr');
+        var targetOtxInput = row.find('input[name$="TARGETOTX"]');
+        targetOtxInput.prop('disabled', !isChecked); 
+    }
+
+    $('input[name*="TARGETOTH"]').each(function () {
+        toggleTargetotxInput(this);
+    });
+
+    $('input[name*="TARGETOTH"]').change(function () {
+        toggleTargetotxInput(this);
+    });
+
+    toggleTargetotxInput($('input[name*="TARGETOTH"]:checked'));
 });
