f4abad78c46c850e2eadea3d6ff4a37158306548
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-03-26 15:59:55 -0400

    Add js for PDNORMAL functionality

---
+++ src/UDS.Net.Forms/wwwroot/js/B3.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+  $('#pdNormalCheckbox').change(function () {
+    if ($(this).is(':checked')) {
+      defaultToZero();
+    }
+  });
+  function defaultToZero() {
+    $('input[type="radio"][name^="B3"]').each(function () {
+      if ($(this).val() === "0") {
+        $(this).prop('checked', true).trigger('change');
+      }
+    });
+  } 
+});
