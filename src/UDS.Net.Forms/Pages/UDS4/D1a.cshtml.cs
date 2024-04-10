f29b4aeab3b71066cb5b5a58b6e635d5b1027ebc
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-04-10 17:00:01 -0400

    adds list items progress

--- src/UDS.Net.Forms/Pages/UDS4/D1a.cshtml.cs
+++ src/UDS.Net.Forms/Pages/UDS4/D1a.cshtml.cs
@@ -19,9 +19,58 @@ namespace UDS.Net.Forms.Pages.UDS4
         [BindProperty]
         public D1a D1a { get; set; } = default!;
 
-        public D1aModel(IVisitService visitService, string formKind) : base(visitService, formKind)
+        public D1aModel(IVisitService visitService) : base(visitService, "D1a")
         {
         }
+
+        public List<RadioListItem> DiagnosisMethodListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("Single clinician", "1"),
+            new RadioListItem("Formal consensus panel", "2"),
+            new RadioListItem("Other (e.g., two or more clinicians or other informal group", "3")
+        };
+
+        public List<RadioListItem> NORMCOGListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("No (SKIP TO QUESTION 3)", "0"),
+            new RadioListItem("Yes (CONTINUE TO QUESTION 2a) ", "1")
+
+        };
+
+        public List<RadioListItem> SCDListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("No (END FORM HERE)", "0"),
+            new RadioListItem("Yes", "1")
+
+        };
+
+        public List<RadioListItem> SCDDXCONFListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("No (END FORM HERE)", "0"),
+            new RadioListItem("Yes (END FORM HERE)", "1")
+
+        };
+
+        public List<RadioListItem> DEMENTEDListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("No (CONTINUE TO QUESTION 4)", "0"),
+            new RadioListItem("Yes (SKIP TO QUESTION 6a)", "1")
+
+        };
+
+        public List<RadioListItem> MCIListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("No (CONTINUE TO QUESTION 5)", "0"),
+            new RadioListItem("Yes (SKIP TO QUESTION 6a)", "1")
+
+        };
+
+        public List<RadioListItem> IMPNOMCIListItems { get; set; } = new List<RadioListItem>
+        {
+            new RadioListItem("No (SKIP TO QUESTION 7)", "0"),
+            new RadioListItem("Yes (SKIP TO QUESTION 7)", "1")
+
+        };
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
