469aa062dc94488ba09c5cdb6714fe73c3646717
Ashley Wilson <ashleybot@users.noreply.github.com>
2024-04-10 12:52:22 -0400

    Scaffold playwright project for end-to-end tests

---
+++ src/UDS.Net.Web.MVC.Services.Test/HomepageTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UDS.Net.Web.MVC.Services.Test;
+
+[TestClass]
+public class HomepageTests : PageTest
+{
+    [TestMethod]
+    public async Task HomepageRedirectsIfNotAuthenticated()
+    {
+        await Page.GotoAsync("");
+
+        await Expect(Page).ToHaveURLAsync("");
+    }
+}
+
