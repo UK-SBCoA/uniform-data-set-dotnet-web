e8f277a955d8187cde44860cc90adf32e97f0e7d
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-04-23 16:49:39 -0400

    Removes D1 dtos

--- src/UDS.Net.Services/DomainModels/Forms/D1FormFields.cs
+++ src/UDS.Net.Services/DomainModels/Forms/D1FormFields.cs
@@ -3,6 +3,7 @@ using UDS.Net.Dto;
 
 namespace UDS.Net.Services.DomainModels.Forms
 {
+    [Obsolete]
     public class D1FormFields : IFormFields
     {
         public int? DXMETHOD { get; set; }
@@ -138,139 +139,6 @@ namespace UDS.Net.Services.DomainModels.Forms
         public D1FormFields() { }
         public D1FormFields(FormDto dto)
         {
-            if (dto is D1Dto)
-            {
-                var d1Dto = ((D1Dto)dto);
-                DXMETHOD = d1Dto.DXMETHOD;
-                NORMCOG = d1Dto.NORMCOG;
-                DEMENTED = d1Dto.DEMENTED;
-                AMNDEM = d1Dto.AMNDEM;
-                PCA = d1Dto.PCA;
-                PPASYN = d1Dto.PPASYN;
-                PPASYNT = d1Dto.PPASYNT;
-                FTDSYN = d1Dto.FTDSYN;
-                LBDSYN = d1Dto.LBDSYN;
-                NAMNDEM = d1Dto.NAMNDEM;
-                MCIAMEM = d1Dto.MCIAMEM;
-                MCIAPLUS = d1Dto.MCIAPLUS;
-                MCIAPLAN = d1Dto.MCIAPLAN;
-                MCIAPATT = d1Dto.MCIAPATT;
-                MCIAPEX = d1Dto.MCIAPEX;
-                MCIAPVIS = d1Dto.MCIAPVIS;
-                MCINON1 = d1Dto.MCINON1;
-                MCIN1LAN = d1Dto.MCIN1LAN;
-                MCIN1ATT = d1Dto.MCIN1ATT;
-                MCIN1EX = d1Dto.MCIN1EX;
-                MCIN1VIS = d1Dto.MCIN1VIS;
-                MCINON2 = d1Dto.MCINON2;
-                MCIN2LAN = d1Dto.MCIN2LAN;
-                MCIN2ATT = d1Dto.MCIN2ATT;
-                MCIN2EX = d1Dto.MCIN2EX;
-                MCIN2VIS = d1Dto.MCIN2VIS;
-                IMPNOMCI = d1Dto.IMPNOMCI;
-                AMYLPET = d1Dto.AMYLPET;
-                AMYLCSF = d1Dto.AMYLCSF;
-                FDGAD = d1Dto.FDGAD;
-                HIPPATR = d1Dto.HIPPATR;
-                TAUPETAD = d1Dto.TAUPETAD;
-                CSFTAU = d1Dto.CSFTAU;
-                FDGFTLD = d1Dto.FDGFTLD;
-                TPETFTLD = d1Dto.TPETFTLD;
-                MRFTLD = d1Dto.MRFTLD;
-                DATSCAN = d1Dto.DATSCAN;
-                OTHBIOM = d1Dto.OTHBIOM;
-                OTHBIOMX = d1Dto.OTHBIOMX;
-                IMAGLINF = d1Dto.IMAGLINF;
-                IMAGLAC = d1Dto.IMAGLAC;
-                IMAGMACH = d1Dto.IMAGMACH;
-                IMAGMICH = d1Dto.IMAGMICH;
-                IMAGMWMH = d1Dto.IMAGMWMH;
-                IMAGEWMH = d1Dto.IMAGEWMH;
-                ADMUT = d1Dto.ADMUT;
-                FTLDMUT = d1Dto.FTLDMUT;
-                OTHMUT = d1Dto.OTHMUT;
-                OTHMUTX = d1Dto.OTHMUTX;
-                ALZDIS = d1Dto.ALZDIS;
-                ALZDISIF = d1Dto.ALZDISIF;
-                LBDIS = d1Dto.LBDIS;
-                LBDIF = d1Dto.LBDIF;
-                PARK = d1Dto.PARK;
-                MSA = d1Dto.MSA;
-                MSAIF = d1Dto.MSAIF;
-                PSP = d1Dto.PSP;
-                PSPIF = d1Dto.PSPIF;
-                CORT = d1Dto.CORT;
-                CORTIF = d1Dto.CORTIF;
-                FTLDMO = d1Dto.FTLDMO;
-                FTLDMOIF = d1Dto.FTLDMOIF;
-                FTLDNOS = d1Dto.FTLDNOS;
-                FTLDNOIF = d1Dto.FTLDNOIF;
-                FTLDSUBT = d1Dto.FTLDSUBT;
-                FTLDSUBX = d1Dto.FTLDSUBX;
-                CVD = d1Dto.CVD;
-                CVDIF = d1Dto.CVDIF;
-                PREVSTK = d1Dto.PREVSTK;
-                STROKDEC = d1Dto.STROKDEC;
-                STKIMAG = d1Dto.STKIMAG;
-                INFNETW = d1Dto.INFNETW;
-                INFWMH = d1Dto.INFWMH;
-                ESSTREM = d1Dto.ESSTREM;
-                ESSTREIF = d1Dto.ESSTREIF;
-                DOWNS = d1Dto.DOWNS;
-                DOWNSIF = d1Dto.DOWNSIF;
-                HUNT = d1Dto.HUNT;
-                HUNTIF = d1Dto.HUNTIF;
-                PRION = d1Dto.PRION;
-                PRIONIF = d1Dto.PRIONIF;
-                BRNINJ = d1Dto.BRNINJ;
-                BRNINJIF = d1Dto.BRNINJIF;
-                BRNINCTE = d1Dto.BRNINCTE;
-                HYCEPH = d1Dto.HYCEPH;
-                HYCEPHIF = d1Dto.HYCEPHIF;
-                EPILEP = d1Dto.EPILEP;
-                EPILEPIF = d1Dto.EPILEPIF;
-                NEOP = d1Dto.NEOP;
-                NEOPIF = d1Dto.NEOPIF;
-                NEOPSTAT = d1Dto.NEOPSTAT;
-                HIV = d1Dto.HIV;
-                HIVIF = d1Dto.HIVIF;
-                OTHCOG = d1Dto.OTHCOG;
-                OTHCOGIF = d1Dto.OTHCOGIF;
-                OTHCOGX = d1Dto.OTHCOGX;
-                DEP = d1Dto.DEP;
-                DEPIF = d1Dto.DEPIF;
-                DEPTREAT = d1Dto.DEPTREAT;
-                BIPOLDX = d1Dto.BIPOLDX;
-                BIPOLDIF = d1Dto.BIPOLDIF;
-                SCHIZOP = d1Dto.SCHIZOP;
-                SCHIZOIF = d1Dto.SCHIZOIF;
-                ANXIET = d1Dto.ANXIET;
-                ANXIETIF = d1Dto.ANXIETIF;
-                DELIR = d1Dto.DELIR;
-                DELIRIF = d1Dto.DELIRIF;
-                PTSDDX = d1Dto.PTSDDX;
-                PTSDDXIF = d1Dto.PTSDDXIF;
-                OTHPSY = d1Dto.OTHPSY;
-                OTHPSYIF = d1Dto.OTHPSYIF;
-                OTHPSYX = d1Dto.OTHPSYX;
-                ALCDEMIF = d1Dto.ALCDEMIF;
-                ALCABUSE = d1Dto.ALCABUSE;
-                IMPSUB = d1Dto.IMPSUB;
-                IMPSUBIF = d1Dto.IMPSUBIF;
-                DYSILL = d1Dto.DYSILL;
-                DYSILLIF = d1Dto.DYSILLIF;
-                MEDS = d1Dto.MEDS;
-                MEDSIF = d1Dto.MEDSIF;
-                COGOTH = d1Dto.COGOTH;
-                COGOTHIF = d1Dto.COGOTHIF;
-                COGOTHX = d1Dto.COGOTHX;
-                COGOTH2 = d1Dto.COGOTH2;
-                COGOTH2F = d1Dto.COGOTH2F;
-                COGOTH2X = d1Dto.COGOTH2X;
-                COGOTH3 = d1Dto.COGOTH3;
-                COGOTH3F = d1Dto.COGOTH3F;
-                COGOTH3X = d1Dto.COGOTH3X;
-            }
         }
         public string GetDescription()
         {
