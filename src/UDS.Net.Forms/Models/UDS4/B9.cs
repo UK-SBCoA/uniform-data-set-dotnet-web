7d5d2caf0333d6661bf8cdef225b51bea0390846
mlan225 <17088502+mlan225@users.noreply.github.com>
2024-05-01 16:57:18 -0400

    Add the rest of the validation and small changes

--- src/UDS.Net.Forms/Models/UDS4/B9.cs
+++ src/UDS.Net.Forms/Models/UDS4/B9.cs
@@ -11,14 +11,6 @@ namespace UDS.Net.Forms.Models.UDS4
     /// </summary>
     /// 
 
-
-    //BEHAGE wil need a custom validation
-    //PERCHAGE will need custom validation
-    //PERCHAGE will need custom validation
-    //MOTORAGE will need custom validation
-
-    //Add regex number ranges to number inputs
-
     public class B9 : FormModel
     {
         [RequiredOnComplete(ErrorMessage = "Response required")]
@@ -74,6 +66,7 @@ namespace UDS.Net.Forms.Models.UDS4
         [RequiredIf(nameof(COGOTHR), "1", ErrorMessage = "Value required")]
         public string? COGOTHRX { get; set; }
         [Display(Name = "If any of the cognitive-related behavioral symptoms in 9a-9h are present, at what age did they begin?")]
+        [RegularExpression("^(9|[1-9]\\d|10\\d|110)$", ErrorMessage = "Valid range is 9 - 110")]
         public int? COGAGE { get; set; }
         [Display(Name = "Indicate the mode of onset for the most prominent cognitive problem that is causing the participant's complaints and/or affecting the participant's function.")]
         [RequiredIf(nameof(DECCLCOG), "1", ErrorMessage = "Value required")]
@@ -103,6 +96,7 @@ namespace UDS.Net.Forms.Models.UDS4
         [RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
         public int? BEAGIT { get; set; }
         [Display(Name = "If any of the mood-related behavioral symptoms in 12a-12f are present, at what age did they begin?")]
+        [RegularExpression("^(9|[1-9]\\d|10\\d|110)$", ErrorMessage = "Valid range is 9 - 110")]
         public int? BEHAGE { get; set; }
         [Display(Name = "Participant currently manifests meaningful change in behavior — Psychosis — Visual hallucinations")]
 		[RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
@@ -160,12 +154,14 @@ namespace UDS.Net.Forms.Models.UDS4
 		[RequiredIf(nameof(OTHSUBUSE), "true", ErrorMessage = "Value required")]
 		public string? OTHSUBUSEX { get; set; }
         [Display(Name = "If any of the personality-related behavioral symptoms in 12m-12r are present, at what age did they begin?")]
+        [RegularExpression("^(9|[1-9]\\d|10\\d|110)$", ErrorMessage = "Valid range is 9 - 110")]
         public int? PERCHAGE { get; set; }
         [Display(Name = "Participant currently manifests meaningful change in behavior — REM sleep behavior disorder")]
 		[RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
 		public int? BEREM { get; set; }
         [Display(Name = "IF YES, at what age did the dream enactment behavior begin?")]
         [RequiredIf(nameof(BEREM), "1", ErrorMessage = "Value Required")]
+        [RegularExpression("^(9|[1-9]\\d|10\\d|110)$", ErrorMessage = "Valid range is 9 - 110")]
         public int? BEREMAGO { get; set; }
         [Display(Name = "Was REM sleep behavior disorder confirmed by polysomnography?")]
         [RequiredIf(nameof(BEREM), "1", ErrorMessage = "Value Required")]
@@ -175,12 +171,14 @@ namespace UDS.Net.Forms.Models.UDS4
 		public int? BEOTHR { get; set; }
         [Display(Name = "Participant currently manifests meaningful change in behavior - Other, specify")]
         [RequiredIf(nameof(BEOTHR), "1", ErrorMessage = "Value required")]
+        [MaxLength(60)]
         public string? BEOTHRX { get; set; }
         [Display(Name = "Overall mode of onset for behavioral symptoms")]
         [RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
         public int? BEMODE { get; set; }
         [Display(Name = "Other mode of onset - specify")]
         [RequiredIf(nameof(BEMODE), "4", ErrorMessage = "Value required")]
+        [MaxLength(60)]
         public string? BEMODEX { get; set; }
         [Display(Name = "Based on the clinician’s judgment, is the participant currently experiencing any motor symptoms?")]
         [RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
@@ -207,12 +205,14 @@ namespace UDS.Net.Forms.Models.UDS4
         [RequiredIf(nameof(DECCLMOT), "1", ErrorMessage = "Value required")]
         public int? MOSPEECH { get; set; }
         [Display(Name = "If changes in motor function are present in 15a-15g, at what age did they begin?")]
+        [RegularExpression("^(9|[1-9]\\d|10\\d|110)$", ErrorMessage = "Valid range is 9 - 110")]
         public int? MOTORAGE { get; set; }
         [Display(Name = "Indicate the mode of onset for the most prominent motor problem that is causing the participant's complaints and/or affecting the participant's function.")]
         [RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
         public int? MOMODE { get; set; }
         [Display(Name = "Indicate mode of onset for the most prominent motor problem that is causing the participant's complains and or affecting the participant's function - Other, specify")]
         [RequiredIf(nameof(MOMODE), "4", ErrorMessage = "Value required")]
+        [MaxLength(60)]
         public string? MOMODEX { get; set; }
         [Display(Name = "Were changes in motor function suggestive of parkinsonism?")]
         [RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
@@ -227,30 +227,48 @@ namespace UDS.Net.Forms.Models.UDS4
         [RequiredIf(nameof(DECCLBE), "1", ErrorMessage = "Value required")]
         public int? FRSTCHG { get; set; }
         [NotMapped]
-        [RequiredOnComplete(ErrorMessage = "Response required")]
+        [RequiredIf(nameof(BESUBAB), "1", ErrorMessage = "Please select at least one substance")]
+        public bool? BESUBABCheckboxValidation
+        {
+            get
+            {
+                if(ALCUSE == false && SEDUSE == false && OPIATEUSE == false && COCAINEUSE == false && OTHSUBUSE == false)
+                {
+                    return null;
+                }
+
+                return true;
+            }
+        }
+        [NotMapped]
+        [RequiredOnComplete(ErrorMessage = "Invalid Response")]
         public bool? COGAGESymptomsPresent
         {
             get
             {
                 if(COGMEM == 1 || COGORI == 1 || COGJUDG == 1 || COGLANG == 1 || COGVIS == 1 || COGATTN == 1 || COGFLUC == 1 || !String.IsNullOrEmpty(COGOTHRX))
                 {
-                    if(COGAGE > 0)
+                    if(COGAGE < 0)
                     {
                         return true;
                     }
 
-                    else
+                    return null;
+                }
+                else
+                {
+                    if(COGAGE > 0)
                     {
                         return null;
                     }
-                }
 
-                return true;
+                    return true;
+                }
             }
         }
 
         [NotMapped]
-        [RequiredOnComplete(ErrorMessage = "Response required")]
+        [RequiredOnComplete(ErrorMessage = "Invalid Response")]
         public bool? BEHAGESymptomsPresent
         {
             get
@@ -262,18 +280,22 @@ namespace UDS.Net.Forms.Models.UDS4
                         return true;
                     }
 
-                    else
+                    return null;
+                }
+                else
+                {
+                    if (BEHAGE > 0)
                     {
                         return null;
                     }
-                }
 
-                return true;
+                    return true;
+                }
             }
         }
 
         [NotMapped]
-        [RequiredOnComplete(ErrorMessage = "Response required")]
+        [RequiredOnComplete(ErrorMessage = "Invalid Response")]
         public bool? PERCHAGESymptomsPresent
         {
             get
@@ -285,19 +307,23 @@ namespace UDS.Net.Forms.Models.UDS4
                         return true;
                     }
 
-                    else
+                    return null;
+                }
+                else
+                {
+                    if (PERCHAGE > 0)
                     {
                         return null;
                     }
-                }
 
-                return true;
+                    return true;
+                }
             }
         }
 
 
         [NotMapped]
-        [RequiredOnComplete(ErrorMessage = "Response required")]
+        [RequiredOnComplete(ErrorMessage = "Invalid Response")]
         public bool? MOTORAGEMotorChangesPresent
         {
             get
@@ -309,13 +335,17 @@ namespace UDS.Net.Forms.Models.UDS4
                         return true;
                     }
 
-                    else
+                    return null;
+                }
+                else
+                {
+                    if (MOTORAGE > 0)
                     {
                         return null;
                     }
-                }
 
-                return true;
+                    return true;
+                }
             }
         }
 
