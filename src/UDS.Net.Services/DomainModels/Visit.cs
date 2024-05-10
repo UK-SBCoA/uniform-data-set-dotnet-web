13a8a1f3cb3541f98c78c992f112040ab561d0d3
mlan225 <17088502+mlan225@users.noreply.github.com>
2024-05-10 14:35:10 -0400

    Fix whitespace formatting

--- src/UDS.Net.Services/DomainModels/Visit.cs
+++ src/UDS.Net.Services/DomainModels/Visit.cs
@@ -7,206 +7,206 @@ using UDS.Net.Services.Enums;
 
 namespace UDS.Net.Services.DomainModels
 {
-    /// <summary>
-    /// Visit domain model
-    /// </summary>
-    public class Visit
-    {
-        public int Id { get; set; }
+	/// <summary>
+	/// Visit domain model
+	/// </summary>
+	public class Visit
+	{
+		public int Id { get; set; }
 
-        public int ParticipationId { get; set; }
+		public int ParticipationId { get; set; }
 
-        public int Number { get; set; }
+		public int Number { get; set; }
 
-        public string Version { get; set; } = "";
+		public string Version { get; set; } = "";
 
-        public VisitKind Kind { get; set; }
+		public VisitKind Kind { get; set; }
 
-        public DateTime StartDateTime { get; set; }
+		public DateTime StartDateTime { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+		public DateTime CreatedAt { get; set; }
 
-        public string CreatedBy { get; set; }
+		public string CreatedBy { get; set; }
 
-        public string ModifiedBy { get; set; }
+		public string ModifiedBy { get; set; }
 
-        public string DeletedBy { get; set; }
+		public string DeletedBy { get; set; }
 
-        public bool IsDeleted { get; set; }
+		public bool IsDeleted { get; set; }
 
-        // Read more https://github.com/dotnet/aspnetcore/blob/6d30638626ff0f471f431ae2247ce95480e418ef/src/Mvc/Mvc.Abstractions/src/ModelBinding/ModelStateDictionary.cs#L616
-        public bool IsValid
-        {
-            get
-            {
-                return GetValidity("", 0);
-            }
+		// Read more https://github.com/dotnet/aspnetcore/blob/6d30638626ff0f471f431ae2247ce95480e418ef/src/Mvc/Mvc.Abstractions/src/ModelBinding/ModelStateDictionary.cs#L616
+		public bool IsValid
+		{
+			get
+			{
+				return GetValidity("", 0);
+			}
 
-        }
+		}
 
-        public Participation Participation { get; set; } = new Participation();
+		public Participation Participation { get; set; } = new Participation();
 
-        public IList<Form> Forms { get; set; } = new List<Form>();
+		public IList<Form> Forms { get; set; } = new List<Form>();
 
-        private void BuildFormsContract(string version, VisitKind kind, IList<Form> existingForms)
-        {
-            if (version == "UDS4")
-            {
-                Dictionary<string, FormContract[]> UDS4 = new Dictionary<string, FormContract[]>
-                {
-                    {
-                        VisitKind.IVP.ToString(),
-                        new FormContract[]
-                        {
-                            new FormContract("A1", true),
-                            new FormContract("A1a", false),
-                            new FormContract("A2", true),
-                            new FormContract("A3", false),
-                            new FormContract("A4", true),
-                            new FormContract("A4a", true),
-                            new FormContract("B1",true),
-                            new FormContract("B3", true),
-                            new FormContract("B4", true),
-                            new FormContract("B5", false),
-                            new FormContract("B6", false),
-                            new FormContract("B7", false),
-                            new FormContract("B8", true),
-                            new FormContract("B9", false),
-                            new FormContract("C2", true)
+		private void BuildFormsContract(string version, VisitKind kind, IList<Form> existingForms)
+		{
+			if (version == "UDS4")
+			{
+				Dictionary<string, FormContract[]> UDS4 = new Dictionary<string, FormContract[]>
+				{
+					{
+						VisitKind.IVP.ToString(),
+						new FormContract[]
+						{
+							new FormContract("A1", true),
+							new FormContract("A1a", false),
+							new FormContract("A2", true),
+							new FormContract("A3", false),
+							new FormContract("A4", true),
+							new FormContract("A4a", true),
+							new FormContract("B1",true),
+							new FormContract("B3", true),
+							new FormContract("B4", true),
+							new FormContract("B5", false),
+							new FormContract("B6", false),
+							new FormContract("B7", false),
+							new FormContract("B8", true),
+							new FormContract("B9", false),
+							new FormContract("C2", true)
 
-                        }
-                    },
-                    {
-                        VisitKind.FVP.ToString(),
-                        new FormContract[]
-                        {
-                            new FormContract("A1", true),
-                            new FormContract("A2", true),
-                            new FormContract("A3", false),
-                            new FormContract("A4", true),
-                            new FormContract("A4a", true),
-                            new FormContract("B4", true),
-                            new FormContract("B5", false),
-                            new FormContract("B6", false),
-                            new FormContract("B7", false),
-                            new FormContract("B8", true),
-                            new FormContract("B9", false),
-                            new FormContract("C2", true)
-                        }
-                    },
-                    {
-                        VisitKind.TIP.ToString(),
-                        new FormContract[]
-                        {
-                            new FormContract("A1", true),
-                            new FormContract("A1a", false),
-                            new FormContract("A2", true),
-                            new FormContract("A3", false),
-                            new FormContract("A4", true),
-                            new FormContract("A4a", true),
-                            new FormContract("B4", true),
-                            new FormContract("B5", false),
-                            new FormContract("B6", false),
-                            new FormContract("B7", false),
-                            new FormContract("B8", true),
-                            new FormContract("B9", false)
-                        }
-                    },
-                    {
-                        VisitKind.TFP.ToString(),
-                        new FormContract[]
-                        {
-                            new FormContract("A1", true),
-                            new FormContract("A2", true),
-                            new FormContract("A3", false),
-                            new FormContract("A4", true),
-                            new FormContract("A4a", true),
-                            new FormContract("B4", true),
-                            new FormContract("B5", false),
-                            new FormContract("B6", false),
-                            new FormContract("B7", false),
-                            new FormContract("B8", false),
-                            new FormContract("B9", false)
-                        }
-                    }
-                };
+						}
+					},
+					{
+						VisitKind.FVP.ToString(),
+						new FormContract[]
+						{
+							new FormContract("A1", true),
+							new FormContract("A2", true),
+							new FormContract("A3", false),
+							new FormContract("A4", true),
+							new FormContract("A4a", true),
+							new FormContract("B4", true),
+							new FormContract("B5", false),
+							new FormContract("B6", false),
+							new FormContract("B7", false),
+							new FormContract("B8", true),
+							new FormContract("B9", false),
+							new FormContract("C2", true)
+						}
+					},
+					{
+						VisitKind.TIP.ToString(),
+						new FormContract[]
+						{
+							new FormContract("A1", true),
+							new FormContract("A1a", false),
+							new FormContract("A2", true),
+							new FormContract("A3", false),
+							new FormContract("A4", true),
+							new FormContract("A4a", true),
+							new FormContract("B4", true),
+							new FormContract("B5", false),
+							new FormContract("B6", false),
+							new FormContract("B7", false),
+							new FormContract("B8", true),
+							new FormContract("B9", false)
+						}
+					},
+					{
+						VisitKind.TFP.ToString(),
+						new FormContract[]
+						{
+							new FormContract("A1", true),
+							new FormContract("A2", true),
+							new FormContract("A3", false),
+							new FormContract("A4", true),
+							new FormContract("A4a", true),
+							new FormContract("B4", true),
+							new FormContract("B5", false),
+							new FormContract("B6", false),
+							new FormContract("B7", false),
+							new FormContract("B8", false),
+							new FormContract("B9", false)
+						}
+					}
+				};
 
-                var formDefinitions = UDS4.Where(u => u.Key == kind.ToString()).FirstOrDefault();
+				var formDefinitions = UDS4.Where(u => u.Key == kind.ToString()).FirstOrDefault();
 
-                foreach (var formContract in formDefinitions.Value)
-                {
-                    bool hasExisting = false;
+				foreach (var formContract in formDefinitions.Value)
+				{
+					bool hasExisting = false;
 
-                    if (existingForms != null && existingForms.Count() > 0)
-                    {
-                        hasExisting = existingForms.Where(f => f.Kind == formContract.Abbreviation).Any();
-                    }
+					if (existingForms != null && existingForms.Count() > 0)
+					{
+						hasExisting = existingForms.Where(f => f.Kind == formContract.Abbreviation).Any();
+					}
 
-                    if (hasExisting)
-                    {
-                        var existing = existingForms.Where(f => f.Kind == formContract.Abbreviation).FirstOrDefault();
+					if (hasExisting)
+					{
+						var existing = existingForms.Where(f => f.Kind == formContract.Abbreviation).FirstOrDefault();
 
-                        Forms.Add(new Form(Id, existing.Id, existing.Title, existing.Kind, formContract.IsRequredForVisitKind, existing.Status, existing.Language, existing.ReasonCode, existing.CreatedAt, existing.CreatedBy, existing.ModifiedBy, existing.DeletedBy, existing.IsDeleted, existing.Fields));
-                    }
-                    else
-                    {
-                        Forms.Add(new Form(Id, formContract.Abbreviation, formContract.IsRequredForVisitKind, CreatedBy));
-                    }
-                }
+						Forms.Add(new Form(Id, existing.Id, existing.Title, existing.Kind, formContract.IsRequredForVisitKind, existing.Status, existing.Language, existing.ReasonCode, existing.CreatedAt, existing.CreatedBy, existing.ModifiedBy, existing.DeletedBy, existing.IsDeleted, existing.Fields));
+					}
+					else
+					{
+						Forms.Add(new Form(Id, formContract.Abbreviation, formContract.IsRequredForVisitKind, CreatedBy));
+					}
+				}
 
 
-            }
-        }
+			}
+		}
 
-        private bool GetValidity(string node, int currentDepth)
-        {
-            // TODO check each property if it has been validated or if it's unvalidated
-            // then if unvalidated, check if it is invalid
-            return true;
-        }
+		private bool GetValidity(string node, int currentDepth)
+		{
+			// TODO check each property if it has been validated or if it's unvalidated
+			// then if unvalidated, check if it is invalid
+			return true;
+		}
 
-        public int ErrorCount { get; private set; }
+		public int ErrorCount { get; private set; }
 
-        public int Count { get; private set; }
+		public int Count { get; private set; }
 
-        public Visit(int id, int number, int participationId, string version, VisitKind kind, DateTime startDateTime, DateTime createdAt, string createdBy, string modifiedBy, string deletedBy, bool isDeleted, IList<Form> existingForms)
-        {
-            Id = id;
-            Number = number;
-            ParticipationId = participationId;
-            Version = version;
-            Kind = kind;
-            StartDateTime = startDateTime;
-            CreatedAt = createdAt;
-            CreatedBy = createdBy;
-            ModifiedBy = modifiedBy;
-            DeletedBy = deletedBy;
-            IsDeleted = IsDeleted;
+		public Visit(int id, int number, int participationId, string version, VisitKind kind, DateTime startDateTime, DateTime createdAt, string createdBy, string modifiedBy, string deletedBy, bool isDeleted, IList<Form> existingForms)
+		{
+			Id = id;
+			Number = number;
+			ParticipationId = participationId;
+			Version = version;
+			Kind = kind;
+			StartDateTime = startDateTime;
+			CreatedAt = createdAt;
+			CreatedBy = createdBy;
+			ModifiedBy = modifiedBy;
+			DeletedBy = deletedBy;
+			IsDeleted = IsDeleted;
 
-            if (existingForms == null)
-                existingForms = new List<Form>();
+			if (existingForms == null)
+				existingForms = new List<Form>();
 
-            BuildFormsContract(Version, Kind, existingForms);
+			BuildFormsContract(Version, Kind, existingForms);
 
-        }
+		}
 
-        // TODO There's form fields and then there's validation rules for the form fields based on visit type
-        // look up generics builder
-        // https://stackoverflow.com/questions/30895888/setting-common-base-class-properties-when-creating-objects
-        // TODO Use notifications pattern to return errors and not throwing exceptions
+		// TODO There's form fields and then there's validation rules for the form fields based on visit type
+		// look up generics builder
+		// https://stackoverflow.com/questions/30895888/setting-common-base-class-properties-when-creating-objects
+		// TODO Use notifications pattern to return errors and not throwing exceptions
 
-        public bool TryValidate()
-        {
-            // TODO validate the visit against any rules that might have changed due to form fields changing
-            GetModelErrors();
-            return true;
-        }
+		public bool TryValidate()
+		{
+			// TODO validate the visit against any rules that might have changed due to form fields changing
+			GetModelErrors();
+			return true;
+		}
 
-        public IEnumerable<VisitValidationResult> GetModelErrors()
-        {
-            /// TODO For UDS3 FVP, either C1 or C2 is required, but not both
-            yield break;
-        }
-    }
+		public IEnumerable<VisitValidationResult> GetModelErrors()
+		{
+			/// TODO For UDS3 FVP, either C1 or C2 is required, but not both
+			yield break;
+		}
+	}
 }
 
