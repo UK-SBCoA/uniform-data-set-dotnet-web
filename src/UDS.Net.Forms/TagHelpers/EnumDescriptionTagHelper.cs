f627c4cf76c7639893adf11a5a289021773a24ae
Ashley Wilson <ashleybot@users.noreply.github.com>
2024-07-01 08:52:56 -0400

    Styling for summary and easy rendering of enum descriptions

---
+++ src/UDS.Net.Forms/TagHelpers/EnumDescriptionTagHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Reflection.Emit;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using UDS.Net.Forms.Extensions;
+
+namespace UDS.Net.Forms.TagHelpers
+{
+    [HtmlTargetElement(Attributes = DescriptionForAttributeName)]
+    public class EnumDescriptionTagHelper : TagHelper
+    {
+        private const string DescriptionForAttributeName = "enum-description-for";
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        protected IHtmlGenerator Generator { get; }
+
+        [HtmlAttributeName(DescriptionForAttributeName)]
+        public ModelExpression DescriptionFor { get; set; }
+
+        public EnumDescriptionTagHelper(IHtmlGenerator generator)
+        {
+            Generator = generator;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var metadata = DescriptionFor.Metadata;
+
+            if (metadata == null)
+            {
+                throw new InvalidOperationException(string.Format("No property provided ({0})", DescriptionForAttributeName));
+            }
+
+            string description = metadata.Description; // value if there is a display-for
+
+            var modelType = DescriptionFor.ModelExplorer.ModelType; // type
+
+            if (modelType.IsEnum)
+            {
+                description = ((Enum)DescriptionFor.ModelExplorer.Model).GetDescription();
+            }
+
+            output.Attributes.SetAttribute("id", metadata.PropertyName + "-description");
+
+            if (!string.IsNullOrWhiteSpace(description))
+            {
+                output.Content.SetContent(description);
+                output.TagMode = TagMode.StartTagAndEndTag;
+            }
+        }
+    }
+}
+
--- src/UDS.Net.Services/Enums/FormStatus.cs
+++ src/UDS.Net.Services/Enums/FormStatus.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel;
+
 namespace UDS.Net.Services.Enums
 {
     public enum FormStatus
     {
+        [Description("Not started")]
         NotStarted,
+        [Description("In progress")]
         InProgress,
+        [Description("Complete")]
         Complete,
+        [Description("Not included")]
         NotIncluded
     }
 }
