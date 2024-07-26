f7ed291e141686d055a5b927bf593e00c3a809ff
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-07-26 15:28:39 -0400

    updates JS to not set milestonestype

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/MilestoneTypeBehavior.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/MilestoneTypeBehavior.js
@@ -1,77 +1,30 @@
-﻿//MilestoneTypeBehavior.js is an alternative to the edit/create pageModel Behavior for Milestone.
-//This file will disable and enable fields based on the milestoneType while also
-//setting the milestoneType value when loading an existing form by checking the Deceased and Discont values
-//if either deceased or discont are select, milestoneType == NO FURTHER CONTACT
-
-class MilestoneTypeBehavior {
-    SetMilestoneType() {
-        let milestoneTypes = $("input[name='Milestone.MilestoneType']")
-        let deceasedInput = $("#Milestone_DECEASED")
-        let discontInput = $("#Milestone_DISCONT")
-
-        if ($(deceasedInput).is(':checked') || $(discontInput).is(':checked')) {
-            console.log('test')
-            $('#Milestone\\.MilestoneType\\[1\\]').prop("checked", true)
-            this.DisableBoxA()
-        } else {
-            $('#Milestone\\.MilestoneType\\[0\\]').prop("checked", true)
-            this.DisableBoxB()
-        }
-    }
-
-    DisableSectionOnChange() {
-        let milestoneTypeValue = $('input[name="Milestone.MilestoneType"]:checked').val()
+﻿class MilestoneTypeBehavior {
+    disableSectionOnChange() {
+        let milestoneTypeValue = $('input[name="Milestone.MILESTONETYPE"]:checked').val();
         if (milestoneTypeValue == 0) {
-            this.DisableBoxA()
-        }
-        if (milestoneTypeValue == 1) {
-            this.DisableBoxB()
+            this.disableBoxA();
+        } else if (milestoneTypeValue == 1) {
+            this.disableBoxB();
         }
     }
 
-    DisableBoxA() {
-        $(".boxA input").prop("disabled", true)
-        $(".boxB input").prop("disabled", false)
+    disableBoxA() {
+        $(".boxA input").prop("disabled", true);
+        $(".boxB input").prop("disabled", false);
     }
 
-    DisableBoxB() {
-        $(".boxA input").prop("disabled", false)
-        $(".boxB input").prop("disabled", true)
+    disableBoxB() {
+        $(".boxA input").prop("disabled", false);
+        $(".boxB input").prop("disabled", true);
     }
 }
 
 $(() => {
-    const milestoneTypeBehavior = new MilestoneTypeBehavior()
+    const milestoneTypeBehavior = new MilestoneTypeBehavior();
 
-    milestoneTypeBehavior.SetMilestoneType()
+    $("input[name='Milestone.MILESTONETYPE']").on("change", () => {
+        milestoneTypeBehavior.disableSectionOnChange();
+    });
 
-    $("input[name='Milestone.MilestoneType']").on("change", () => {
-        milestoneTypeBehavior.DisableSectionOnChange()
-    })
-})
-
-//class MilestoneTypeBehavior {
-//    SetMilestoneType() {
-//        let deceasedInput = $("#Milestone_DECEASED")
-//        let discontInput = $("#Milestone_DISCONT")
-
-//        if ($(deceasedInput).is(':checked') || $(discontInput).is(':checked')) {
-//            $('#Milestone\\.MilestoneType\\[1\\]').prop("checked", true)
-//        } else {
-//            $('#Milestone\\.MilestoneType\\[0\\]').prop("checked", true)
-//        }
-//    }
-//}
-
-//$(() => {
-//    const milestoneTypeBehavior = new MilestoneTypeBehavior()
-
-//    // Wait for the DOM to be fully loaded before setting the MilestoneType
-//    $(document).ready(() => {
-//        milestoneTypeBehavior.SetMilestoneType()
-//    });
-
-//    $("input[name='Milestone.MilestoneType']").on("change", () => {
-//        milestoneTypeBehavior.DisableSectionOnChange()
-//    })
-//})
+    milestoneTypeBehavior.disableSectionOnChange();
+});
