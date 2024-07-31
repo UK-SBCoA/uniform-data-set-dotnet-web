85ce962f0ffb90d4ba0ce1b43065d905062196e2
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-07-31 17:06:27 -0400

    updates formatting

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/MilestoneTypeBehavior.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/MilestoneTypeBehavior.js
@@ -1,32 +1,32 @@
 ﻿class MilestoneTypeBehavior {
-  disableSectionOnChange() {
-    let milestoneTypeValue = $(
-      'input[name="Milestone.MILESTONETYPE"]:checked'
-    ).val();
-    if (milestoneTypeValue == 0) {
-      this.disableBoxA();
-    } else if (milestoneTypeValue == 1) {
-      this.disableBoxB();
+    disableSectionOnChange() {
+        let milestoneTypeValue = $(
+            'input[name="Milestone.MILESTONETYPE"]:checked',
+        ).val();
+        if (milestoneTypeValue == 0) {
+            this.disableBoxA();
+        } else if (milestoneTypeValue == 1) {
+            this.disableBoxB();
+        }
     }
-  }
 
-  disableBoxA() {
-    $(".boxA input").prop("disabled", true);
-    $(".boxB input").prop("disabled", false);
-  }
+    disableBoxA() {
+        $(".boxA input").prop("disabled", true);
+        $(".boxB input").prop("disabled", false);
+    }
 
-  disableBoxB() {
-    $(".boxA input").prop("disabled", false);
-    $(".boxB input").prop("disabled", true);
-  }
+    disableBoxB() {
+        $(".boxA input").prop("disabled", false);
+        $(".boxB input").prop("disabled", true);
+    }
 }
 
 $(() => {
-  const milestoneTypeBehavior = new MilestoneTypeBehavior();
+    const milestoneTypeBehavior = new MilestoneTypeBehavior();
 
-  $("input[name='Milestone.MILESTONETYPE']").on("change", () => {
+    $("input[name='Milestone.MILESTONETYPE']").on("change", () => {
+        milestoneTypeBehavior.disableSectionOnChange();
+    });
+
     milestoneTypeBehavior.disableSectionOnChange();
-  });
-
-  milestoneTypeBehavior.disableSectionOnChange();
 });
