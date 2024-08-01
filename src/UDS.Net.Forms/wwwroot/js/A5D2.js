1565c317c01302e5b4014d91bec9795ff59bd50b
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2024-08-01 14:00:55 -0400

    Formatting update

--- src/UDS.Net.Forms/wwwroot/js/A5D2.js
+++ src/UDS.Net.Forms/wwwroot/js/A5D2.js
@@ -1,22 +1,30 @@
 ﻿/*Implemented on questions 6a1 6a2 and 6a3 form A5D2*/
 
 $(document).ready(function () {
-
-  var majorDepRadios = Array.from(document.getElementsByName('A5D2.MAJORDEP'));
-  var otherDepRadios = Array.from(document.getElementsByName('A5D2.OTHERDEP'));
-  var deprTreatRadios = Array.from(document.getElementsByName('A5D2.DEPRTREAT'));
-
+  var majorDepRadios = Array.from(document.getElementsByName("A5D2.MAJORDEP"));
+  var otherDepRadios = Array.from(document.getElementsByName("A5D2.OTHERDEP"));
+  var deprTreatRadios = Array.from(
+    document.getElementsByName("A5D2.DEPRTREAT"),
+  );
 
   function toggleDeprTreat() {
-    const recentMajorDep = majorDepRadios.some(radio => radio.checked && radio.value == '1');
-    const recentOtherDep = otherDepRadios.some(radio => radio.checked && radio.value == '1');
+    const recentMajorDep = majorDepRadios.some(
+      (radio) => radio.checked && radio.value == "1",
+    );
+    const recentOtherDep = otherDepRadios.some(
+      (radio) => radio.checked && radio.value == "1",
+    );
 
-  deprTreatRadios.forEach(radio => {
-      radio.disabled = !(recentMajorDep || recentOtherDep)
+    deprTreatRadios.forEach((radio) => {
+      radio.disabled = !(recentMajorDep || recentOtherDep);
     });
   }
-  majorDepRadios.forEach(radio => radio.addEventListener('change', toggleDeprTreat));
-  otherDepRadios.forEach(radio => radio.addEventListener('change', toggleDeprTreat));
+  majorDepRadios.forEach((radio) =>
+    radio.addEventListener("change", toggleDeprTreat),
+  );
+  otherDepRadios.forEach((radio) =>
+    radio.addEventListener("change", toggleDeprTreat),
+  );
 
   toggleDeprTreat();
 });
