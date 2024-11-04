37146a9a4e1489f96cb1d96ee9ca36698b961701
Ashley Wilson <ashleybot@users.noreply.github.com>
2024-11-04 10:56:48 -0500

    Enhance enum with descriptions

--- src/UDS.Net.Services/Enums/PacketKind.cs
+++ src/UDS.Net.Services/Enums/PacketKind.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel;
+
 namespace UDS.Net.Services.Enums
 {
     public enum PacketKind
     {
+        [Description("Initial")]
         I,
+        [Description("Follow-up")]
         F
     }
 }
