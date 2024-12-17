7453b8c3a1067c37c758ddf21a86889e26c26d50
Ashley Wilson <ashleybot@users.noreply.github.com>
2024-12-17 14:11:23 -0500

    Cleanup

--- src/UDS.Net.Web.MVC.Services.Test/HomepageTests.cs
+++ src/UDS.Net.Web.MVC.Services.Test/HomepageTests.cs
@@ -6,20 +6,55 @@ namespace UDS.Net.Web.MVC.Services.Test;
 [TestClass]
 public class HomepageTests : PageTest
 {
+    int _salt = 0;
+
+    [TestInitialize]
+    public async Task Initiatize()
+    {
+        await Page.GotoAsync("https://localhost:4811");
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+
+        Random random = new Random();
+        _salt = random.Next(1, 1000);
+
+        string emailAddress = $"test{_salt}@test.com";
+        string password = $"Password_{_salt}";
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Register as a new user" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync(emailAddress);
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync(password);
+        await Page.GetByLabel("Confirm Password").ClickAsync();
+        await Page.GetByLabel("Confirm Password").FillAsync(password);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+        await Page.WaitForRequestFinishedAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync(emailAddress);
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByPlaceholder("password").FillAsync(password);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+
+    }
+
     [TestMethod]
     public async Task HomepageHasNavigationToParticipations()
     {
-        await Page.GotoAsync("/");
+        await Page.Locator("#desktop-menu").GetByRole(AriaRole.Link, new() { Name = "Participation" }).ClickAsync();
 
-        //var participation = Page.GetByRole(AriaRole.Link, new() { Name = "Participation" });
+        await Expect(Page.GetByRole(AriaRole.Table)).ToBeVisibleAsync();
+    }
 
-        //await Expect(participation).ToHaveAttributeAsync("href", "/Participations");
+    [TestMethod]
+    public async Task HomepageHasNavigationToVisits()
+    {
+        await Page.Locator("#desktop-menu").GetByRole(AriaRole.Link, new() { Name = "Visits" }).ClickAsync();
 
-        //var createButton = Page.GetByRole(AriaRole.Link, new() { Name = "New participation" });
-
-        //await Expect(createButton).ToHaveAttributeAsync("href", "/Participations/Create");
-
-        //await participation.ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Table)).ToBeVisibleAsync();
     }
 }
 
