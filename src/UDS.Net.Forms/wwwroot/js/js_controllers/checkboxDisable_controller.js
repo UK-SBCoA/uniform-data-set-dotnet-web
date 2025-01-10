24ec95f37f98517d748c48d89f32013ad470c4bd
Cody Penn <96854090+Oddvocado@users.noreply.github.com>
2025-01-10 13:32:10 -0500

    Additional a1 validation (#218)
    * adjust stimulus controller for multiple properties
    
    * Add GENDKN UI behavior
    
    * adjust stimulus to handle multiple targets
    
    * adds additional validation
    
    * Adds model validation for question 15 & 16
    
    * Add additional property to validation rule
    
    * additional model validation properties
    
    * fix counter
    
    * fix for turbo form submission bug
    
    * typo
    
    * update stimulus on page load
    
    * edge case model validation
    
    * additional server side validation
    
    * Add validation error message displays close to the inputs
    
    * remove unmapped bool validations
    
    * Fix tag helper
    
    * add back bool validation question 4 & 7
    
    * linter fix
    
    * Fixes incorrect ui behaviors on GENOTHX
    
    * Updated stimulus to be handle more complex situations when inputs both need to be enabled and another set of inputs need to be disabled
    
    * adjustments to stimulus targets
    
    * Update A1a
    
    * updates documentation
    
    * removes outdated documentation
    
    * remove camel case for OnLoad variables
    
    * adds additional method to replace 'data affects' on page load
    
    * Simplify code by calling togglegroup on every checkboxDisable item
    
    * Change simple ui behaviors back to data-affects
    
    * Add note of where to add a check if checkbox is disabled
    
    * If checkbox is disabled, don't run togglegroup
    
    * Version bump
    
    ---------
    
    Co-authored-by: Ashley Wilson <ashleybot@users.noreply.github.com>

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/checkboxDisable_controller.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/checkboxDisable_controller.js
@@ -3,17 +3,23 @@ DESCRIPTION:
 checkboxDisable_controller.js is for custom UI behavior for disabling and enabling inputs by their name attribute when a checkbox is checked, using stimulus.js
 
 HTML USAGE:
-<div>
-    @Html.CheckBox("A1a.EXPNOTAPP", Model.A1a.EXPNOTAPP, new {@class = "h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600",
-        data_checkBoxDisable_target = "checkboxTrigger",
-        data_action = "input->checkboxDisable#ToggleGroup",
-        data_checkBoxDisable_group_param = "A1a.EXPSTRS",
-        data_checkboxDisable_disable_param = "true"})
-</div>
+     <div class="flex h-6 items-center">
+                 @Html.CheckBox("A1.GENNOANS", Model.A1.GENNOANS, new
+                     {
+                         @class = "h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none",
+                         data_checkBoxDisable_target = "checkboxTrigger",
+                         data_action = "input->checkboxDisable#ToggleGroup",
+                         data_checkBoxDisable_enableGroup_param = "",
+                         data_checkBoxDisable_disableGroup_param = "A1.GENMAN,A1.GENWOMAN,A1.GENTRMAN,A1.GENTRWOMAN,A1.GENNONBI,A1.GENOTH,A1.GENDKN,A1.GENOTH",
+                         data_checkboxDisable_toggleState_param = "true"
+                     })
+             </div>
 
-STIMULUS PARAMETERS:
-data_checkboxDisable_group_param = the name of the input(s) you are wanting to disable
-data_checkboxDisable_disable_param = a flag for if clicking the checkbox disables or enables the group. true = disable on check and false = enable on check
+*/
+
+/*
+ TODO This controller was initally created to support the A1a form. The complexity grew when the code was modified to support the A1 form as well.
+ We should be able to refactor this so that it uses the stimulus targets instead of the the group params that are currently used.
 */
 
 import { Controller } from '@hotwired/stimulus';
@@ -25,44 +31,77 @@ export default class extends Controller {
     this.OnLoad()
   }
 
-  ToggleGroup({ params: { group, disable } }) {
+  /*
+    There are only two states to a checkbox. We need:
+    1. Which state will control the toggle (currently named disable)
+    2. When the toggle is activated then EnableGroup inputs will be enabled
+    3. When the toggle is activated then DisableGroup inputs will be disabled
+  */
 
-    if (group == undefined || disable == undefined) {
-      return console.error(`Missing required parameter of group or disable on a target element with id: ${this.checkboxTriggerTarget.id}`)
+  IterateAndSetDisable(group, disableValue) {
+    //split the group parameter into an array of property names
+    if (group == undefined) {
+      return console.log('Nothing here to change the state to');
+    }
+    var propertyNames = group.split(',')
+
+    propertyNames.forEach(propertyName => {
+      //get all elements that are to be affected by checkbox using javascript
+      var targetElements = document.getElementsByName(propertyName.trim());
+
+      if (targetElements.length == undefined) {
+        return console.warn('No targets found to be affected by checkbox of id: ${checkbox.id} for property: ${propertyName}')
+      }
+
+      targetElements.forEach((element) => {
+        element.disabled = disableValue;
+      })
+    });
+  }
+
+  /*
+    ToggleGroup is called when the checkbox is changed
+  */
+  ToggleGroup(event) {
+    const { enablegroup, disablegroup, togglestate } = event.params;
+    const checkbox = event.currentTarget;
+
+    var isChecked = checkbox.checked
+
+    if (isChecked == togglestate) {
+      this.IterateAndSetDisable(enablegroup, false);
+      this.IterateAndSetDisable(disablegroup, true);
+    }
+    else {
+      this.IterateAndSetDisable(enablegroup, true);
+      this.IterateAndSetDisable(disablegroup, false);
     }
 
-    var isChecked = this.checkboxTriggerTarget.checked
-    //apply disable value if the checkbox is checked and the opposite when unchecked
-    var disableValue = isChecked ? disable : !disable
-    //get all elements that are to be affected by checkbox using javascript
-    var targetElements = document.getElementsByName(group)
-
-    if (targetElements.length < 1) {
-      return console.warn(`No targets found to be affected by checkbox of id: ${this.checkboxTriggerTarget.id}`)
-    }
-
-    targetElements.forEach((element) => {
-      element.disabled = disableValue
-    })
   }
 
   OnLoad() {
-    this.checkboxTriggerTargets.forEach(() => {
-
-      let disableString = this.checkboxTriggerTarget.dataset.checkboxdisableDisableParam
-      let group = this.checkboxTriggerTarget.dataset.checkboxdisableGroupParam
+    for (let checkbox of this.checkboxTriggerTargets) {
+      let toggleStateString = checkbox.dataset.checkboxdisableTogglestateParam
+      let enablegroup = checkbox.dataset.checkboxdisableEnablegroupParam
+      let disablegroup = checkbox.dataset.checkboxdisableDisablegroupParam
+      let disableElement = checkbox.dataset.checkboxdisableDisableonloadParam
 
       //due to javascript typing, a boolean needs to be set manually to the destructured parameter or it will read as a string
       //Stimulus only assumes type when using params with an action
-      let disable = new Boolean()
+      let togglestate = new Boolean()
 
-      if (disableString == "false")
-        disable = false
+      if (toggleStateString == "false")
+        togglestate = false
 
-      if (disableString == "true")
-        disable = true
+      if (toggleStateString == "true")
+        togglestate = true
 
-      this.ToggleGroup({ params: { group, disable } })
-    })
+      // Need to call toggle group on each checkboxDisable checkbox
+      // console.log("Setting up state for " + checkbox.id);
+      // if checkbox is disabled, don't run togglegroup
+      if (checkbox.disabled == false) {
+        this.ToggleGroup({ params: { enablegroup, disablegroup, togglestate }, currentTarget: checkbox });
+      }
+    }
   }
 }
