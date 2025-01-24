3c49cc84941697e6775a8667610ab41abb0379ca
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-01-24 11:36:54 -0500

    Fetches list, filter by user input, user can click

---
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/autocomplete_controller.js
@@ -0,0 +1,45 @@
+﻿
+// wwwroot/js/js_controllers/autocomplete_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["searchBox", "list", "item", "noResults"]
+
+  showList(event) {
+    this.listTarget.classList.remove("hidden")
+  }
+
+  hideList(event) {
+    this.listTarget.classList.add("hidden")
+  }
+
+  filterList(event) {
+    var startsWithMatcher = new RegExp("^" + this.searchBoxTarget.value, "i")
+    this.itemTargets.map((item) => {
+      if (startsWithMatcher.test(item.innerHTML)) {
+        item.classList.remove("hidden")
+      }
+      else {
+        item.classList.add("hidden")
+      }
+    });
+    // TODO check if list is empty and if so, display the no results
+  }
+
+  showNoResults(event) {
+    this.noResultsTarget.remove("hidden")
+  }
+
+  hideNoResults(event) {
+    this.noResultsTarget.add("hidden")
+  }
+
+  setSearchBox(event) {
+    this.searchBoxTarget.value = event.target.innerHTML;
+    this.hideList();
+  }
+
+  connect() {
+    this.hideList()
+  }
+}
---
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/rxNormDisplayNames_controller.js
@@ -0,0 +1,43 @@
+﻿
+// wwwroot/js/js_controllers/rxNormDisplayNames_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["list"]
+
+  connect() {
+    this.fetchData()
+
+  }
+
+  async fetchData() {
+    try {
+      const response = await fetch("https://rxnav.nlm.nih.gov/REST/displaynames.json")
+      const data = await response.json()
+      console.log(data.displayTermsList)
+      // Update the target element with the received data
+      //this.resultTarget.textContent = data.message
+      data.displayTermsList.term.map((item) => {
+        const newLi = document.createElement("li")
+        newLi.textContent = item // Set the content of the new list item
+        newLi.classList.add("cursor-default")
+        newLi.classList.add("rounded-md")
+        newLi.classList.add("px-4")
+        newLi.classList.add("py-2")
+        newLi.classList.add("select-none")
+        newLi.classList.add("hover:bg-indigo-600")
+        newLi.classList.add("hover:text-white")
+        newLi.setAttribute("data-autocomplete-target", "item")
+        newLi.setAttribute("data-action", "click->autocomplete#setSearchBox")
+        newLi.id = "option"
+        newLi.role = "option"
+        newLi.tabIndex = -1
+        this.listTarget.appendChild(newLi)
+      })
+
+    } catch (error) {
+      console.error("Error fetching data:", error)
+    }
+  }
+
+}
