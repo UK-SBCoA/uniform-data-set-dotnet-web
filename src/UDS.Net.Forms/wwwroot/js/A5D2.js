d186f167ba7e53dbe92a6eec2759f717af1c9eca
mlan225 <17088502+mlan225@users.noreply.github.com>
2025-01-24 15:58:53 -0500

    Format the a5d2 javascript for readability

--- src/UDS.Net.Forms/wwwroot/js/A5D2.js
+++ src/UDS.Net.Forms/wwwroot/js/A5D2.js
@@ -1,30 +1,36 @@
 ﻿/*Implemented on questions 6a1 6a2 and 6a3 form A5D2*/
 
 $(document).ready(function () {
-  var majorDepRadios = Array.from(document.getElementsByName("A5D2.MAJORDEP"));
-  var otherDepRadios = Array.from(document.getElementsByName("A5D2.OTHERDEP"));
-  var deprTreatRadios = Array.from(
-    document.getElementsByName("A5D2.DEPRTREAT"),
-  );
 
-  function toggleDeprTreat() {
-    const recentMajorDep = majorDepRadios.some(
-      (radio) => radio.checked && radio.value == "1",
+    //element selectors
+    var majorDepRadios = Array.from(document.getElementsByName("A5D2.MAJORDEP"));
+    var otherDepRadios = Array.from(document.getElementsByName("A5D2.OTHERDEP"));
+    var deprTreatRadios = Array.from(document.getElementsByName("A5D2.DEPRTREAT"));
+
+
+    function toggleDeprTreat() {
+        const recentMajorDep = majorDepRadios.some(
+            (radio) => radio.checked && radio.value == "1",
+        );
+        const recentOtherDep = otherDepRadios.some(
+            (radio) => radio.checked && radio.value == "1",
+        );
+
+        deprTreatRadios.forEach((radio) => {
+            radio.disabled = !(recentMajorDep || recentOtherDep);
+        });
+    }
+
+    //Event listeners
+    majorDepRadios.forEach((radio) =>
+        radio.addEventListener("change", toggleDeprTreat),
     );
-    const recentOtherDep = otherDepRadios.some(
-      (radio) => radio.checked && radio.value == "1",
+
+    otherDepRadios.forEach((radio) =>
+        radio.addEventListener("change", toggleDeprTreat),
     );
 
-    deprTreatRadios.forEach((radio) => {
-      radio.disabled = !(recentMajorDep || recentOtherDep);
-    });
-  }
-  majorDepRadios.forEach((radio) =>
-    radio.addEventListener("change", toggleDeprTreat),
-  );
-  otherDepRadios.forEach((radio) =>
-    radio.addEventListener("change", toggleDeprTreat),
-  );
 
-  toggleDeprTreat();
+    //On load methods
+    toggleDeprTreat();
 });
