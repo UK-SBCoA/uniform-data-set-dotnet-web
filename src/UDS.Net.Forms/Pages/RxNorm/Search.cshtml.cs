60fb099c8f2d0f726ddf3e67baea3da59a7241e5
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-01-25 10:30:18 -0500

    Add ability to find and persist custom medication codes (#243)
    * Prevent potential for duplicates
    
    * Uses RxNorm.NET client
    
    * Fetches list, filter by user input, user can click
    
    * Fetch handles autocomplete event so list is incrementally added
    
    * Lookups local drug code before RxNorm, adds to A4
    
    * Handles user activity before clicking add custom medication

---
+++ src/UDS.Net.Forms/Pages/RxNorm/Search.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UDS.Net.Forms.Models;
+using UDS.Net.Services;
+
+namespace UDS.Net.Forms.Pages.RxNorm
+{
+    public class SearchModel : PageModel
+    {
+        protected readonly ILookupService _lookupService;
+
+        [BindProperty]
+        public RxNormLookupModel RxNormLookup { get; set; } = default!;
+
+        public SearchModel(ILookupService lookupService)
+        {
+            _lookupService = lookupService;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            RxNormLookup = new RxNormLookupModel();
+            if (id.HasValue)
+                RxNormLookup.Id = id.Value;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (String.IsNullOrWhiteSpace(RxNormLookup.SearchTerm))
+            {
+                ModelState.AddModelError("RxNormLookup.SearchTerm", "Must include a search term.");
+                return Page();
+            }
+
+            try
+            {
+                var results = await _lookupService.LookupRxNormApproximateMatches(RxNormLookup.SearchTerm);
+
+                var count = results.Where(t => t.Name.ToLower().Contains(RxNormLookup.SearchTerm.ToLower())).Count();
+
+                if (count > 0)
+                {
+                    return RedirectToPage("/RxNorm/Select", new { Id = RxNormLookup.Id, SearchTerm = RxNormLookup.SearchTerm });
+                }
+                else
+                {
+                    ModelState.AddModelError("RxNormLookup.SearchTerm", "No results found.");
+                    return Page();
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("RxNormLookup.SearchTerm", "Trouble accessing RxNorm web api. Please use RxNav: https://lhncbc.nlm.nih.gov/RxNav/ to find RxCUI and submit ticket to DMS to update packet.");
+                return Page();
+            }
+        }
+    }
+}
