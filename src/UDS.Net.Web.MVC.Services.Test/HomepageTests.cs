32cf9c6a4c560cd3628bb802316a6436df97afba
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-02-10 15:51:47 -0500

    Attempt to figure out how to reuse auth state - reauths for each TestMethod

--- src/UDS.Net.Web.MVC.Services.Test/HomepageTests.cs
+++ src/UDS.Net.Web.MVC.Services.Test/HomepageTests.cs
@@ -6,31 +6,42 @@ namespace UDS.Net.Web.MVC.Services.Test;
 [TestClass]
 public class HomepageTests : PageTest
 {
-    int _salt = 0;
 
-    [TestInitialize]
-    public async Task Initiatize()
+    public override BrowserNewContextOptions ContextOptions()
     {
-        await Page.GotoAsync("https://localhost:4811");
-        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        return new BrowserNewContextOptions()
+        {
+            StorageStatePath = "../../../.auth/state.json"
+        };
+    }
+
+    // Test initialize runs before each TestMethod
+    [TestInitialize]
+    public async Task TestInitialize()
+    {
+        await Page.GotoAsync("https://localhost:4811/Identity/Account/Register");
 
         Random random = new Random();
-        _salt = random.Next(1, 1000);
+        int _salt = random.Next(1, 1000);
 
         string emailAddress = $"test{_salt}@test.com";
         string password = $"Password_{_salt}";
 
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Register as a new user" }).ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").FillAsync(emailAddress);
-        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByLabel("Email").ClickAsync();
+        await Page.GetByLabel("Email").FillAsync(emailAddress);
+
+        // Click ensures that the element is visible
+        await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
         await Page.GetByLabel("Password", new() { Exact = true }).FillAsync(password);
+
         await Page.GetByLabel("Confirm Password").ClickAsync();
         await Page.GetByLabel("Confirm Password").FillAsync(password);
+
         await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
         await Page.WaitForRequestFinishedAsync();
 
         await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+        await Page.WaitForRequestFinishedAsync();
 
         await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
         await Page.GetByPlaceholder("name@example.com").ClickAsync();
@@ -49,16 +60,12 @@ public class HomepageTests : PageTest
     public async Task HomepageHasNavigationToParticipations()
     {
         await Page.Locator("#desktop-menu").GetByRole(AriaRole.Link, new() { Name = "Participation" }).ClickAsync();
-
-        await Expect(Page.GetByRole(AriaRole.Table)).ToBeVisibleAsync();
     }
 
     [TestMethod]
     public async Task HomepageHasNavigationToVisits()
     {
         await Page.Locator("#desktop-menu").GetByRole(AriaRole.Link, new() { Name = "Visits" }).ClickAsync();
-
-        await Expect(Page.GetByRole(AriaRole.Table)).ToBeVisibleAsync();
     }
 }
 
