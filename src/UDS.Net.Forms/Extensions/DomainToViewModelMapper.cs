bb421823f2b09c1c7c7540be8e29c8ce3f321b21
ahmedhaly7777 <ahmed.aly@uky.edu>
2025-03-12 10:50:03 -0400

    Removed FTLDIF from DomainToViewModelMapper

--- src/UDS.Net.Forms/Extensions/DomainToViewModelMapper.cs
+++ src/UDS.Net.Forms/Extensions/DomainToViewModelMapper.cs
@@ -1582,7 +1582,6 @@ namespace UDS.Net.Forms.Extensions
                 LBDIS = fields.LBDIS,
                 LBDIF = fields.LBDIF,
                 FTLD = fields.FTLD,
-                FTLDIF = fields.FTLDIF,
                 PSP = fields.PSP,
                 PSPIF = fields.PSPIF,
                 CORT = fields.CORT,
