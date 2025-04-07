8fa5bdd506de79f93859cf92d9dfe709fa6f3f2e
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2025-04-07 16:34:21 -0400

    add javascript to form footer selectors

---
+++ src/UDS.Net.Forms/wwwroot/js/FormFooter.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+  const modeSelect = document.querySelector('[name$=".MODE"]');
+  const remoteReasonSelect = document.querySelector('[name$=".RMREAS"]');
+  const remoteMode = document.querySelector('[name$=".RMMODE"]');
+  const notCompletedReason = document.querySelector('[name$=".NOT"]')
+
+  function resetToDefault(select) {
+    if (select) select.selectedIndex = 0
+  }
+  modeSelect.addEventListener('change', function () {
+
+    const mode = parseInt(this.value);
+
+    if (mode === 1) { //In person
+      resetToDefault(remoteReasonSelect);
+      resetToDefault(remoteMode);
+      resetToDefault(notCompletedReason);
+
+    }
+    if (mode === 2) { //Remote
+      resetToDefault(notCompletedReason);
+    }
+    if (mode === 3) { //Not Completed
+      resetToDefault(remoteReasonSelect);
+      resetToDefault(remoteMode);
+    }
+  });
+});
