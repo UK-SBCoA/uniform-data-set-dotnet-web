f7ba1650e71ec2ecbe111b4f0f508289c06499cb
mlan225 <17088502+mlan225@users.noreply.github.com>
2025-04-16 01:59:49 -0400

    Remove unused usings

--- src/UDS.Net.Forms/Records/RecordConstants.cs
+++ src/UDS.Net.Forms/Records/RecordConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UDS.Net.Forms.Records
+﻿namespace UDS.Net.Forms.Records
 {
     public static class RecordConstants
     {
