800944baf68a773d65868e3b2ffa0a12ff807b9e
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-04-23 11:05:51 -0400

    NotCompleted value changed to 0 rather than 3

--- src/UDS.Net.Forms/Models/FormModel.cs
+++ src/UDS.Net.Forms/Models/FormModel.cs
@@ -51,7 +51,7 @@ namespace UDS.Net.Forms.Models
 
         public List<int> AllowedFormModes { get; set; } = new List<int>();
 
-        [RequiredIf(nameof(MODE), "3", ErrorMessage = "Specify reason not completed")]
+        [RequiredIf(nameof(MODE), "0", ErrorMessage = "Specify reason not completed")]
         [Display(Name = "If not completed, specify reason")]
         public NotIncludedReasonCode? NOT { get; set; }
 
