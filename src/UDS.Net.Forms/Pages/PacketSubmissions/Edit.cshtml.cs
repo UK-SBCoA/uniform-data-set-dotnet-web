e025c2224b99823b101f28be1652f896a8418367
mlan225 <17088502+mlan225@users.noreply.github.com>
2025-05-05 10:26:09 -0400

    Update edit page code behind and adjust formatting

--- src/UDS.Net.Forms/Pages/PacketSubmissions/Edit.cshtml.cs
+++ src/UDS.Net.Forms/Pages/PacketSubmissions/Edit.cshtml.cs
@@ -34,41 +34,41 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions
             return Partial("_Edit", PacketSubmission);
         }
 
-        public async Task<IActionResult> OnPostAsync(PacketSubmissionModel packetSubmission, [FromForm]int packetStatus)
+        public async Task<IActionResult> OnPostAsync(PacketSubmissionModel packetSubmission, [FromForm] int packetStatus)
         {
             Packet existingPacket = await GetPacketData(packetSubmission.PacketId);
 
-            if (existingPacket == null)
-                return NotFound($"Existing packet with Id of {packetSubmission.PacketId} not found");
-
-            // Update packet status
-            if (existingPacket.TryUpdateStatus((PacketStatus)packetStatus))
-            {
-                existingPacket.UpdateStatus((PacketStatus)packetStatus);
-            }
-            else
-            {
-                return NotFound($"Unable to set packet Id ${existingPacket.Id} status to: {packetStatus}");
-            }
-
-            // Update status to failed error checks when error count is saved
             if (existingPacket != null)
             {
-                Packet updatedPacket = await _packetService.UpdatePacketSubmissionErrorCount(User.Identity.Name, existingPacket, (int)packetSubmission.ErrorCount, packetSubmission.Id);
+                // Update packet status using index of packetStatus enum index
+                if (existingPacket.TryUpdateStatus((PacketStatus)packetStatus))
+                {
+                    existingPacket.UpdateStatus((PacketStatus)packetStatus);
+                }
+                else
+                {
+                    return NotFound($"Unable to set packet Id ${existingPacket.Id} status to: {packetStatus}");
+                }
 
-                // Create a packetModel to return to the index
-                PacketModel updatedPacketModel = updatedPacket.ToVM();
-                updatedPacketModel.Participation = existingPacket.Participation.ToVM();
+                if (existingPacket != null)
+                {
+                    Packet updatedPacket = await _packetService.UpdatePacketSubmissionErrorCount(User.Identity.Name, existingPacket, (int)packetSubmission.ErrorCount, packetSubmission.Id);
 
-                return Partial("_Index", updatedPacketModel);
+                    // Create a packetModel to return to the index
+                    PacketModel updatedPacketModel = updatedPacket.ToVM();
+                    updatedPacketModel.Participation = existingPacket.Participation.ToVM();
+
+                    return Partial("_Index", updatedPacketModel);
+                }
+
+                // return existing packetModel on failure to update
+                PacketModel existingPacketModel = existingPacket.ToVM();
+                existingPacketModel.Participation = existingPacket.Participation.ToVM();
+
+                return Partial("_Index", existingPacketModel);
             }
 
-            // return existing packetModel on failure to update
-            PacketModel existingPacketModel = existingPacket.ToVM();
-            existingPacketModel.Participation = existingPacket.Participation.ToVM();
-
-            return Partial("_Index", existingPacketModel);
-
+            return NotFound($"Unable to set packet Id ${packetSubmission.PacketId} status to: {(PacketStatus)packetStatus}");
         }
 
         private async Task<Packet> GetPacketData(int packetId)
