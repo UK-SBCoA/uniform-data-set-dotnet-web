78bac468dbceec85aa1a054242e70d8221b01db5
mlan225 <17088502+mlan225@users.noreply.github.com>
2025-06-13 18:02:30 -0400

    Update functionality for b6 calculations

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/b6_controller.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/b6_controller.js
@@ -20,41 +20,46 @@ export default class extends Controller {
         let answeredCheckedRadios = this.radioInputTargets.filter(input => input.checked && input.value != 9)
         let unansweredCheckedRadios = this.radioInputTargets.filter(input => input.checked && input.value == 9)
 
-        //if fewer than 12 questions were answered check NOGDS, auto set 88, and end CalculateGDS method
-        if (unansweredCheckedRadios.length > 3) {
-            this.NOGDSInputTarget.checked = true
-            this.NOGDSBehavior()
-
-            return
-        }
-        else {
-            //if 12 or more questions are answered, auto uncheck NOGDS and continue with calculations
-            this.NOGDSInputTarget.checked = false
-        }
-
-        answeredCheckedRadios.forEach((radio) => {
-            GDSScore += Number(radio.value)
-        })
-
-        //display the total only when all radios have been checked
+        //Only run calculations when all questions are answered
         if (answeredCheckedRadios.length + unansweredCheckedRadios.length == totalQuestionCount) {
-            // all questions are != 9, then do not prorate score
-            if (unansweredCheckedRadios.length < 1) {
-                this.GDSInputTarget.value = GDSScore
-            } else {
-                this.GDSInputTarget.value = this.ProrateGDSScore(GDSScore, answeredCheckedRadios.length, unansweredCheckedRadios.length)
+            //if fewer than 12 questions were answered check NOGDS and auto set 88
+            if (unansweredCheckedRadios.length > 3) {
+
+                this.NOGDSInputTarget.checked = true
+                this.GDSInputTarget.value = 88
             }
+            else if(unansweredCheckedRadios.length <= 3) {
+                //if 12 or more questions are answered, auto uncheck NOGDS and continue with calculations
+                this.NOGDSInputTarget.checked = false
+
+                answeredCheckedRadios.forEach((radio) => {
+                    GDSScore += Number(radio.value)
+                })
+
+                //display the total only when all radios have been checked
+                if (answeredCheckedRadios.length + unansweredCheckedRadios.length == totalQuestionCount) {
+                    // all questions are != 9, then do not prorate score
+                    if (unansweredCheckedRadios.length < 1) {
+                        this.GDSInputTarget.value = GDSScore
+                    } else {
+                        this.GDSInputTarget.value = this.ProrateGDSScore(GDSScore, answeredCheckedRadios.length, unansweredCheckedRadios.length)
+                    }
+                }
+            }
+        }
+        //if NOGDS selected, GDS score will always be 88
+        else if (this.NOGDSInputTarget.checked) {
+            this.GDSInputTarget.value = 88
+        }
+        //if not all radios are selected and NOGDS is not selected, clear GDS score
+        else {
+            this.GDSInputTarget.value = ""
         }
     }
 
-    //GDS must be 88 if NOGDS checkbox is checked. Auto set for user on check
+    
     NOGDSBehavior() {
-        if (this.NOGDSInputTarget.checked) {
-            this.GDSInputTarget.value = 88
-        } else {
-            //recalculate GDS with enabled radios
-            this.CalculateGDS()
-        }
+        this.CalculateGDS()
     }
 
     ProrateGDSScore(GDSScore, answeredCount, unansweredCount) {
