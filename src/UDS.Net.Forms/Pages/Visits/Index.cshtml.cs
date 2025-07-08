7c0c7e4bb31f1dc0f74e97fe7c0535d08c8e80a9
CallowaySimon <simon.calloway@uky.edu>
2025-07-08 14:02:30 -0400

    update api dependencies

--- src/UDS.Net.Forms/Pages/Visits/Index.cshtml.cs
+++ src/UDS.Net.Forms/Pages/Visits/Index.cshtml.cs
@@ -13,7 +13,6 @@ namespace UDS.Net.Forms.Pages.Visits
 
         public VisitsPaginatedModel Visits { get; set; } = new VisitsPaginatedModel();
 
-        //Create pageModel filter property to apply values to
         public FilterModel Filter;
 
         public IndexModel(IVisitService visitService)
@@ -23,7 +22,6 @@ namespace UDS.Net.Forms.Pages.Visits
 
         public async Task<IActionResult> OnGetAsync(string[] filter, DateTime? startDate, DateTime? endDate, int pageSize = 10, int pageIndex = 1, string search = null)
         {
-            // Initialize filter
             Filter = new FilterModel(Enum.GetNames(typeof(PacketStatus)).ToList(), filter.ToList(), startDate, endDate);
 
             IEnumerable<Services.DomainModels.Visit> visits;
@@ -37,12 +35,15 @@ namespace UDS.Net.Forms.Pages.Visits
                 visits = await _visitService.ListByStatus(User.Identity.Name, pageSize, pageIndex, Filter.SelectedItems.ToArray());
                 total = await _visitService.CountByStatus(User.Identity.Name, Filter.SelectedItems.ToArray());
             }
-            else if (startDate.HasValue && endDate.HasValue)
+            else if (startDate.HasValue || endDate.HasValue)
             {
-                endDate = endDate.Value.Date.AddDays(1).AddTicks(-1);
+                if (endDate.HasValue)
+                {
+                    endDate = endDate.Value.Date.AddDays(1).AddTicks(-1);
+                }
 
-                visits = await _visitService.ListByDateRangeAndStatus(User.Identity.Name, Filter.SelectedItems.ToArray(), startDate.Value, endDate.Value, pageSize, pageIndex);
-                total = await _visitService.CountByDateRangeAndStatus(User.Identity.Name, Filter.SelectedItems.ToArray(), startDate.Value, endDate.Value);
+                visits = await _visitService.ListByDateRangeAndStatus(User.Identity.Name, Filter.SelectedItems.ToArray(), startDate, endDate, pageSize, pageIndex);
+                total = await _visitService.CountByDateRangeAndStatus(User.Identity.Name, Filter.SelectedItems.ToArray(), startDate, endDate);
             }
             else
             {
