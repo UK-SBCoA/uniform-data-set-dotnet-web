dc41602df20cff01df57bbf7131af526eec6c619
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-07-09 21:35:08 -0400

    Only search RxNorm if we haven't added drugs that start with the first letter

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/rxNormDisplayNames_controller.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/rxNormDisplayNames_controller.js
@@ -16,42 +16,45 @@ export default class extends Controller {
   }
 
   async fetchData({ detail: { content } }) {
-    var search = content;
-    if (!this.fetchedLettersValue.includes(search)) {
-      if (this.hasProgressIndicatorTarget) {
-        this.progressIndicatorTarget.classList.remove("hidden");
-      }
-      this.fetchedLettersValue = this.fetchedLettersValue + search;
-      try {
-        const response = await fetch("https://rxnav.nlm.nih.gov/REST/displaynames.json")
-        const data = await response.json()
+    if (content.length > 0) {
+      // only get new results from rxnorm when the first letter has not already been added to the list
+      var search = content[0];
+      if (!this.fetchedLettersValue.includes(search)) {
+        if (this.hasProgressIndicatorTarget) {
+          this.progressIndicatorTarget.classList.remove("hidden");
+        }
+        this.fetchedLettersValue = this.fetchedLettersValue + search;
+        try {
+          const response = await fetch("https://rxnav.nlm.nih.gov/REST/displaynames.json")
+          const data = await response.json()
 
-        // Update the list with the received data
-        data.displayTermsList.term.forEach(item => {
-          var startsWithMatcher = new RegExp("^" + search, "i")
-          if (startsWithMatcher.test(item)) {
-            const newLi = document.createElement("li")
-            newLi.textContent = item // Set the content of the new list item
-            newLi.classList.add("cursor-default")
-            newLi.classList.add("rounded-md")
-            newLi.classList.add("px-4")
-            newLi.classList.add("py-2")
-            newLi.classList.add("select-none")
-            newLi.classList.add("hover:bg-indigo-600")
-            newLi.classList.add("hover:text-white")
-            newLi.setAttribute("data-autocomplete-target", "item")
-            newLi.setAttribute("data-action", "click->autocomplete#setSearchBox")
-            newLi.role = "option"
-            newLi.tabIndex = -1
-            this.listTarget.appendChild(newLi)
-          }
-        })
+          // Update the list with the received data
+          data.displayTermsList.term.forEach(item => {
+            var startsWithMatcher = new RegExp("^" + search, "i")
+            if (startsWithMatcher.test(item)) {
+              const newLi = document.createElement("li")
+              newLi.textContent = item // Set the content of the new list item
+              newLi.classList.add("cursor-default")
+              newLi.classList.add("rounded-md")
+              newLi.classList.add("px-4")
+              newLi.classList.add("py-2")
+              newLi.classList.add("select-none")
+              newLi.classList.add("hover:bg-indigo-600")
+              newLi.classList.add("hover:text-white")
+              newLi.setAttribute("data-autocomplete-target", "item")
+              newLi.setAttribute("data-action", "click->autocomplete#setSearchBox")
+              newLi.role = "option"
+              newLi.tabIndex = -1
+              this.listTarget.appendChild(newLi)
+            }
+          })
 
-      } catch (error) {
-        console.error("Error fetching data:", error)
-      }
-      if (this.hasProgressIndicatorTarget) {
-        this.progressIndicatorTarget.classList.add("hidden");
+        } catch (error) {
+          console.error("Error fetching data:", error)
+        }
+        if (this.hasProgressIndicatorTarget) {
+          this.progressIndicatorTarget.classList.add("hidden");
+        }
       }
     }
   }
