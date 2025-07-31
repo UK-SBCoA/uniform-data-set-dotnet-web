ca24a0a798b9062d70a46fdd976b0600b48210a2
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2025-07-31 17:51:08 -0400

    remove unsed using statements

--- src/UDS.Net.Forms/DataAnnotations/RequiredIfInPersonVisitAttribute.cs
+++ src/UDS.Net.Forms/DataAnnotations/RequiredIfInPersonVisitAttribute.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UDS.Net.Forms.Models;
 using UDS.Net.Services.Enums;
 
