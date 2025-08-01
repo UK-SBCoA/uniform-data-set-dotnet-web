a9a7792cf6f40f04bcd8313ec369dcc4630e55d0
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-08-01 13:23:40 -0400

    End to end tests with Playwright (#161)
    * Sets up playwright project for end-to-end tests using SQLite
    
    * Add to CI pipeline on PR "ready for review"
    
    * Fixes other actions
    
    * Parses test output and displays in summary on action and PR as a comment

---
+++ src/UDS.Net.Forms.Tests.Runtime/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using UDS.Net.Forms.Tests.Runtime.Models;
+using UDS.Net.Services;
+using UDS.Net.Services.DomainModels;
+
+namespace UDS.Net.Forms.Tests.Runtime.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IVisitService _visitService;
+    private readonly IParticipationService _participationService;
+
+    public HomeController(ILogger<HomeController> logger, IVisitService visitService, IParticipationService participationService)
+    {
+        _logger = logger;
+        _visitService = visitService;
+        _participationService = participationService;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(string legacyId)
+    {
+        var participation = await _participationService.GetByLegacyId("username", legacyId);
+
+        var visit = await _visitService.GetById("username", 1);
+
+        if (visit == null)
+        {
+            var v = new Visit(1, 1, participation.Id, "4", Net.Services.Enums.PacketKind.I, DateTime.Now, "TST", Net.Services.Enums.PacketStatus.Pending, DateTime.Now, "email@uky.edu", "", "", false, null);
+
+            visit = await _visitService.Add("username", v);
+        }
+
+        return RedirectToPage("/Visits/Details", new { Id = visit.Id });
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ src/UDS.Net.Forms.Tests.Runtime/Data/Participation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UDS.Net.Forms.Tests.Runtime.Data
+{
+    public class Participation
+    {
+        [Key]
+        public int ParticipationId { get; set; }
+    }
+}
+
