ccf8b787bb18987df74088a60d1d4c644f1ce590
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-08-15 14:18:56 -0400

    Adds some UI tests for D1a

---
+++ src/UDS.Net.Forms.Tests/D1aImpairedTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace UDS.Net.Forms.Tests
+{
+    /// <summary>
+    /// Path: Impaired optional MBI
+    /// 7. Impaired + MBI, IMPNOMCI =1, MBI = 1
+    /// 8. Impaired, IMPNOMCI = 1, not MBI
+    /// </summary>
+    public class D1aImpairedTests
+    {
+    }
+}
+
