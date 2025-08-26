6c4150dbb1efe34e19a70501b9ba43c88c8c0955
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-08-26 13:24:54 -0400

    Added a few more simple tests

--- src/UDS.Net.Forms.Tests/D1aMBITests.cs
+++ src/UDS.Net.Forms.Tests/D1aMBITests.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Playwright;
 
 namespace UDS.Net.Forms.Tests
 {
     /// <summary>
-    /// Path: Impaired optional MBI
-    /// 7. Impaired + MBI, IMPNOMCI = 1, MBI = 1
-    /// 8. Impaired, IMPNOMCI = 1, not MBI
+    /// Path:  MBI
+    /// 4. MBI, MCI = 0, IMPNOMCI = 0, MBI = 1
+    /// (Other paths where MCI = 1 in D1aMCITests)
+    /// (Other paths where IMPNOMCI = 1 in D1aImpairedTests)
     ///
     /// NORMCOG = 0
     /// DEMENTED = 0
-    /// MCI = 0
-    /// IMPNOMCI = 1
-    /// MBI = (0,1)
+    /// MCI = 0 (other option is 1 can be found in D1aMCITests)
+    /// IMPNOMCI = 0 (other option is 1 can be found in D1aImpairedTests)
+    /// MBI = 1
     /// </summary>
     [TestClass]
-    public class D1aImpairedTests : TestBase
+    public class D1aMBITests : TestBase
     {
         [TestMethod]
-        public async Task D1aImpairedUIBehaviors()
+        public async Task D1aMBIUIBehaviors()
         {
             await Page.GotoAsync(BaseUrl);
             await Page.GetByRole(AriaRole.Button, new() { Name = "Go" }).ClickAsync();
@@ -37,31 +37,21 @@ namespace UDS.Net.Forms.Tests
             // Question 4b MCI == 0 No
             await Page.Locator("input[type=\"radio\"][name=\"D1a.MCI\"][value=\"0\"]").ClickAsync();
 
-            // Question 5
-            await Expect(Page.Locator("input[name=\"D1a.IMPNOMCIFU\"][type=\"checkbox\"]")).ToBeEnabledAsync();
-            await Expect(Page.Locator("input[name=\"D1a.IMPNOMCICG\"][type=\"checkbox\"]")).ToBeEnabledAsync();
-            await Expect(Page.Locator("input[name=\"D1a.IMPNOMCLCD\"][type=\"checkbox\"]")).ToBeEnabledAsync();
-            await Expect(Page.Locator("input[name=\"D1a.IMPNOMCIO\"][type=\"checkbox\"]")).ToBeEnabledAsync();
-            // Question 5b
-            await Expect(Page.Locator("input[name=\"D1a.IMPNOMCI\"][value=\"0\"]")).ToBeEnabledAsync();
-            await Expect(Page.Locator("input[name=\"D1a.IMPNOMCI\"][value=\"1\"]")).ToBeEnabledAsync();
-
-            // Question 5b IMPNOMCI == 1 Yes
-            await Page.Locator("input[type=\"radio\"][name=\"D1a.IMPNOMCI\"][value=\"1\"]").ClickAsync();
+            // Question 5b IMPNOMCI == 0 No (IMPNOMCI == 1 in D1aImpairedTests)
+            await Page.Locator("input[type=\"radio\"][name=\"D1a.IMPNOMCI\"][value=\"0\"]").ClickAsync();
 
             // Question 7 MBI
             await Expect(Page.Locator("input[name=\"D1a.MBI\"][value=\"0\"]")).ToBeEnabledAsync();
             await Expect(Page.Locator("input[name=\"D1a.MBI\"][value=\"1\"]")).ToBeEnabledAsync();
-            await Page.Locator("input[type=\"radio\"][name=\"D1a.MBI\"][value=\"0\"]").ClickAsync();
+            await Page.Locator("input[type=\"radio\"][name=\"D1a.MBI\"][value=\"1\"]").ClickAsync();
 
             // Question 8 PREDOMSYN
             await Expect(Page.Locator("input[name=\"D1a.PREDOMSYN\"][value=\"0\"]")).ToBeEnabledAsync();
             await Expect(Page.Locator("input[name=\"D1a.PREDOMSYN\"][value=\"1\"]")).ToBeEnabledAsync();
-
         }
 
         [TestMethod]
-        public async Task D1aImpairedValidatesRequiredFields()
+        public async Task D1aMBIValidatesRequiredFields()
         {
             await Page.GotoAsync(BaseUrl);
             await Page.GetByRole(AriaRole.Button, new() { Name = "Go" }).ClickAsync();
@@ -79,10 +69,10 @@ namespace UDS.Net.Forms.Tests
             // Question 4b MCI == 0 No
             await Page.Locator("input[type=\"radio\"][name=\"D1a.MCI\"][value=\"0\"]").ClickAsync();
 
-            // Question 5b IMPNOMCI == 1 Yes
-            await Page.Locator("input[type=\"radio\"][name=\"D1a.IMPNOMCI\"][value=\"1\"]").ClickAsync();
+            // Question 5b IMPNOMCI == 0 No (IMPNOMCI == 1 in D1aImpairedTests)
+            await Page.Locator("input[type=\"radio\"][name=\"D1a.IMPNOMCI\"][value=\"0\"]").ClickAsync();
 
-            // TODO Complete Impaired tests for validation
+            // TODO Complete MBI tests for validation
         }
     }
 }
