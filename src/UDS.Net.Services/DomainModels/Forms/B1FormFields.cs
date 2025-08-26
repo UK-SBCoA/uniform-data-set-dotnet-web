0484dfd1a7b898e67340ac9c50ccda83053f151c
CallowaySimon <simon.calloway@uky.edu>
2025-08-26 08:47:58 -0400

    Adjusted Non complete reason codes for A2, B1, B3

--- src/UDS.Net.Services/DomainModels/Forms/B1FormFields.cs
+++ src/UDS.Net.Services/DomainModels/Forms/B1FormFields.cs
@@ -35,7 +35,7 @@ namespace UDS.Net.Services.DomainModels.Forms
         {
             get
             {
-                return new List<NotIncludedReasonCode>() { NotIncludedReasonCode.NoCoParticipantOrRemoteVisit, NotIncludedReasonCode.PhysicalProblem, NotIncludedReasonCode.CognitiveBehavioralProblem, NotIncludedReasonCode.Other, NotIncludedReasonCode.VerbalRefusal };
+                return new List<NotIncludedReasonCode>() { NotIncludedReasonCode.RemoteVisit, NotIncludedReasonCode.PhysicalProblem, NotIncludedReasonCode.CognitiveBehavioralProblem, NotIncludedReasonCode.Other, NotIncludedReasonCode.VerbalRefusal };
             }
         }
 
