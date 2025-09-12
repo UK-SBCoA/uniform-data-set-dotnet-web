715095a917d73cf6a7bf8bb66aec1cde4cd25f2d
CallowaySimon <simon.calloway@uky.edu>
2025-09-12 16:50:47 -0400

    add error resolution

--- src/UDS.Net.Forms/Pages/Visits/Details.cshtml.cs
+++ src/UDS.Net.Forms/Pages/Visits/Details.cshtml.cs
@@ -12,6 +12,38 @@ namespace UDS.Net.Forms.Pages.Visits
 {
     public class DetailsModel : VisitPageModel
     {
-        public DetailsModel(IVisitService visitService, IParticipationService participationService) : base(visitService, participationService) { }
+        private readonly IPacketService _packetService;
+
+        public DetailsModel(IVisitService visitService, IParticipationService participationService, IPacketService packetService)
+: base(visitService, participationService)
+        {
+            _packetService = packetService;
+        }
+
+
+        [BindProperty]
+        public int ErrorId { get; set; }
+
+        public async Task<IActionResult> OnPostResolveErrorAsync(int errorId)
+        {
+            var username = User.Identity?.Name ?? "unknown";
+
+            var packet = await _packetService.GetById(username, Visit.Id);
+
+            var submission = packet.Submissions.FirstOrDefault(sub => sub.Errors.Any(e => e.Id == errorId));
+            if (submission == null)
+                return NotFound("Submission not found.");
+
+            var error = submission.Errors.FirstOrDefault(e => e.Id == errorId);
+            if (error == null)
+                return NotFound("Error not found.");
+
+            error.Resolve(username, username);
+
+            await _packetService.UpdatePacketSubmissionErrors(username, packet, submission.Id, submission.Errors.ToList());
+
+            return RedirectToPage();
+        }
+
     }
 }
