dcb01da57e9b07cffd23ae8be2287d060d187c8e
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-09-30 14:19:25 -0400

    [Hotfix] D1a Question 6 tests

--- src/UDS.Net.Forms.Tests/D1aDementiaTests.cs
+++ src/UDS.Net.Forms.Tests/D1aDementiaTests.cs
@@ -29,6 +29,8 @@ namespace UDS.Net.Forms.Tests
             // Question 3 DEMENTED == 1 Yes
             await Page.Locator("input[type=\"radio\"][name=\"D1a.DEMENTED\"][value=\"1\"]").ClickAsync();
 
+            // Question 6a must have a checkbox enabled
+            await Expect(Page.Locator("input[name=\"D1a.CDOMMEM\"][type=\"checkbox\"]")).ToBeEnabledAsync();
 
         }
 
@@ -51,6 +53,39 @@ namespace UDS.Net.Forms.Tests
 
         }
 
+        [TestMethod]
+        public async Task D1aDementiaDisplaysQuestion6()
+        {
+            await Page.GotoAsync(BaseUrl);
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Go" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Listitem).Filter(new() { HasText = "D1a Required" }).GetByRole(AriaRole.Link).ClickAsync();
+
+            // Question 1
+            await Page.GetByText("Formal consensus panel").ClickAsync();
+
+            // Question 2 NORMCOG == 0 No
+            await Page.Locator("input[type=\"radio\"][name=\"D1a.NORMCOG\"][value=\"0\"]").ClickAsync();
+
+            // Question 3 DEMENTED == 0 No
+            await Page.Locator("input[type=\"radio\"][name=\"D1a.DEMENTED\"][value=\"1\"]").ClickAsync();
+
+            // Question 6a-6g
+            await Page.Locator("input[name=\"D1a.CDOMMEM\"][type=\"checkbox\"]").CheckAsync();
+            await Page.Locator("input[name=\"D1a.CDOMLANG\"][type=\"checkbox\"]").CheckAsync();
+            await Page.Locator("input[name=\"D1a.CDOMATTN\"][type=\"checkbox\"]").CheckAsync();
+
+            await Expect(Page.GetByLabel("Save status")).ToContainTextAsync("Not started In progress Finalized");
+            await Page.GetByLabel("Save status").SelectOptionAsync("In progress");
+
+            // Attempt to save
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Save", Exact = true }).ClickAsync();
+
+            // Re-load
+            await Page.GetByRole(AriaRole.Listitem).Filter(new() { HasText = "D1a Required" }).GetByRole(AriaRole.Link).ClickAsync();
+            await Expect(Page.Locator("input[name=\"D1a.CDOMMEM\"][type=\"checkbox\"]")).ToBeCheckedAsync();
+            await Expect(Page.Locator("input[name=\"D1a.CDOMLANG\"][type=\"checkbox\"]")).ToBeCheckedAsync();
+            await Expect(Page.Locator("input[name=\"D1a.CDOMATTN\"][type=\"checkbox\"]")).ToBeCheckedAsync();
+        }
     }
 }
 
