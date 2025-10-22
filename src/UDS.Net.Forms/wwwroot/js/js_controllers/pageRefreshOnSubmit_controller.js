bc19c6424e3e5eab9f27b881b81182e3330d9bd5
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2025-10-22 15:26:26 -0400

    fixes double submission bug

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/pageRefreshOnSubmit_controller.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/pageRefreshOnSubmit_controller.js
@@ -3,8 +3,7 @@
 export default class extends Controller {
   static targets = ["button"]
 
-  async submit(event) {
-
+  submit(event) {
     if (this.hasButtonTarget) {
       this.buttonTarget.disabled = true
       this.buttonTarget.classList.remove('bg-indigo-600', 'hover:bg-indigo-700')
@@ -12,33 +11,15 @@ export default class extends Controller {
       this.buttonTarget.textContent = "Exporting..."
     }
 
-    // Get form and endpoint URL
-    const form = event.target.closest('form')
-    const url = form.action
-    const formData = new FormData(form)
-
-    // Reset existing cookie
     document.cookie = "downloadComplete=false"
 
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: formData,
-      })
-
-      if (response.ok) {
-        const downloadCompleteChecker = setInterval(() => {
-          if (document.cookie.includes("downloadComplete=true")) {
-            clearInterval(downloadCompleteChecker);
-            alert("Packet export complete!");
-            window.location.reload();
-          }
-        }, 500)
-      } else {
-        alert("Export failed. Please try again.")
+    const downloadChecker = setInterval(() => {
+      if (document.cookie.includes("downloadComplete=true")) {
+        clearInterval(downloadChecker)
+        alert("Packet export complete!")
+        window.location.reload()
       }
-    } catch (error) {
-      alert("An error occurred. Please try again.")
-    }
+    }, 500)
   }
 }
+
