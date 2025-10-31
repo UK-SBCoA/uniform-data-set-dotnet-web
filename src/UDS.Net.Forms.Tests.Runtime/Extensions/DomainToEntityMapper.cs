70bc6f92eae7bdc0b141de037a163ed9f67a5ff3
ahmedhaly7777 <ahmed.aly@uky.edu>
2025-10-31 14:30:20 -0400

    Update RadioButtonGroupTagHelper.cs (#573)
    * Update RadioButtonGroupTagHelper.cs
    
    * Adds playwright tests for A4a initialization
    
    * Take out the tag helper from the view and fix bug
    
    * Add more A4a playwright tests
    
    * Form view changes and validation updates
    
    * Bump version
    
    ---------
    
    Co-authored-by: Ashley Wilson <ashleybot@users.noreply.github.com>

--- src/UDS.Net.Forms.Tests.Runtime/Extensions/DomainToEntityMapper.cs
+++ src/UDS.Net.Forms.Tests.Runtime/Extensions/DomainToEntityMapper.cs
@@ -8,40 +8,36 @@ namespace UDS.Net.Forms.Tests.Runtime.Extensions
 {
     public static class DomainToEntityMapper
     {
-        public static void UpdateFromDomain(this UDS.Net.API.Entities.A3 existing, string formKind, UDS.Net.Services.DomainModels.Visit visit)
+        public static void UpdateFromDomain(this UDS.Net.API.Entities.Form existing, string formKind, UDS.Net.Services.DomainModels.Visit visit)
         {
             var form = visit.Forms.Where(f => f.Kind == formKind).FirstOrDefault();
 
             var formDto = form.ToDto(formKind);
 
-            var dto = (A3Dto)formDto;
+            if (existing is UDS.Net.API.Entities.A3)
+            {
+                var dto = (A3Dto)formDto;
 
-            existing.Update(dto);
-        }
+                ((UDS.Net.API.Entities.A3)existing).Update(dto);
+            }
+            else if (existing is UDS.Net.API.Entities.A4a)
+            {
+                var dto = (A4aDto)formDto;
 
-        public static void UpdateFromDomain(this UDS.Net.API.Entities.C2 existing, string formKind, UDS.Net.Services.DomainModels.Visit visit)
-        {
-            // formId should = C2 here
-            var form = visit.Forms.Where(f => f.Kind == formKind).FirstOrDefault();
+                ((UDS.Net.API.Entities.A4a)existing).Update(dto);
+            }
+            else if (existing is UDS.Net.API.Entities.C2)
+            {
+                var dto = (C2Dto)formDto;
 
-            var formDto = form.ToDto(formKind);
+                ((UDS.Net.API.Entities.C2)existing).Update(dto);
+            }
+            else if (existing is UDS.Net.API.Entities.D1a)
+            {
+                var dto = (D1aDto)formDto;
 
-            // domain to dto
-            var dto = (C2Dto)formDto;
-
-            // this will update the entity with the updated properties
-            existing.Update(dto);
-        }
-
-        public static void UpdateFromDomain(this UDS.Net.API.Entities.D1a existing, string formKind, UDS.Net.Services.DomainModels.Visit visit)
-        {
-            var form = visit.Forms.Where(f => f.Kind == formKind).FirstOrDefault();
-
-            var formDto = form.ToDto(formKind);
-
-            var dto = (D1aDto)formDto;
-
-            existing.Update(dto);
+                ((UDS.Net.API.Entities.D1a)existing).Update(dto);
+            }
         }
     }
 }
