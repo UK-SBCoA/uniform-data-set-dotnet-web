03842769c74b3e07fc68138e67e0b16bade95d8a
Cody Penn <96854090+Oddvocado@users.noreply.github.com>
2025-11-05 10:25:14 -0500

    Bulk export functionality (#555)
    * update view and model
    
    * add stimulus controller & modal
    
    * update stimulus controller & view markup
    
    * add routing for multiple packet export
    
    * extracts csv method to a helper method & updates endpoints
    
    * cleaning up
    
    * corrects csv formatting
    
    * creates and updates new packet submissions
    
    * full stream ahead
    
    * adds page refresh stimulus controller
    
    * persist checkbox state with pagination
    
    * adjusts file naming on multiple export
    
    * corrects stimulus data attribute placement
    
    * creates cookie to flag a completed packet export before reloading the page
    
    * linter fix
    
    * clears session storage to prevent duplicate submissions
    
    * fixes double submission bug
    
    * adds converter and fixes null form field bug
    
    * match file name to NACC requirements/adds converter to single export
    
    * adjust date format on csv document title
    
    * address PR comments
    
    * add preview tag
    
    * removes redundant converter
    
    * removes redundancy & adds some null checks
    
    * more specific cookie name
    
    To prevent any issues when a user has multiple sessions open in the browser
    
    * preview bump
    
    * hides submit button until the table updates
    
    * preview tag bump
    
    * remove unused code-behind
    
    * version bump

---
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/pageRefreshOnSubmit_controller.js
@@ -0,0 +1,25 @@
+﻿import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = ["button"]
+
+  submit(event) {
+    if (this.hasButtonTarget) {
+      this.buttonTarget.disabled = true
+      this.buttonTarget.classList.remove('bg-indigo-600', 'hover:bg-indigo-700')
+      this.buttonTarget.classList.add('bg-gray-400', 'cursor-not-allowed')
+      this.buttonTarget.textContent = "Exporting..."
+    }
+
+    document.cookie = "udsPacketExportComplete=false"
+
+    const downloadChecker = setInterval(() => {
+      if (document.cookie.includes("udsPacketExportComplete=true")) {
+        clearInterval(downloadChecker)
+        alert("Packet export complete!")
+        window.location.reload()
+      }
+    }, 500)
+  }
+}
+
