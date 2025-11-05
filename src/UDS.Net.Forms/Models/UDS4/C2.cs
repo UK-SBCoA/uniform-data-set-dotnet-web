761fbde94cfc98700f524a3b97254a7a82a85d57
mlan225 <17088502+mlan225@users.noreply.github.com>
2025-11-05 03:23:32 -0500

    Add property names back to the C2T and add additional validation method for C2T REYDREC

--- src/UDS.Net.Forms/Models/UDS4/C2.cs
+++ src/UDS.Net.Forms/Models/UDS4/C2.cs
@@ -638,11 +638,30 @@ namespace UDS.Net.Forms.Models.UDS4
         // If REY1REC is 95 - 98 for in person and video modalities, then REYDREC must have a value
         [NotMapped]
         [RequiredOnFinalized(ErrorMessage = "A value of 95 - 98 is required for 13a. Total delayed recall when 12a. is 95 - 98")]
-        public bool? REYDRECValidation
+        public bool? REYDRECC2Validation
         {
             get
             {
                 if (MODE == FormMode.InPerson || RMMODE == RemoteModality.Video)
+                {
+                    if (REY1REC.HasValue && (REY1REC.Value >= 95 && REY1REC.Value <= 98))
+                    {
+                        return REYDREC.HasValue && (REYDREC.Value >= 95 && REYDREC.Value <= 98) ? true : null;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        // If REY1REC is 95 - 98 for in person and video modalities, then REYDREC must have a value
+        [NotMapped]
+        [RequiredOnFinalized(ErrorMessage = "A value of 95 - 98 is required for 13a. Total delayed recall when 6a. is 95 - 98")]
+        public bool? REYDRECC2TValidation
+        {
+            get
+            {
+                if (MODE == FormMode.Remote && RMMODE == RemoteModality.Telephone)
                 {
                     if (REY1REC.HasValue && (REY1REC.Value >= 95 && REY1REC.Value <= 98))
                     {
