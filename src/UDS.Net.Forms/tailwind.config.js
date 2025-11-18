437d8a35d571317d63407b06367bc883fbe26ace
Ashley Wilson <ashleybot@users.noreply.github.com>
2025-11-18 11:03:41 -0500

    Adjust forms razor class library to use local tailwind

---
+++ src/UDS.Net.Forms/tailwind.config.js
@@ -0,0 +1,17 @@
+﻿// tailwind.config.js
+// https://tailwindcss.com/docs/configuration
+// https://tailwindcss.com/docs/theme
+
+module.exports = {
+  content: [
+    './Pages/**/*.cshtml',
+    './Views/**/*.cshtml'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      }
+    }
+  }
+}
