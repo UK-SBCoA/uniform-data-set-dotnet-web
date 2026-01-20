0451ed31372ff746c00780195a28176b524d4876
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-01-20 17:42:08 -0500

    Display current form values in packet submission error table (#609)
    * Initialize draft PR
    
    * Reformat the table, add data, and create stimulus controller
    
    * Ui adjustments and create function of category filter
    
    * Add current value to the packet submission table
    
    * Add null checks and update getId methods in packet submissions
    
    To perform the reflection, we need to use GetPacketWithForms in the packet details and packet submissions views, or the returning data will not have the values for reflection to perform on.
    
    * bug fixes and adjustments to data handling on view
    
    * Revert the table to its original colors
    
    * Update null value display for previous value
    
    * Code review adjustments
    
    * In progress draft turbo-frame for current value
    
    * In progress update prepare handler and view for getting current value
    
    * Update submission error table to use stimulus lazy loading
    
    * Address linter error
    
    * Address spacing linter error
    
    * white space linter error fix
    
    * linter spacing error issue
    
    * Address superlinter spacing in details.cs
    
    * Rename method to add Async suffix
    
    * Remove unnecessary indent add on details.cshtml
    
    * reapply packetsubmission using and registration
    
    * Change _index partial back to PacketModel
    
    * Rename files, methods, and ids for better self documentation
    
    * Adjust errors count in view and current value logic
    
    * Adjust error message string and default route values
    
    * Address the turbo stream bug with current value
    
    Apply relative pathing due to turbo stream loading a partial for the stream element
    
    * Remove unnecessary usings
    
    * Bump to patch version 15.3.7

---
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/packetSubmission_controller.js
@@ -0,0 +1,85 @@
+import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+
+export default class extends Controller {
+    static targets = ["row", "unresolvedButton", "resolvedButton", "ignoredButton"]
+
+    static values = {
+        showUnresolved: { type: Boolean, default: false },
+        showResolved: { type: Boolean, default: false },
+        showIgnored: { type: Boolean, default: false }
+    }
+
+    connect() {
+        this.ToggleUnresolved()
+    }
+
+    FilterError(event) {
+        if (event.params.status == "unresolved") {
+            this.ToggleUnresolved()
+        }
+        if (event.params.status == "resolved") {
+            this.ToggleResolved()
+        }
+        if (event.params.status == "ignored") {
+            this.ToggleIgnored()
+        }
+    }
+
+    ToggleUnresolved() {
+        this.showUnresolvedValue = !this.showUnresolvedValue;
+
+        if (this.showUnresolvedValue) {
+            this.unresolvedButtonTarget.classList.add("border", "border-2")
+
+            this.ShowRowsByStatus("pending")
+        } else {
+            this.unresolvedButtonTarget.classList.remove("border", "border-2")
+
+            this.HideRowsByStatus("pending")
+        }
+    }
+
+    ToggleResolved() {
+        this.showResolvedValue = !this.showResolvedValue;
+
+        if (this.showResolvedValue) {
+            this.resolvedButtonTarget.classList.add("border", "border-2")
+
+            this.ShowRowsByStatus("resolved")
+        } else {
+            this.resolvedButtonTarget.classList.remove("border", "border-2")
+
+            this.HideRowsByStatus("resolved")
+        }
+    }
+
+    ToggleIgnored() {
+        this.showIgnoredValue = !this.showIgnoredValue;
+
+        if (this.showIgnoredValue) {
+            this.ignoredButtonTarget.classList.add("border", "border-2")
+
+            this.ShowRowsByStatus("ignored")
+        } else {
+            this.ignoredButtonTarget.classList.remove("border", "border-2")
+
+            this.HideRowsByStatus("ignored")
+        }
+    }
+
+    HideRowsByStatus(status) {
+        this.rowTargets.forEach((row) => {
+            if (row.dataset.status == status) {
+                row.classList.add("hidden")
+            }
+        })
+    }
+
+    ShowRowsByStatus(status) {
+        this.rowTargets.forEach((row) => {
+            if (row.dataset.status == status) {
+                row.classList.remove("hidden")
+            }
+        })
+    }
+}
