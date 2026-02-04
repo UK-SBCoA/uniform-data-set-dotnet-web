edbbe4a48bdd620ed47c9e8d0242166d3c5a48d8
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-02-04 02:29:48 -0500

    Only display errors from CSV for packets in submitted status

--- src/UDS.Net.Forms/Pages/PacketSubmissions/BulkPacketSubmission/Create.cshtml.cs
+++ src/UDS.Net.Forms/Pages/PacketSubmissions/BulkPacketSubmission/Create.cshtml.cs
@@ -5,20 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Globalization;
 using UDS.Net.Forms.Models;
+using UDS.Net.Services;
+using UDS.Net.Services.DomainModels;
+using UDS.Net.Services.DomainModels.Submission;
+using UDS.Net.Services.Enums;
 
 namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
 {
     public class CreateModel : PageModel
     {
+        protected readonly IVisitService _visitService;
+        protected readonly IPacketService _packetService;
+        protected readonly IParticipationService _participationService;
         public IFormFile? ErrorFileUpload { get; set; }
         public List<NACCErrorModel> PacketSubmissionErrors { get; set; } = new List<NACCErrorModel>();
+
+        public CreateModel(IVisitService visitService, IPacketService packetService, IParticipationService participationService)
+        {
+            _visitService = visitService;
+            _packetService = packetService;
+            _participationService = participationService;
+        }
 
         public void OnGet()
         {
         }
 
         [ValidateAntiForgeryToken]
-        public IActionResult OnPostUploadBulkSubmission()
+        public async Task<IActionResult> OnPostUploadBulkSubmission()
         {
             if (ErrorFileUpload == null)
             {
@@ -31,6 +45,31 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
             };
 
             int rowIndex = 0;
+
+            //DEVNOTE: variables for submitted packets to filter the CSV data
+            //Ptid must == legacyId, visitNum must match, and the alert must not have been previously accepted
+
+            //DEVNOTE: We only want to get errors for packets that are in the submitted status as well
+            //1. Get all packets with status = 2 (submitted)
+            //2. Create list of participation ids of packets returned
+            //3. in the if, check if the PTID is in the list of participation Ids
+
+            var submittedPackets = await _visitService.ListByStatus(User.Identity.Name, 10, 1, [PacketStatus.Submitted.ToString()]);
+            //DEVNOTE: can get legacyId (PTID) from participation
+            //ParticipationId and legacyId are NOT always equal
+            var legacyIdsFromPackets = new List<string>();
+
+            foreach (var submittedPacket in submittedPackets)
+            {
+                //legacyIdsFromPackets.Add(await _participationService.GetById(User.Identity.Name, submittedPacket.ParticipationId, false));
+                //DEVNOTE: getById allows "include visit" as a boolean argument. This could help?
+                var participation = await _participationService.GetById(User.Identity.Name, submittedPacket.ParticipationId);
+                var legacyId = participation.LegacyId;
+
+                //DEVNOTE: legacyId is a string
+                legacyIdsFromPackets.Add(legacyId);
+            }
+
             using (var stream = ErrorFileUpload.OpenReadStream())
             using (var reader = new StreamReader(stream))
             using (var csv = new CsvReader(reader, config))
@@ -43,9 +82,7 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
                     {
                         var record = csv.GetRecord<NACCErrorModel>();
 
-                        //Ptid must == legacyId, visitNum must match, and the alert must not have been previously accepted
-                        //DEVNOTE: We only want to get errors for packets that are in the submitted status as well
-                        if (record.Approved.ToLower() == "false")
+                        if (record.Approved.ToLower() == "false" && legacyIdsFromPackets.Contains(record.Ptid))
                         {
                             NACCErrorModel newPacketSubmissionError = new NACCErrorModel
                             {
@@ -62,6 +99,10 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
 
                             PacketSubmissionErrors.Add(newPacketSubmissionError);
                         }
+
+                        //Update current packet with newPacketSubmissionError list
+                        //await _packetService.UpdatePacketSubmissionErrors(User.Identity.Name, currentPacket, PacketSubmissionId, packetSubmissionErrors);
+
                         rowIndex++;
                     }
                 }
