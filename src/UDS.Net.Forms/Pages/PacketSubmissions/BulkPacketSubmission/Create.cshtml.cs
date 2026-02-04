6a1cd646d3f08712ddb8a71b92bc14f41166e790
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-02-04 18:45:51 -0500

    Implement draft version of bulk packet error save

--- src/UDS.Net.Forms/Pages/PacketSubmissions/BulkPacketSubmission/Create.cshtml.cs
+++ src/UDS.Net.Forms/Pages/PacketSubmissions/BulkPacketSubmission/Create.cshtml.cs
@@ -3,6 +3,7 @@ using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Immutable;
 using System.Globalization;
 using UDS.Net.Forms.Models;
 using UDS.Net.Services;
@@ -15,24 +16,25 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
     public class CreateModel : PageModel
     {
         protected readonly IVisitService _visitService;
-        protected readonly IPacketService _packetService;
         protected readonly IParticipationService _participationService;
+        protected readonly IPacketService _packetService;
         public IFormFile? ErrorFileUpload { get; set; }
+        [BindProperty]
         public List<NACCErrorModel> PacketSubmissionErrors { get; set; } = new List<NACCErrorModel>();
-
-        public CreateModel(IVisitService visitService, IPacketService packetService, IParticipationService participationService)
+        public CreateModel(IVisitService visitService, IParticipationService participationService, IPacketService packetService)
         {
             _visitService = visitService;
-            _packetService = packetService;
             _participationService = participationService;
+            _packetService = packetService;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
         }
 
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> OnPostUploadBulkSubmission()
+        public async Task<IActionResult> OnPostDisplayBulkSubmission()
         {
             if (ErrorFileUpload == null)
             {
@@ -54,7 +56,8 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
             //2. Create list of participation ids of packets returned
             //3. in the if, check if the PTID is in the list of participation Ids
 
-            var submittedPackets = await _visitService.ListByStatus(User.Identity.Name, 10, 1, [PacketStatus.Submitted.ToString()]);
+            IEnumerable<Visit> submittedPackets = await _visitService.ListByStatus(User.Identity.Name, 10, 1, [PacketStatus.Submitted.ToString()]);
+
             //DEVNOTE: can get legacyId (PTID) from participation
             //ParticipationId and legacyId are NOT always equal
             var legacyIdsFromPackets = new List<string>();
@@ -126,6 +129,97 @@ namespace UDS.Net.Forms.Pages.PacketSubmissions.BulkPacketSubmission
             }
 
             return Page();
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OnPostConfirmBulkSubmission()
+        {
+            //DEVNOTE: dirty approach here, reget the submitted packets for use with updating packet submission
+            IEnumerable<Visit> submittedPackets = await _visitService.ListByStatus(User.Identity.Name, 10, 1, [PacketStatus.Submitted.ToString()]);
+
+            var submittedParticipationList = new List<Participation>();
+
+            foreach (var packet in submittedPackets)
+            {
+                //legacyIdsFromPackets.Add(await _participationService.GetById(User.Identity.Name, packet.ParticipationId, false));
+                //DEVNOTE: getById allows "include visit" as a boolean argument. This could help?
+                var participation = await _participationService.GetById(User.Identity.Name, packet.ParticipationId);
+
+                submittedParticipationList.Add(participation);
+            }
+
+            //DEVNOTE: Group packet submission errors by PTID
+            var packetSubmissionErrorsGrouped = PacketSubmissionErrors.GroupBy(p => p.Ptid);
+
+            //DEVNOTE: For each group of ptids update packet submissions
+            foreach(var errorGroup in packetSubmissionErrorsGrouped)
+            {
+                var groupPtid = errorGroup.ElementAt(0).Ptid;
+                var groupVisitnum = errorGroup.ElementAt(0).Visitnum;
+
+                var groupParticipation = submittedParticipationList.Where(p => p.LegacyId == groupPtid).FirstOrDefault();
+
+                var groupVisit = submittedPackets.Where(p => p.ParticipationId == groupParticipation?.Id).FirstOrDefault();
+
+                var groupPacket = await _packetService.GetById(User.Identity.Name, groupVisit.Id);
+
+                var groupSubmissionId = groupPacket.Submissions.Where(s => s.ErrorCount == null).Select(i => i.Id).FirstOrDefault();
+
+                List<PacketSubmissionError> groupPacketSubmissionErrors = new List<PacketSubmissionError>();
+
+                foreach(var error in errorGroup)
+                {
+                    PacketSubmissionError newPacketSubmissionError = new PacketSubmissionError(
+                        id: 0,
+                        packetSubmissionId: groupSubmissionId,
+                        formKind: error.Code.Split("-")[0].ToUpper(),
+                        message: error.Message,
+                        assignedTo: groupPacket.CreatedBy,
+                        level: GetErrorLevel(error.Type),
+                        status: PacketSubmissionErrorStatus.Pending,
+                        statusChangedBy: null,
+                        createdAt: DateTime.Now,
+                        createdBy: User.Identity.Name,
+                        modifiedBy: null,
+                        deletedBy: null,
+                        isDeleted: false,
+                        location: error.Location?.ToUpper(),
+                        value: error.Value
+                    );
+
+                    groupPacketSubmissionErrors.Add(newPacketSubmissionError);
+                }
+
+                await _packetService.UpdatePacketSubmissionErrors(User.Identity.Name, groupPacket, groupSubmissionId, groupPacketSubmissionErrors);
+            }
+
+
+            //DEVNOTE: need to get the packetSubmissionId from the item from _packetService.GetById with submission.ErrorCount == null
+
+            //DEVNOTE: Things I need
+            //current packet (Packet type) - 
+            //Packet Submission Id (int)
+            //Packet submission errors (List<PacketSubmissionErrors>)
+
+            //await _packetService.UpdatePacketSubmissionErrors(User.Identity.Name, currentPacket, PacketSubmissionId, packetSubmissionErrors);
+
+            return Page();
+        }
+
+        //DEVNOTE: Copied from the packetSubmissionError/Create.cshtml.cs
+        private static PacketSubmissionErrorLevel GetErrorLevel(string errorType)
+        {
+            if (errorType.Trim().ToLower() == "alert")
+            {
+                return PacketSubmissionErrorLevel.Information;
+            }
+            else if (errorType.Trim().ToLower() == "error")
+            {
+                return PacketSubmissionErrorLevel.Error;
+            }
+
+            //return information as default
+            return PacketSubmissionErrorLevel.Information;
         }
     }
 }
