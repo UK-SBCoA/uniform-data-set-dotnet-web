d5a696768dd1313a6fe73d0054a5f4a14d55ae91
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-03-11 18:16:05 -0400

    Upgrade to preview packages, create service method, create NACCError domain model

---
+++ src/UDS.Net.Services/DomainModels/NACCError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UDS.Net.Services.DomainModels
+{
+    public class NACCError
+    {
+        public string Timestamp { get; set; }
+        public string Type { get; set; }
+        public string Code { get; set; }
+        public string Location { get; set; }
+        public string File { get; set; }
+        public string Value { get; set; }
+        public string Message { get; set; }
+        public string Ptid { get; set; }
+        public string Visitnum { get; set; }
+        public string Approved { get; set; }
+    }
+}
--- src/UDS.Net.Services/IPacketService.cs
+++ src/UDS.Net.Services/IPacketService.cs
@@ -16,6 +16,8 @@ namespace UDS.Net.Services
         Task<int> Count(string username, List<PacketStatus> statuses);
 
         Task<Packet> UpdatePacketSubmissionErrors(string username, Packet packetToEdit, int packetSubmissionId, List<PacketSubmissionError> errors);
+        Task<List<NACCError>> CreatePacketSubmissionErrors(string username, List<NACCError> errors);
+
     }
 }
 
