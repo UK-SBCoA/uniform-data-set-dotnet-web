492dd3af1a90f6df82e4480b01099bbb6d44b977
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-04-01 18:13:11 -0400

    Initialize PR

---
+++ src/UDS.Net.Forms.Tests/ExpotTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Playwright;
+
+namespace UDS.Net.Forms.Tests
+{
+    [TestClass]
+    public class ExpotTests : TestBase
+    {
+        [TestMethod]
+        public async Task ExportSinglePacket()
+        {
+            //await Page.GotoAsync(BaseUrl);
+            //await Page.GetByRole(AriaRole.Button, new() { Name = "New visit" }).ClickAsync();
+            //await Expect(Page.GetByRole(AriaRole.List)).ToContainTextAsync("A3");
+            //await Expect(Page.GetByRole(AriaRole.List)).ToContainTextAsync("Required");
+            //await Page.GetByRole(AriaRole.Listitem).Filter(new() { HasText = "A3 Required" }).GetByRole(AriaRole.Link).ClickAsync();
+
+            //await Expect(Page.GetByLabel("Save status")).ToContainTextAsync("Not started In progress Finalized");
+            //await Page.GetByLabel("Save status").SelectOptionAsync(new[] { "1" });
+
+            //// reopen the page and see the data persisted
+            //await Page.GetByRole(AriaRole.Button, new() { Name = "Save", Exact = true }).ClickAsync();
+            //await Page.GetByRole(AriaRole.Listitem).Filter(new() { HasText = "A3 Required" }).GetByRole(AriaRole.Link).ClickAsync();
+
+
+        }
+    }
+}
