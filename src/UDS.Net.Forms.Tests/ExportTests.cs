11c6629a8812569c0283d39d060f8111c41e7df5
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-04-08 18:37:40 -0400

    Implement the first test against export file

---
+++ src/UDS.Net.Forms.Tests/ExportTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright;
+
+namespace UDS.Net.Forms.Tests
+{
+    [TestClass]
+    public class ExportTests : TestBase
+    {
+        [TestMethod]
+        public async Task ExportPacketHas1413Colums()
+        {
+            await Page.GotoAsync(BaseUrl);
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Packets Index" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "View" }).ClickAsync();
+
+            var download = await Page.RunAndWaitForDownloadAsync(async () =>
+            {
+                await Page.GetByRole(AriaRole.Link, new() { Name = "UDS_1000_" }).ClickAsync();
+            });
+
+            using var stream = await download.CreateReadStreamAsync();
+            using var reader = new StreamReader(stream);
+
+            var firstLine = await reader.ReadLineAsync();
+            var columns = firstLine?.Split(',');
+
+            Assert.AreEqual(1413, columns?.Length);
+        }
+    }
+}
