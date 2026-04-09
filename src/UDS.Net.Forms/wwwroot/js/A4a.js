9e1f6b5c3916364f49d7a3d595502b49cbbed4ea
Oddvocado <96854090+Oddvocado@users.noreply.github.com>
2026-04-09 16:12:57 -0400

    adds javascript to listen for changes and adjust inputs accordingly

--- src/UDS.Net.Forms/wwwroot/js/A4a.js
+++ src/UDS.Net.Forms/wwwroot/js/A4a.js
@@ -4,76 +4,151 @@ This custom js is for handling the repeating child rows in the A4a. Other enabli
 
 $(document).ready(function () {
 
-    function toggleTreatmentTable(value) {
-        var enable = (value == '1');
-        var fields = $('.treatmentTable, input[type=radio][name*="CARETRIAL"], input[type=radio][name*="TARGETOTX"]');
-        fields.prop('disabled', !enable);
-        if (!enable) {
-            fields.each(function () {
-                if ($(this).is(":radio") || $(this).is(":checkbox")) {
-                    $(this).prop("checked", false);
-                } else {
-                    $(this).val("");
-                }
-            });
-
-            $('input[type=radio][name*="TRIALGRP"]').prop('disabled', true).prop('checked', false);
-        }
-    }
-
-    /* Child row TARGETOTH checkbox and TARGETOTX input */
-    function toggleTargetotxInput(checkbox) {
-        var isChecked = $(checkbox).is(':checked');
-        var row = $(checkbox).closest('tr');
-        var targetOtxInput = row.find('input[name$="TARGETOTX"]');
-        targetOtxInput.prop('disabled', !isChecked);
-        if (!isChecked) {
-            targetOtxInput.val("");
-        }
-    }
-
-    // Enable/disable TRIALGRP based on CARETRIAL radio selection
-    function toggleTrialGroupRadioGroup(row) {
-        var careTrialValue = row.find('input[type=radio][name*="CARETRIAL"]:checked').val();
-        var trialGroupRadios = row.find('input[type=radio][name*="TRIALGRP"]');
-
-        if (careTrialValue === '2' || careTrialValue === '3') {
-            trialGroupRadios.prop('disabled', false);
+  function toggleTreatmentTable(value) {
+    var enable = (value == '1');
+    var fields = $('.treatmentTable, input[type=radio][name*="CARETRIAL"], input[type=radio][name*="TARGETOTX"]');
+    fields.prop('disabled', !enable);
+    if (!enable) {
+      fields.each(function () {
+        if ($(this).is(":radio") || $(this).is(":checkbox")) {
+          $(this).prop("checked", false);
         } else {
-            trialGroupRadios.prop('disabled', true).prop('checked', false);
+          $(this).val("");
         }
+      });
+
+      $('input[type=radio][name*="TRIALGRP"]').prop('disabled', true).prop('checked', false);
+    }
+  }
+
+  /* Child row TARGETOTH checkbox and TARGETOTX input */
+  function toggleTargetotxInput(checkbox) {
+    var isChecked = $(checkbox).is(':checked');
+    var row = $(checkbox).closest('tr');
+    var targetOtxInput = row.find('input[name$="TARGETOTX"]');
+    targetOtxInput.prop('disabled', !isChecked);
+    if (!isChecked) {
+      targetOtxInput.val("");
+    }
+  }
+
+  // Enable/disable TRIALGRP based on CARETRIAL radio selection
+  function toggleTrialGroupRadioGroup(row) {
+    var careTrialValue = row.find('input[type=radio][name*="CARETRIAL"]:checked').val();
+    var trialGroupRadios = row.find('input[type=radio][name*="TRIALGRP"]');
+
+    if (careTrialValue === '2' || careTrialValue === '3') {
+      trialGroupRadios.prop('disabled', false);
+    } else {
+      trialGroupRadios.prop('disabled', true).prop('checked', false);
+    }
+  }
+
+  /* Set up event handlers */
+  $('input[type=radio][name*="A4a.TRTBIOMARK"]').change(function () {
+    toggleTreatmentTable($(this).val());
+  });
+
+  $('input[name*="TARGETOTH"]').change(function () {
+    toggleTargetotxInput(this);
+  });
+
+  $('input[type=radio][name*="CARETRIAL"]').change(function () {
+    var row = $(this).closest('tr');
+    toggleTrialGroupRadioGroup(row);
+  });
+
+  /* Initialize */
+  var trtbiomarkValue = $('input[type=radio][name*="A4a.TRTBIOMARK"]:checked').val();
+
+  /* Determine how child rows should load based on TRTBIOMARK value */
+  toggleTreatmentTable(trtbiomarkValue);
+
+  // Loop through rows to initialize TARGETOTX and TRIALGRP fields
+  $('tr').each(function () {
+    // Initialize TARGETOTX (Other checkbox/text pairing)
+    var targetOthCheckbox = $(this).find('input[name*="TARGETOTH"]');
+    if (targetOthCheckbox.length) {
+      toggleTargetotxInput(targetOthCheckbox);
     }
 
-    /* Set up event handlers */
-    $('input[type=radio][name*="A4a.TRTBIOMARK"]').change(function () {
-        toggleTreatmentTable($(this).val());
-    });
+    // Initialize TRIALGRP based on CARETRIAL
+    toggleTrialGroupRadioGroup($(this));
+  });
 
-    $('input[name*="TARGETOTH"]').change(function () {
-        toggleTargetotxInput(this);
-    });
 
-    $('input[type=radio][name*="CARETRIAL"]').change(function () {
-        var row = $(this).closest('tr');
-        toggleTrialGroupRadioGroup(row);
-    });
+  //#region Handles UI Behaviors of NEWTREAT && NEWADEVENT
+  function setNewTreatAndAdEvent() {
+    if (!a4ainputs) {
+      return;
+    }
 
-    /* Initialize */
-    var trtbiomarkValue = $('input[type=radio][name*="A4a.TRTBIOMARK"]:checked').val();
+    const inputs = a4ainputs.querySelectorAll("input");
+    const newTreat = a4ainputs.querySelectorAll('input[type=radio][name="A4a.NEWTREAT"]');
+    const newAdEvent = a4ainputs.querySelectorAll('input[name="A4a.NEWADEVENT"]');
 
-    /* Determine how child rows should load based on TRTBIOMARK value */
-    toggleTreatmentTable(trtbiomarkValue);
+    if (newTreat.length === 0 || newAdEvent.length === 0) {
+      return;
+    }
 
-    // Loop through rows to initialize TARGETOTX and TRIALGRP fields
-    $('tr').each(function () {
-        // Initialize TARGETOTX (Other checkbox/text pairing)
-        var targetOthCheckbox = $(this).find('input[name*="TARGETOTH"]');
-        if (targetOthCheckbox.length) {
-            toggleTargetotxInput(targetOthCheckbox);
+    let hasChanged = false;
+
+    function setInitialState() {
+      newTreat.forEach(radio => {
+        if (radio.value === "1") {
+          radio.checked = false;
+          radio.disabled = true;
+        } else if (radio.value === "0" || radio.value === "9") {
+          radio.disabled = false;
         }
+      });
 
-        // Initialize TRIALGRP based on CARETRIAL
-        toggleTrialGroupRadioGroup($(this));
+      newAdEvent.forEach(radio => {
+        if (radio.value === "1") {
+          radio.checked = false;
+          radio.disabled = true;
+        } else if (radio.value === "0" || radio.value === "9") {
+          radio.disabled = false;
+        }
+      });
+    }
+
+    function setChangedState() {
+      newTreat.forEach(radio => {
+        if (radio.value === "1") {
+          radio.checked = true;
+          radio.disabled = false;
+        } else if (radio.value === "0" || radio.value === "9") {
+          radio.disabled = true;
+        }
+      });
+
+      newAdEvent.forEach(radio => {
+        if (radio.value === "1") {
+          radio.checked = true;
+          radio.disabled = false;
+        } else if (radio.value === "0" || radio.value === "9") {
+          radio.disabled = true;
+        }
+      });
+    }
+
+    setInitialState();
+
+    inputs.forEach(input => {
+      input.addEventListener('input', (event) => {
+
+        if (event.target.name === "A4a.NEWTREAT") return;
+        if (event.target.name === "A4a.NEWADEVENT") return;
+
+        if (!hasChanged) {
+          hasChanged = true;
+          setChangedState();
+        }
+      });
     });
+  }
 
+  setNewTreatAndAdEvent();
+  //#endregion
 });
