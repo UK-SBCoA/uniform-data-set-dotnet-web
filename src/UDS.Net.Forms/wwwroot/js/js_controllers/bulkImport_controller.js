1a50e6d2d49ca9a2c9217c52c15d97b4e2d0aa0c
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-07-07 17:57:06 -0400

    Adjust the functionality of ignored/resolved checkbox

--- src/UDS.Net.Forms/wwwroot/js/js_controllers/bulkImport_controller.js
+++ src/UDS.Net.Forms/wwwroot/js/js_controllers/bulkImport_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
     static targets = ["window", "packetStatus", "ignoreResolvedCheckbox", "ptidGroup", "errorStatus"]
 
     ToggleErrorDisplay(event) {
-        console.log("initiating toggle error display")
         this.windowTargets.forEach((group) => {
             if (String(event.params.submissionIndex) == group.dataset.submissionIndex) {
                 group.toggleAttribute("hidden")
@@ -12,40 +11,39 @@ export default class extends Controller {
         })
     }
 
-    //DEVNOTE: currently only closes all
-    ToggleAll() {
+    CollapseAll() {
         this.windowTargets.forEach((group) => {
             group.hidden = true
         })
     }
 
     SetPacketStatusByAlertStatus() {
-        //Search all submission groups
-        this.ptidGroupTargets.forEach((group) => {
-            let groupIndex = group.dataset.bulkErrorSubmissionItemIndex
+        //check if checkbox is selected
+        if (this.ignoreResolvedCheckboxTarget.checked) {
+            //Search all submission groups
+            this.ptidGroupTargets.forEach((group) => {
+                let groupIndex = group.dataset.bulkErrorSubmissionItemIndex
 
-            //find all error statuses pertaining to that group
-            let groupErrors = this.errorStatusTargets.filter(e => e.dataset.bulkErrorSubmissionItemIndex == groupIndex)
+                //find all error statuses pertaining to that group
+                let groupErrors = this.errorStatusTargets.filter(e => e.dataset.bulkErrorSubmissionItemIndex == groupIndex)
 
-            if (groupErrors.length > 0) {
-                let statusForPacket = groupErrors.some(e => e.value == 0) ? "FailedErrorChecks" : "PassedErrorChecks";
+                if (groupErrors.length > 0) {
+                    let statusForPacket = groupErrors.some(e => e.value == 0) ? "FailedErrorChecks" : "PassedErrorChecks";
 
-                this.packetStatusTargets.filter(p => p.dataset.bulkErrorSubmissionItemIndex == groupIndex).forEach((e) => {
-                    e.value = statusForPacket
-                })
-            } 
-        })
-
-        //if any of the statuses are error = set to FailedErrorChecks
+                    this.packetStatusTargets.filter(p => p.dataset.bulkErrorSubmissionItemIndex == groupIndex).forEach((e) => {
+                        e.value = statusForPacket
+                    })
+                }
+            })
+        }
     }
 
-    ResetStatuses() {
-        // uncheck the update ignored/resolved checkbox
-        this.ignoreResolvedCheckboxTarget.checked = false
-
-        // Set all packet statuses back to FailedErrorChecks
-        this.packetStatusTargets.forEach((status) => {
-            status.value = "FailedErrorChecks"
-        });
+    HandleIgnoredResolved() {
+        if (this.ignoreResolvedCheckboxTarget.checked == false) {
+            //if ignored/resolved is unchecked, set all packets to failedErrorChecks
+            this.packetStatusTargets.forEach((status) => {
+                status.value = "FailedErrorChecks"
+            });
+        }
     }
 }
